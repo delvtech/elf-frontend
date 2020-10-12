7e414752cb59539079fca1174b28d766e331d023
Danny Delott <dannydelott@gmail.com>
2020-10-11 17:51:56 -0700

    Add hook for wallet connection, update state logic for tracking active wallet connection

--- frontend/src/efi/ui/wallets/hooks/useWallet.ts
+++ frontend/src/efi/ui/wallets/hooks/useWallet.ts
@@ -1,52 +1,35 @@
 import { UnsupportedChainIdError, useWeb3React } from "@web3-react/core";
-import { useState, useEffect, useCallback } from "react";
-import { useEagerConnect } from "efi/ui/wallets/hooks/useEagerConnect";
-import { useSyncWithInjectedEthereum } from "efi/ui/wallets/hooks/useSyncWithInjectedEthereum";
+import { useState, useEffect } from "react";
 import { AppToaster, makeErrorToast } from "efi/ui/app/AppToaster/AppToaster";
 import { t } from "ttag";
 import { BigNumber } from "ethers";
 import { Web3Provider } from "@ethersproject/providers";
-import { AbstractConnector } from "@web3-react/abstract-connector";
 import {
   NoEthereumProviderError,
   UserRejectedRequestError as UserRejectedRequestErrorInjected,
 } from "@web3-react/injected-connector";
 import { getConnectorName } from "efi/wallets/connectors";
+import { useWalletConnection } from "efi/ui/wallets/hooks/useWalletConnection";
 
 export function useWallet() {
-  const {
-    connector,
-    library,
-    chainId,
-    account,
-    activate,
-    deactivate,
-    active,
-    error,
-  } = useWeb3React<Web3Provider>();
+  const { connector, library, chainId, account, active, error } = useWeb3React<
+    Web3Provider
+  >();
 
-  // Handle logic to recognize the connector currently being activated. This can
-  // happen when the user changes to a different wallet or deactivates their
-  // active wallet.
-  const { setActiveConnector, isPending } = useActiveConnector(
-    connector,
-    activate
-  );
+  useErrorToast(error);
+
+  // Don't provide the account if the wallet was disconnected
+  const { isDisconnected } = useWalletConnection();
 
   // fetch eth balance of the connected account
   const ethBalance = useEthBalance(library, account, chainId);
 
-  const connectorName = useConnectorName(library, connector);
-
-  useErrorToast(error);
+  const connectorName = getConnectorName(connector);
 
   return {
     library,
     chainId,
-    account,
-    setActiveConnector,
-    isPending,
-    deactivate,
+    account: isDisconnected ? undefined : account,
     active,
     error,
     ethBalance,
@@ -57,69 +40,10 @@ export function useWallet() {
 function useErrorToast(error: Error | undefined) {
   useEffect(() => {
     if (error) {
-      AppToaster.clear(); // Only show one wallet error at a time
-
       const errorToast = makeErrorToast(getErrorMessage(error));
       AppToaster.show(errorToast);
     }
   }, [error]);
-}
-
-function useActiveConnector(
-  connector: AbstractConnector | undefined,
-  activate: (connector: AbstractConnector) => void
-) {
-  const [pendingConnector, setPendingConnectorState] = useState<
-    AbstractConnector
-  >();
-  const isPending = !!pendingConnector;
-
-  // Handle clearing out the pending connector
-  useEffect(() => {
-    if (isPending && pendingConnector === connector) {
-      setPendingConnectorState(undefined);
-    }
-  }, [pendingConnector, connector, isPending]);
-
-  const setActiveConnector = useCallback(
-    (connector: AbstractConnector) => {
-      setPendingConnectorState(connector);
-      activate(connector);
-    },
-    [activate]
-  );
-
-  // handle logic to eagerly connect to the injected ethereum provider, eg:
-  // MetaMask, if it exists and has granted access already
-  const triedEager = useEagerConnect();
-
-  // handle logic to connect in reaction to certain events on the injected
-  // ethereum provider, ie: window.ethereum, if it exists
-  const suppress = !triedEager || isPending;
-  useSyncWithInjectedEthereum(suppress);
-
-  return {
-    setActiveConnector,
-    pendingConnector,
-    isPending: !!pendingConnector,
-  };
-}
-
-function useConnectorName(
-  library: Web3Provider | undefined,
-  connector: AbstractConnector | undefined
-): string {
-  const [connectorName, setConnectorName] = useState(t`No connection.`);
-
-  useEffect(() => {
-    if (!library) {
-      return;
-    }
-
-    setConnectorName(getConnectorName(library, connector));
-  }, [connector, library]);
-
-  return connectorName;
 }
 
 function useEthBalance(
