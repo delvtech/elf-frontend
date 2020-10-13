6ce8d92f4d705a0339cde4489ebe0853e2edac61
Matthew Brown <mattfdbrown@gmail.com>
2020-10-12 23:38:29 -0700

    relayout App.tsx

--- frontend/src/efi/ui/app/App/App.tsx
+++ frontend/src/efi/ui/app/App/App.tsx
@@ -1,42 +1,46 @@
-import React, { FC, useState } from "react";
-import { MainNavigation } from "efi/ui/app/MainNavigation/MainNavigation";
 import { Classes } from "@blueprintjs/core";
+import { LocationProvider, Router } from "@reach/router";
 import classNames from "classnames";
+import { Navigation } from "efi/ui/navigation/navigation";
+import { MainNavigation } from "efi/ui/navigation/MainNavigation/MainNavigation";
+import { HomeView } from "efi/ui/home/HomeView";
+import { PortfolioView } from "efi/ui/portfolio/PortfolioView";
+import { SwapView } from "efi/ui/swaps/SwapView/SwapView";
+import React, { FC } from "react";
 import { tw } from "tailwindcss-classnames";
 
 import styles from "./App.module.css";
-import { Navigation } from "efi/app/navigation";
-import { MissingWalletEmptyState } from "efi/ui/wallets/MissingWalletEmptyState/MissingWalletEmptyState";
-import { ActiveTab } from "../ActiveTab/ActiveTab";
-import { useWallet } from "efi/ui/wallets/hooks/useWallet";
 
-const App: FC<{}> = () => {
-  const { account } = useWallet();
+const appClassName = classNames(
+  styles.app,
+  Classes.DARK,
+  tw("flex", "flex-col", "lg:flex-row", "w-full", "h-full", "overflow-auto")
+);
 
-  const [activeTab, setActiveTab] = useState(Navigation.SWAP);
+const contentClassName = tw(
+  "flex-1",
+  "w-full",
+  "h-auto",
+  "lg:h-full",
+  "lg:w-auto",
+  "pt-12",
+  "lg:pt-0"
+);
 
+interface AppProps {}
+
+const App: FC<AppProps> = () => {
   return (
-    <div
-      className={classNames(
-        styles.app,
-        Classes.DARK,
-        tw("flex", "w-full", "h-full", "overflow-auto")
-      )}
-    >
-      <MainNavigation activeTab={activeTab} onSetActiveTab={setActiveTab} />
+    <div className={appClassName}>
+      <LocationProvider>
+        <MainNavigation />
+      </LocationProvider>
 
-      <div
-        className={classNames(
-          tw("flex", "w-full", "h-full"),
-          styles.contentContainer
-        )}
-      >
-        {!account ? (
-          <MissingWalletEmptyState />
-        ) : (
-          <ActiveTab activeTab={activeTab} />
-        )}
-      </div>
+      <Router className={contentClassName}>
+        <HomeView path="/" />
+        <PortfolioView path={Navigation.PORTFOLIO} />
+        <SwapView path={Navigation.SWAP} />
+      </Router>
     </div>
   );
 };
