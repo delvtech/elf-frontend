5404e5d338c7c4bd08ee76dc99bff43835efdec6
Matthew Brown <mattfdbrown@gmail.com>
2020-10-13 21:22:47 -0700

    add HomeView.tsx

---
+++ frontend/src/efi/ui/home/HomeView.tsx
@@ -0,0 +1,22 @@
+import { Card } from "@blueprintjs/core";
+import { RouteComponentProps } from "@reach/router";
+import React, { FC } from "react";
+import tw from "tailwindcss-classnames";
+import { t } from "ttag";
+
+interface HomeViewProps extends RouteComponentProps {}
+export const HomeView: FC<HomeViewProps> = () => {
+  return (
+    <div
+      className={tw(
+        "flex",
+        "h-full",
+        "w-full",
+        "justify-center",
+        "items-center"
+      )}
+    >
+      <Card>{t`Welcome to Element Finance`}</Card>
+    </div>
+  );
+};
