1545ea9c54ac059f234203005a761922ccaf1a0e
Danny Delott <dannydelott@gmail.com>
2020-10-21 21:53:34 -0700

    Add local chain constants

--- frontend/src/efi/crypto/ethereum.ts
+++ frontend/src/efi/crypto/ethereum.ts
@@ -4,16 +4,20 @@
 export enum ChainId {
   MAINNET = 1,
   KOVAN = 42,
+  LOCAL = 99,
 }
 
 export enum ChainName {
   MAINNET = "Ethereum Mainnet",
   KOVAN = "Ethereum Testnet Kovan",
+
+  LOCAL = "Local development",
 }
 
 export enum NetworkId {
   MAINNET = 1,
   KOVAN = 42,
+  LOCAL = 99,
 }
 
 export enum NetworkName {
@@ -21,4 +25,8 @@ export enum NetworkName {
   KOVAN = "kovan",
 }
 
-export const DEFAULT_CHAIN_IDS = [ChainId.MAINNET, ChainId.KOVAN];
+export const DEFAULT_CHAIN_IDS = [
+  ChainId.MAINNET,
+  ChainId.KOVAN,
+  ChainId.LOCAL,
+];
