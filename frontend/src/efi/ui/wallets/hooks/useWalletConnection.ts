5398d7128cf6330012cbd81cee518619fd5615a3
Danny Delott <dannydelott@gmail.com>
2020-10-22 12:23:54 -0700

    Refactor useWalletBalance and useWalletConnection to react-query

--- frontend/src/efi/ui/wallets/hooks/useWalletConnection.ts
+++ frontend/src/efi/ui/wallets/hooks/useWalletConnection.ts
@@ -7,47 +7,70 @@ import { AppToaster, makeSuccessToast } from "efi/ui/app/AppToaster/AppToaster";
 import { t } from "ttag";
 import { useMutation } from "react-query";
 
-export function useWalletConnection() {
-  const { activate, deactivate } = useWeb3React<Web3Provider>();
-  const [pendingConnector, setPendingConnector] = useState<
-    AbstractConnector | undefined
-  >();
+interface WalletConnection {
+  disconnect: () => void;
+  connect: (connector: AbstractConnector) => void;
+  status: WalletConnectionStatus;
+}
 
-  // Track when the user connects and disconnects.
-  const [isDisconnected, setDisconnected] = useState(false);
+export enum WalletConnectionStatus {
+  /**
+   * When the user has been prompted to authorize the app, but has yet to do
+   * so.
+   */
+  PENDING = "pending",
+
+  /**
+   * When the user has disconnected their wallet.
+   *
+   * Note: Being disconnected does not mean the user has de-authorized their
+   * wallet from the app, ie: it would not be safe to walk away from the
+   * computer. We should use this to allow users to pick a different wallet,
+   * but never to suggest that they have actually disconnected.
+   */
+  DISCONNECTED = "disconnected",
+
+  /**
+   * When the user has a connected wallet.
+   */
+  CONNECTED = "connected",
+}
+
+export function useWalletConnection(): WalletConnection {
+  const { activate, deactivate } = useWeb3React<Web3Provider>();
+  const [status, setStatus] = useState(WalletConnectionStatus.DISCONNECTED);
 
   const [connect] = useMutation(
     (connector: AbstractConnector) => {
       return activate(connector, undefined, true);
     },
     {
-      onMutate: (connector: AbstractConnector) => {
-        setPendingConnector(connector);
+      onMutate: () => {
+        // Hold onto the connector until the user grants authorization from
+        // their wallet app. We can use this to show a message or spinner.
+        setStatus(WalletConnectionStatus.PENDING);
       },
       onSuccess: () => {
         AppToaster.show(makeSuccessToast(t`Connected`));
-        setDisconnected(false);
-        setPendingConnector(undefined);
+        setStatus(WalletConnectionStatus.CONNECTED);
       },
     }
   );
 
   const disconnect = useCallback(() => {
-    setDisconnected(true);
     deactivate();
+    setStatus(WalletConnectionStatus.DISCONNECTED);
     AppToaster.show(makeSuccessToast(t`Disconnected`));
   }, [deactivate]);
 
   // handle logic to connect in reaction to certain events on the injected
   // ethereum provider, ie: window.ethereum, if it exists
-  const isPending = !!pendingConnector;
-  const suppress = isPending;
+  const suppress = status === WalletConnectionStatus.PENDING;
   useSyncWithInjectedEthereum(connect, suppress);
 
   return {
     disconnect,
     connect,
-    isPending,
-    isDisconnected,
+    status,
   };
 }
