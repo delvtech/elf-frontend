bb0da3d9a70d5a7a2f257b5b43795722627dca5d
Matthew Brown <mattfdbrown@gmail.com>
2020-10-24 13:20:34 -0700

    add preview cards to InvestView.tsx

--- frontend/src/efi/ui/invest/InvestView.tsx
+++ frontend/src/efi/ui/invest/InvestView.tsx
@@ -1,10 +1,13 @@
 import { RouteComponentProps } from "@reach/router";
+import { ElfStrategyHighRisk } from "efi/pools/highRisk";
 import { ElfStrategyLowRisk } from "efi/pools/lowRisk";
+import { ElfStrategyMediumRisk } from "efi/pools/mediumRisk";
+import { StrategyPreviewCard } from "efi/ui/pools/StrategeyPreviewCard/StrategyPreviewCard";
+import { StrategyCard } from "efi/ui/pools/StrategyCard/StrategyCard";
 import { useWallet } from "efi/ui/wallets/hooks/useWallet";
 import { MissingWalletEmptyState } from "efi/ui/wallets/MissingWalletEmptyState/MissingWalletEmptyState";
-import React, { FC } from "react";
+import React, { FC, useState } from "react";
 import tw from "tailwindcss-classnames";
-import { StrategyCard } from "efi/ui/pools/StrategyCard/StrategyCard";
 
 interface InvestViewProps extends RouteComponentProps {}
 
@@ -13,24 +16,59 @@ const investViewClassName = tw(
   "flex-col",
   "h-full",
   "w-full",
-  "justify-center",
-  "items-center"
+  "overflow-y-scroll"
 );
 
-const availableStrategies = [ElfStrategyLowRisk];
+const CardContainer = tw(
+  "flex-1",
+  "flex",
+  "flex-col",
+  "w-full",
+  "justify-center",
+  "items-center",
+  "p-3",
+  "space-y-3"
+);
+
+const availableStrategies = [
+  ElfStrategyLowRisk,
+  ElfStrategyMediumRisk,
+  ElfStrategyHighRisk,
+];
 
 export const InvestView: FC<InvestViewProps> = () => {
   const { account } = useWallet();
+  const [selectedStrategy, setSelectedStrategy] = useState<string>();
 
   if (!account) {
     return <MissingWalletEmptyState />;
   }
 
+  if (!selectedStrategy) {
+    return (
+      <div className={investViewClassName}>
+        <div className={CardContainer}>
+          {availableStrategies.map((strategy, index) => {
+            return (
+              <StrategyPreviewCard
+                onSelectStrategy={setSelectedStrategy}
+                key={strategy.id + index}
+                strategy={strategy}
+              />
+            );
+          })}
+        </div>
+      </div>
+    );
+  }
+
   return (
     <div className={investViewClassName}>
-      {availableStrategies.map((strategy) => {
-        return <StrategyCard key={strategy.id} strategy={strategy} />;
-      })}
+      <div className={CardContainer}>
+        {availableStrategies.map((strategy) => {
+          return <StrategyCard key={strategy.id} strategy={strategy} />;
+        })}
+      </div>
     </div>
   );
 };
