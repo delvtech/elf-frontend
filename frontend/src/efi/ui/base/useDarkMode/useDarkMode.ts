12663d9aae1cdc9220499355bb2a6ecc8b356f16
Danny Delott <dannydelott@gmail.com>
2020-10-27 16:16:15 -0700

    Use placeholderData so we don't pre-populate the cache

--- frontend/src/efi/ui/base/useDarkMode/useDarkMode.ts
+++ frontend/src/efi/ui/base/useDarkMode/useDarkMode.ts
@@ -9,17 +9,18 @@ const DARK_MODE_QUERY_KEY = "isDarkMode";
 const DARK_MODE_DEFAULT = false;
 
 export function useDarkMode(): DarkMode {
-  const { data: isDarkMode } = useQuery(
+  const { data: isDarkMode } = useQuery<boolean>(
     DARK_MODE_QUERY_KEY,
     () => {
       const item = window.localStorage.getItem(DARK_MODE_QUERY_KEY);
       return item ? JSON.parse(item) : DARK_MODE_DEFAULT;
     },
-    { initialData: DARK_MODE_DEFAULT }
+    { placeholderData: DARK_MODE_DEFAULT }
   );
 
   return {
-    isDarkMode,
+    // safe to cast when placeholder is set in useQuery
+    isDarkMode: isDarkMode as boolean,
     setDarkMode,
   };
 }
