6802101d82d73a9d7e0845857e610b5190a68b28
Danny Delott <dannydelott@gmail.com>
2020-10-30 17:17:36 -0700

    Relayout pulseview, add tabs for different view

--- frontend/src/efi/ui/pulse/PulseView.tsx
+++ frontend/src/efi/ui/pulse/PulseView.tsx
@@ -1,42 +1,113 @@
-import React, { FC } from "react";
+import React, { FC, useState } from "react";
 
+import { Card, Tab, Tabs } from "@blueprintjs/core";
 import { RouteComponentProps } from "@reach/router";
-import tw from "efi-tailwindcss-classnames";
+import { t } from "ttag";
 
+import tw from "efi-tailwindcss-classnames";
 import BrushChart from "efi/ui/charts/BrushChart/BrushChart";
 import { PieChart } from "efi/ui/charts/PieChart/PieChart";
 import { useDarkMode } from "efi/ui/prefs/useDarkMode/useDarkMode";
-import { Card } from "@blueprintjs/core";
 
 interface PulseViewProps extends RouteComponentProps {}
+
+enum PulseTab {
+  ALL_MARKETS = "all-markets",
+  WALLET = "current-wallet",
+}
 export const PulseView: FC<PulseViewProps> = () => {
   const { isDarkMode } = useDarkMode();
+  const [activePulse, setActivePulse] = useState<PulseTab>(
+    PulseTab.ALL_MARKETS
+  );
   return (
     <div
       className={tw(
         "flex",
-        "flex-wrap",
+        "flex-col",
         "h-full",
         "w-full",
-        "grid",
-        "grid-cols-1",
-        "md:grid-cols-2",
-        "grid-rows-none",
-        "md:grid-rows-2",
-        "gap-6",
-        "p-8"
+        "p-8",
+        "space-y-4",
+        "lg:flex-row",
+        "lg:flex-wrap",
+        "lg:overflow-scroll"
       )}
     >
-      <div className={tw("md:justify-center", "md:items-center")} />
-      <Card className={tw("justify-center", "items-center")}>
-        <BrushChart isDarkMode={isDarkMode} />
-      </Card>
+      <div className={tw("pb-4", "flex", "items-center")}>
+        <Tabs
+          large
+          selectedTabId={activePulse}
+          onChange={(tabId: PulseTab) => setActivePulse(tabId)}
+          className={tw("flex", "w-full", "justify-center")}
+        >
+          <Tab id={PulseTab.ALL_MARKETS}>{t`All strategies`}</Tab>
+          <Tab id={PulseTab.WALLET}>{t`My portfolio`}</Tab>
+        </Tabs>
+      </div>
 
-      <Card className={tw("justify-center", "items-center")}>
-        <PieChart isDarkMode={isDarkMode} />
-      </Card>
+      <div
+        className={tw(
+          "flex",
+          "flex-col",
+          "h-full",
+          "w-full",
+          "space-y-4",
+          "lg:h-auto",
+          "lg:min-h-1/4",
+          "lg:flex-row",
+          "lg:space-y-0",
+          "lg:space-x-4",
+          "lg:pb-4"
+        )}
+      >
+        <Card
+          className={tw(
+            "flex",
+            "h-64",
+            "justify-center",
+            "items-center",
+            "flex-1",
+            "lg:min-h-1/2",
+            "lg:h-auto"
+          )}
+        >
+          {activePulse === PulseTab.ALL_MARKETS ? (
+            <BrushChart compact isDarkMode={isDarkMode} />
+          ) : (
+            <PieChart isDarkMode={isDarkMode} />
+          )}
+        </Card>
 
-      <Card className={tw("justify-center", "items-center")}>
+        <Card
+          className={tw(
+            "flex",
+            "h-64",
+            "justify-center",
+            "items-center",
+            "flex-1",
+            "lg:min-h-1/2",
+            "lg:h-auto"
+          )}
+        >
+          {activePulse === PulseTab.ALL_MARKETS ? (
+            <PieChart isDarkMode={isDarkMode} />
+          ) : (
+            <BrushChart compact isDarkMode={isDarkMode} />
+          )}
+        </Card>
+      </div>
+
+      <Card
+        className={tw(
+          "flex",
+          "h-64",
+          "justify-center",
+          "items-center",
+          "flex-1",
+          "lg:min-h-1/2"
+        )}
+      >
         <BrushChart isDarkMode={isDarkMode} />
       </Card>
     </div>
