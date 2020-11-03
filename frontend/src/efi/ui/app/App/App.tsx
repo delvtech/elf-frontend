a8b922946afe0e92ddaa413bef083f2dd92be48f
Matthew Brown <mattfdbrown@gmail.com>
2020-11-02 21:52:39 -0800

    lint fix

--- frontend/src/efi/ui/app/App/App.tsx
+++ frontend/src/efi/ui/app/App/App.tsx
@@ -1,5 +1,4 @@
 import React, { FC } from "react";
-import { ReactQueryDevtools } from "react-query-devtools";
 
 import { Classes } from "@blueprintjs/core";
 import { LocationProvider, Router } from "@reach/router";
