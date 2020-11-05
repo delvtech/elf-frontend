d94aeda20fb33b71865481785b67364c6d198ad2
Matthew Brown <mattfdbrown@gmail.com>
2020-11-05 12:26:10 -0800

    add a hook to open/close the crypto drawer

---
+++ frontend/src/efi/ui/crypto/useCryptoDrawer/useCryptoDrawer.ts
@@ -0,0 +1,40 @@
+import { queryCache, useQuery } from "react-query";
+
+interface CryptoDrawer {
+  cryptoDrawerIsOpen: boolean;
+  setCryptoDrawerIsOpen: (isOpen: boolean) => void;
+}
+
+const DRAWER_CRYPTO_IS_OPEN_QUERY_KEY = ["drawer", "crypto", "isOpen"];
+const DRAWER_IS_OPEN_DEFAULT = false;
+
+export function useCryptoDrawer(): CryptoDrawer {
+  const { data } = useQuery<boolean>(
+    DRAWER_CRYPTO_IS_OPEN_QUERY_KEY,
+    () => {
+      const item = window.localStorage.getItem(
+        DRAWER_CRYPTO_IS_OPEN_QUERY_KEY.join()
+      );
+      return item ? JSON.parse(item) : DRAWER_IS_OPEN_DEFAULT;
+    },
+    { placeholderData: DRAWER_IS_OPEN_DEFAULT }
+  );
+
+  const cryptoDrawerIsOpen = data || DRAWER_IS_OPEN_DEFAULT;
+
+  return {
+    cryptoDrawerIsOpen,
+    setCryptoDrawerIsOpen,
+  };
+}
+
+function setCryptoDrawerIsOpen(drawerIsOpen: boolean) {
+  // Save to local storage
+  window.localStorage.setItem(
+    DRAWER_CRYPTO_IS_OPEN_QUERY_KEY.join(),
+    JSON.stringify(drawerIsOpen)
+  );
+
+  // Invalidate so callers will re-ensure the data as needed
+  queryCache.invalidateQueries(DRAWER_CRYPTO_IS_OPEN_QUERY_KEY);
+}
