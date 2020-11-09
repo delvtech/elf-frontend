4fd701f66a74ab7d4b6168d7a49c383610e378cb
Danny Delott <dannydelott@gmail.com>
2020-11-09 10:40:42 -0800

    Add tailwind configuration for min/max height

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -14,10 +14,22 @@ module.exports = {
     minHeight: {
       0: "0",
       "1/4": "25%",
+      "1/3": "33%",
+      "2/3": "66%",
       "1/2": "50%",
       "3/4": "75%",
       full: "100%",
     },
+    maxHeight: {
+      0: "0",
+      "1/4": "25%",
+      "1/3": "33%",
+      "2/3": "66%",
+      "1/2": "50%",
+      "3/4": "75%",
+      full: "100%",
+    },
+
     colors: {
       // these can be used as 'text-primary' etc.
       primary: "var(--bp3-intent-primary)",
