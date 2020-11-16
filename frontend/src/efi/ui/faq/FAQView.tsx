c94e03c7467ed9a24e038cba9e52a7265aeb72bc
Matthew Brown <mattfdbrown@gmail.com>
2020-11-16 13:19:56 -0800

    fix typos

--- frontend/src/efi/ui/faq/FAQView.tsx
+++ frontend/src/efi/ui/faq/FAQView.tsx
@@ -25,19 +25,19 @@ export const FAQView: FC<InvestViewProps> = () => {
   return (
     <div className={investViewClassName}>
       <div className={sectionClassName}>
-        <H2>{t`What is ${"Element Finance"}?`}</H2>
+        <H2>{t`What is Element Finance?`}</H2>
         <p>
           {t`Element Finance offers new and simple ways to invest in the decentralized finance (DeFi) space.
-          There are many opportunites for investing in DeFi and the landscape can change very quickly.  It often
+          There are many opportunities for investing in DeFi and the landscape can change very quickly.  It often
           requires many steps to aquire the correct assets to invest in different strategies.  Our smart contracts
           accept a few basic staking assets and handle the rest of the transactions.  We invest through several
           pools, delegated vaults and automated money markets, (AMMs) to find the best returns.  On top of that, we
-          automatically rebalance funds to keep collatorization ratios healthy.`}
+          automatically rebalance funds to keep collateralization ratios healthy.`}
         </p>
         <br />
       </div>
       <div className={sectionClassName}>
-        <H2>{t`Why use ${"Element Finance"}?`}</H2>
+        <H2>{t`Why use Element Finance?`}</H2>
         <br />
         <H3>{t`Ease of use`}</H3>
         <p>
@@ -55,20 +55,15 @@ export const FAQView: FC<InvestViewProps> = () => {
         <br />
         <H3>{t`Automated rebalancing`}</H3>
         <p>
-          {t`Element Finance allows users access to a variety of investment strategies while maintaing the ownership
-          of the asset they invest with.
-          There are many opportunites for investing in DeFi and the landscape can change very quickly.  It often
-          requires many steps to aquire the correct assets to invest in different strategies.  Our smart contracts
-          accept a few basic staking assets and handle the rest of the transactions.  We invest through several
-          pools, delegated vaults and automated money markets, (AMMs) to find the best returns.  On top of that, we
-          automatically rebalance funds to keep collatorization ratios healthy.`}
+          {t`One of risks associated with DeFi is maintaining a proper collatorization ratio when borrowing funds to prevent liquidation.
+          Element Finance contracts will automatically rebalance funds to make sure that all positions maintain a healthy ratio.`}
         </p>
         <br />
         <br />
         <H3>{t`Community first`}</H3>
         <p>
           {t`Our team believes strongly in the community of the DeFi space.  It is a core part of our mission to have
-          our protocols be goverenend by a decentralized autonomous organization (DAO).  This means that anyone who uses the
+          our protocols be goverend by a decentralized autonomous organization (DAO).  This means that anyone who uses the
           protocols we create to be able to have a say in how they operate, what the investing strategies are, and updates to
           the protocol itself.  We are hard at work setting this up.  `}
         </p>
@@ -85,7 +80,7 @@ export const FAQView: FC<InvestViewProps> = () => {
       <div className={sectionClassName}>
         <H2>{t`What are the risks?`}</H2>
         <p>
-          {t`As with any investment, there are always inherint risks.  DeFi is quickly evolving and the assets can be volitile
+          {t`As with any investment, there are always inherit risks.  DeFi is quickly evolving and the assets can be volatile
           on a daily basis.  We use our knowledge to offer a range of strategies that vary in risk.  Ultimately though the
           decision is yours to make and we always suggest research the risks involved with DeFi investing.
           `}
