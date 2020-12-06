da8a3bc7a0b47337845245316c8d0383a96da614
Matthew Brown <mattfdbrown@gmail.com>
2020-12-05 18:05:49 -0800

    refactor useTokenBalance

--- frontend/src/efi-ui/token/hooks/useTokenBalance/useTokenBalance.ts
+++ frontend/src/efi-ui/token/hooks/useTokenBalance/useTokenBalance.ts
@@ -1,36 +1,41 @@
-import { useTokenBalanceOf } from "efi-ui/token/hooks/useTokenBalanceOf";
-import { useTokenDecimals } from "efi-ui/token/hooks/useTokenDecimals";
-import { TokenContractSymbols } from "efi/crypto/TokenContractSymbols";
-import { BigNumber } from "ethers";
 import { QueryResult } from "react-query";
 
-export interface UseTokenBalanceResult {
-  /**
-   * the fractional unit value of the token.  i.e. wei
-   */
-  value: QueryResult<BigNumber | undefined>;
+import { BigNumber } from "ethers";
 
-  /**
-   * the number of decimal places between the fractional unit value to the basic unit value.
-   */
-  decimals: QueryResult<number | undefined>;
-}
+import { useTokenBalanceOf } from "efi-ui/token/hooks/useTokenBalanceOf";
+import { useTokenDecimals } from "efi-ui/token/hooks/useTokenDecimals";
+import { TokenBalance } from "efi/crypto/TokenBalance";
+import { TokenContractSymbols } from "efi/crypto/TokenContractSymbols";
 
 /**
  * Gets the ERC20 token balance for the prodvided account address and the number of decimals.
  *
- * @param {SupportedERC20StakingAssets} name 'name of ERC20 token to get a user's balance of'
+ * @param {TokenContractSymbols} name 'name of ERC20 token to get a user's balance of'
  * @param {string} account {string} 'user's account address.
  */
 export function useTokenBalance(
   name: TokenContractSymbols,
   account: string | null | undefined
-): UseTokenBalanceResult {
-  const value = useTokenBalanceOf(name, account);
-  const decimals = useTokenDecimals(name);
+): [
+  TokenBalance | undefined,
+  QueryResult<BigNumber | undefined>,
+  QueryResult<number | undefined>
+] {
+  const valueResult = useTokenBalanceOf(name, account);
+  const decimalsResult = useTokenDecimals(name);
+  const value = valueResult.data;
+  const decimals = decimalsResult.data;
 
-  return {
-    value,
-    decimals,
-  };
+  if (value === undefined || decimals === undefined) {
+    return [undefined, valueResult, decimalsResult];
+  }
+
+  return [
+    {
+      value,
+      decimals: BigNumber.from(decimals),
+    },
+    valueResult,
+    decimalsResult,
+  ];
 }
