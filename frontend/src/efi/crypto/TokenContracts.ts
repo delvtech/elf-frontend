4c74e57b95b904e0295663f66b0ef4c14c1ebaf6
Danny Delott <dannydelott@gmail.com>
2020-12-14 19:54:59 -0800

    Use erc20 abi from efi-contracts

--- frontend/src/efi/crypto/TokenContracts.ts
+++ frontend/src/efi/crypto/TokenContracts.ts
@@ -1,7 +1,7 @@
 import { Erc20 } from "elf-contracts/types/Erc20";
+import erc20abi from "elf-contracts/contracts/ERC20.json";
 import { Contract } from "ethers";
 
-import erc20abi from "efi/crypto/erc20abi.json";
 import { jsonRpcProvider } from "efi/providers/jsonRpcProviders";
 
 import {
@@ -10,13 +10,13 @@ import {
 } from "./TokenContractAddresses";
 import { TokenContractSymbols } from "./TokenContractSymbols";
 
-const WethContract = new Contract(
+export const WethContract = new Contract(
   wethAddress,
   erc20abi,
   jsonRpcProvider
 ) as Erc20;
 
-const UsdcContract = new Contract(
+export const UsdcContract = new Contract(
   USDC_CONTRACT_ADDRESS_MAINNET,
   erc20abi,
   jsonRpcProvider
