91e4903fc19fcf21b88f5180757f3b841f5b1763
Matthew Brown <mattfdbrown@gmail.com>
2020-12-14 16:57:56 -0800

    change color of icon, add back button

--- frontend/src/efi-ui/wallets/ConnectWalletView/ConnectWalletView.tsx
+++ frontend/src/efi-ui/wallets/ConnectWalletView/ConnectWalletView.tsx
@@ -1,6 +1,9 @@
 import React, { FC } from "react";
 
+import { Button } from "@blueprintjs/core";
+import { IconNames } from "@blueprintjs/icons";
 import { RouteComponentProps } from "@reach/router";
+import { t } from "ttag";
 
 import tw from "efi-tailwindcss-classnames";
 import { ConnectWalletState } from "efi-ui/wallets/ConnectWalletState/ConnectWalletState";
@@ -8,16 +11,24 @@ import { ConnectWalletState } from "efi-ui/wallets/ConnectWalletState/ConnectWal
 interface ConnectWalletViewProps extends RouteComponentProps {}
 export const ConnectWalletView: FC<ConnectWalletViewProps> = () => {
   return (
-    <div
-      className={tw(
-        "flex",
-        "h-full",
-        "w-full",
-        "justify-center",
-        "items-center"
-      )}
-    >
-      <ConnectWalletState />
+    <div className={tw("flex", "flex-col", "p-12", "items-center")}>
+      <Button
+        icon={IconNames.CHEVRON_LEFT}
+        minimal
+        outlined
+        onClick={() => window.history.back()}
+      >{t`Go back`}</Button>
+      <div
+        className={tw(
+          "flex",
+          "h-full",
+          "w-full",
+          "justify-center",
+          "items-center"
+        )}
+      >
+        <ConnectWalletState />
+      </div>
     </div>
   );
 };
