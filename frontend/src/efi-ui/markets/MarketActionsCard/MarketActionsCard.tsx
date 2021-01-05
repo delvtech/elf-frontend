39b36dc1d90209cb6ac6fc3c6a63e5992e7d08c8
Matthew Brown <mattfdbrown@gmail.com>
2021-01-05 10:46:33 -0800

    add some non-ideal state

--- frontend/src/efi-ui/markets/MarketActionsCard/MarketActionsCard.tsx
+++ frontend/src/efi-ui/markets/MarketActionsCard/MarketActionsCard.tsx
@@ -1,10 +1,11 @@
 import React, { FC } from "react";
 
-import { Card, Tab, Tabs } from "@blueprintjs/core";
+import { Button, Card, NonIdealState, Tab, Tabs } from "@blueprintjs/core";
 
 import tw from "efi-tailwindcss-classnames";
 import { Pool } from "efi/pools/Pool";
 import { t } from "ttag";
+import { IconNames } from "@blueprintjs/icons";
 
 interface MarketActionsCardProps {
   pool: Pool;
@@ -14,8 +15,30 @@ export const MarketActionsCard: FC<MarketActionsCardProps> = ({ pool }) => {
   return (
     <Card className={tw("flex", "flex-col", "w-full", "transition-all")}>
       <Tabs id="market-actions">
-        <Tab id="trade" title={t`Trade`} panel={<div />} />
-        <Tab id="stake" title={t`Stake`} panel={<div />} />
+        <Tab
+          id="trade"
+          title={t`Trade`}
+          panel={
+            <NonIdealState
+              icon={IconNames.EXCHANGE}
+              description={t`Trade under construction`}
+              action={<Button outlined disabled>{t`Trade coming soon`}</Button>}
+            />
+          }
+        />
+        <Tab
+          id="stake"
+          title={t`Stake`}
+          panel={
+            <NonIdealState
+              icon={IconNames.SERIES_ADD}
+              description={t`Staking under construction`}
+              action={
+                <Button outlined disabled>{t`Staking coming soon`}</Button>
+              }
+            />
+          }
+        />
       </Tabs>
     </Card>
   );
