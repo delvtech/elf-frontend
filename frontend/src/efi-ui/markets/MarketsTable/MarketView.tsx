8afaf455e6cb9a40856630ce30fcc467426fda72
Matthew Brown <mattfdbrown@gmail.com>
2021-01-05 09:52:54 -0800

    remove WalletPane
    this takes up too much space for what its worth.

--- frontend/src/efi-ui/markets/MarketsTable/MarketView.tsx
+++ frontend/src/efi-ui/markets/MarketsTable/MarketView.tsx
@@ -1,18 +1,30 @@
 import React, { FC } from "react";
 
 import { Classes, H2 } from "@blueprintjs/core";
+import { Web3Provider } from "@ethersproject/providers";
 import { RouteComponentProps } from "@reach/router";
+import { useWeb3React } from "@web3-react/core";
 import classNames from "classnames";
 import { t } from "ttag";
 
 import tw from "efi-tailwindcss-classnames";
 import { MarketCard } from "efi-ui/markets/MarketsTable/MarketCard/MarketCard";
-import WalletSummaryPane from "efi-ui/wallets/WalletSummaryPane/WalletSummaryPane";
+import { WalletConnectionCard } from "efi-ui/wallets/WalletConnectionCard/WalletConnectionCard";
 import { ElfStrategyLowRisk } from "efi/pools/lowRisk";
+import { getConnectorName } from "efi/wallets/connectors";
 
 interface MarketViewProps extends RouteComponentProps {}
 
 export const MarketView: FC<MarketViewProps> = () => {
+  const {
+    active,
+    account,
+    chainId,
+    connector,
+    library,
+  } = useWeb3React<Web3Provider>();
+
+  const connectorName = getConnectorName(connector, library);
   return (
     <div
       className={tw("flex", "p-12", "h-full", "space-x-12", "overflow-scroll")}
@@ -20,31 +32,26 @@ export const MarketView: FC<MarketViewProps> = () => {
       {/* Main content */}
       <div className={tw("flex", "flex-col", "flex-1", "space-y-12")}>
         {/* page title */}
-        <div className={tw("flex", "flex-col", "justify-start")}>
-          <H2 className={tw("mb-4")}>{t`Element Market`}</H2>
-          <span
-            className={classNames(
-              Classes.RUNNING_TEXT,
-              Classes.TEXT_MUTED,
-              tw("text-base")
-            )}
-          >{t`Provide liquidity for this market, or trade for what you want.`}</span>
+        <div className={tw("flex", "justify-between")}>
+          <div className={tw("flex", "flex-col", "justify-start")}>
+            <H2 className={tw("mb-4")}>{t`Element Market`}</H2>
+            <span
+              className={classNames(
+                Classes.RUNNING_TEXT,
+                Classes.TEXT_MUTED,
+                tw("text-base")
+              )}
+            >{t`Provide liquidity for this market, or trade for what you want.`}</span>
+          </div>
+          <WalletConnectionCard
+            active={active}
+            account={account}
+            chainId={chainId}
+            connectorName={connectorName}
+          />
         </div>
 
         <MarketCard pool={ElfStrategyLowRisk} />
-      </div>
-
-      {/* Right hand side */}
-      <div
-        className={tw(
-          "hidden",
-          "lg:block",
-          "h-full",
-          "flex-shrink-0",
-          "w-3/10"
-        )}
-      >
-        <WalletSummaryPane />
       </div>
     </div>
   );
