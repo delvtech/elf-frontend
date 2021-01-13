961a266955a61531edbcc8be366f9499f69448eb
Danny Delott <dannydelott@gmail.com>
2021-01-13 13:22:23 -0800

    Fix css stylesheet order

--- frontend/src/stylesheets.ts
+++ frontend/src/stylesheets.ts
@@ -1,19 +1,22 @@
 // Normalize before everything else
 import "normalize.css/normalize.css";
 
+// Tailwind provides some modern low-level resets that don't need to be compiled
+// into the main tailwind.output.css file.
+import "tailwindcss/dist/base.css";
+
 // custom fonts
 import "@fontsource/open-sans";
 // end custom fonts
-
-// Tailwind provides low-level css utilities
-import "./tailwind.output.css";
-// end Tailwind
 
 // Third party libraries
 import "@blueprintjs/core/lib/css/blueprint.css";
 import "@blueprintjs/icons/lib/css/blueprint-icons.css";
 import "@blueprintjs/select/lib/css/blueprint-select.css";
 // end Third party libraries
+
+// Tailwind compiled
+import "./tailwind.output.css";
 
 // Our CSS
 import "./variables.css";
