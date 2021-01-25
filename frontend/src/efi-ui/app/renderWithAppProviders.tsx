6bcaab053b517dd1fe031cc6fec1a54fcf861f2f
Matthew Brown <mattfdbrown@gmail.com>
2021-01-25 13:47:40 -0800

    tweaks to renderWithAppProviders

--- frontend/src/efi-ui/app/renderWithAppProviders.tsx
+++ frontend/src/efi-ui/app/renderWithAppProviders.tsx
@@ -1,24 +1,39 @@
 import React, { ReactNode } from "react";
+import { QueryClient, QueryClientProvider } from "react-query";
+
 import {
   createHistory,
   createMemorySource,
   LocationProvider,
 } from "@reach/router";
 import { render } from "@testing-library/react";
-import { QueryClientProvider } from "react-query";
-import { efiQueryClient } from "efi/queryClient";
-import { getEthereumProviderLibrary } from "efi/wallets/providers";
 import { Web3ReactProvider } from "@web3-react/core";
 
+import { getEthereumProviderLibrary } from "efi/wallets/providers";
+
+const defaultQueryClient = new QueryClient();
+
+interface ProviderOptions {
+  route: string;
+  queryClient: QueryClient;
+}
+
+const defaultOptions: ProviderOptions = {
+  route: "/",
+  queryClient: defaultQueryClient,
+};
 export function renderWithAppProviders(
-  ui: ReactNode,
-  { route = "/", history = createHistory(createMemorySource(route)) } = {}
+  // unit under test
+  uut: ReactNode,
+  options?: Partial<ProviderOptions>
 ) {
+  const { route, queryClient } = { ...defaultOptions, ...options };
+  const history = createHistory(createMemorySource(route));
   return {
     ...render(
       <Web3ReactProvider getLibrary={getEthereumProviderLibrary}>
-        <QueryClientProvider client={efiQueryClient}>
-          <LocationProvider history={history}>{ui}</LocationProvider>
+        <QueryClientProvider client={queryClient}>
+          <LocationProvider history={history}>{uut}</LocationProvider>
         </QueryClientProvider>
       </Web3ReactProvider>
     ),
