b83fd0f3224a438ff8aec875a3e457013c19ca0a
Matthew Brown <mattfdbrown@gmail.com>
2021-01-25 12:58:21 -0800

    add web3react provider

--- frontend/src/efi-ui/app/renderWithAppProviders.tsx
+++ frontend/src/efi-ui/app/renderWithAppProviders.tsx
@@ -7,6 +7,8 @@ import {
 import { render } from "@testing-library/react";
 import { QueryClientProvider } from "react-query";
 import { efiQueryClient } from "efi/queryClient";
+import { getEthereumProviderLibrary } from "efi/wallets/providers";
+import { Web3ReactProvider } from "@web3-react/core";
 
 export function renderWithAppProviders(
   ui: ReactNode,
@@ -14,9 +16,11 @@ export function renderWithAppProviders(
 ) {
   return {
     ...render(
-      <QueryClientProvider client={efiQueryClient}>
-        <LocationProvider history={history}>{ui}</LocationProvider>
-      </QueryClientProvider>
+      <Web3ReactProvider getLibrary={getEthereumProviderLibrary}>
+        <QueryClientProvider client={efiQueryClient}>
+          <LocationProvider history={history}>{ui}</LocationProvider>
+        </QueryClientProvider>
+      </Web3ReactProvider>
     ),
     // adding `history` to the returned utilities to allow us
     // to reference it in our tests (just try to avoid using
