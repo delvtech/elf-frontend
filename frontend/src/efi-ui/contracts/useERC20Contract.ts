ddfb9307d872b590bc2d48147ac31d881bbd18ff
Matthew Brown <mattfdbrown@gmail.com>
2021-02-12 14:39:37 -0800

    allow undefined address for useERC20Contract
    have to handle this case, sometimes the address isn't available yet.

--- frontend/src/efi-ui/contracts/useERC20Contract.ts
+++ frontend/src/efi-ui/contracts/useERC20Contract.ts
@@ -1,27 +1,29 @@
 import { useMemo } from "react";
 
 import { Provider } from "@ethersproject/providers";
+import { ERC20 } from "elf-contracts/types/ERC20";
 import { Signer } from "ethers";
 
-import { jsonRpcProvider } from "efi/providers/jsonRpcProviders";
 import { getERC20Contract } from "efi/contracts/getERC20Contract";
+
 /**
- * Returns a new instance of the ERC20 contract.  This should only be used for contracts that act as
- * basic ERC20 tokens as no extra methods will be available.
- * @param { string } address the ERC20 contract address.
+ * Returns a new instance of the ERC20 contract if an address is supplied.  This should only be used
+ * for contracts that act as basic ERC20 tokens as no extra methods will be available.
+ * @param { string | undefined } address the ERC20 contract address.
  * @param { Signer | Provider } signerOrProvider optionally pass a signer if transactions need to be
  * signed. A default provider is supplied to allow read operations.
+ * @returns { ERC20 | undefined }
  */
 export function useERC20Contract(
-  address: string,
+  address: string | undefined,
   signerOrProvider?: Signer | Provider
-) {
-  const signer = signerOrProvider ?? jsonRpcProvider;
-
-  const erc20Contract = useMemo(() => getERC20Contract(address, signer), [
-    address,
-    signer,
-  ]);
+): ERC20 | undefined {
+  const erc20Contract = useMemo(() => {
+    if (!address) {
+      return;
+    }
+    return getERC20Contract(address, signerOrProvider);
+  }, [address, signerOrProvider]);
 
   return erc20Contract;
 }
