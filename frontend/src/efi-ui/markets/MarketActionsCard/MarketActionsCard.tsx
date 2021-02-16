9b86a3be1ff9871ea1e94b6f71f60ba7aa1145d7
Matthew Brown <mattfdbrown@gmail.com>
2021-02-16 08:00:20 -0800

    show different UI for Stake

--- frontend/src/efi-ui/markets/MarketActionsCard/MarketActionsCard.tsx
+++ frontend/src/efi-ui/markets/MarketActionsCard/MarketActionsCard.tsx
@@ -1,4 +1,4 @@
-import React, { FC } from "react";
+import React, { FC, useCallback, useState } from "react";
 
 import { Card, Colors, Intent } from "@blueprintjs/core";
 import { BigNumber } from "ethers";
@@ -13,35 +13,61 @@ interface MarketActionsCardProps {
   market: Market;
 }
 
+type MarketAction = "trade" | "stake";
 export const MarketActionsCard: FC<MarketActionsCardProps> = ({ market }) => {
+  const [action, setActionUI] = useState<MarketAction>("trade");
+  const showTradeUI = useCallback(() => setActionUI("trade"), []);
+  const showStakeUI = useCallback(() => setActionUI("stake"), []);
+
   return (
     <div className={tw("flex", "flex-col", "flex-1", "h-500", "w-3/10")}>
       <div className={tw("mb-2", "space-x-4")}>
         <button
           aria-label="trade"
+          onClick={showTradeUI}
           style={{ color: Colors.BLUE5 }}
         >{t`Trade`}</button>
-        <button aria-label="stake">{t`Stake`}</button>
+        <button onClick={showStakeUI} aria-label="stake">{t`Stake`}</button>
       </div>
       <Card
         className={tw("flex", "flex-col", "flex-1", "w-full", "transition-all")}
       >
-        <TradePanel
-          inputLabel={"Trade"}
-          buttonLabel={"Trade"}
-          buttonIntent={Intent.PRIMARY}
-          tradeCryptoSymbol={"ETH"}
-          tradeCryptoBalance={{
-            value: BigNumber.from(5000000000),
-            decimals: BigNumber.from(9),
-          }}
-          receiveCryptoSymbol={"fyETH" as CryptoSymbolOld}
-          receiveCryptoBalance={{
-            value: BigNumber.from(0),
-            decimals: BigNumber.from(9),
-          }}
-          onTransaction={() => {}}
-        />
+        {action === "trade" && (
+          <TradePanel
+            inputLabel={"Trade"}
+            buttonLabel={"Trade"}
+            buttonIntent={Intent.PRIMARY}
+            tradeCryptoSymbol={"ETH"}
+            tradeCryptoBalance={{
+              value: BigNumber.from(5000000000),
+              decimals: BigNumber.from(9),
+            }}
+            receiveCryptoSymbol={"fyETH" as CryptoSymbolOld}
+            receiveCryptoBalance={{
+              value: BigNumber.from(0),
+              decimals: BigNumber.from(9),
+            }}
+            onTransaction={() => {}}
+          />
+        )}
+        {action === "stake" && (
+          <TradePanel
+            inputLabel={"Stake"}
+            buttonLabel={"Stake"}
+            buttonIntent={Intent.PRIMARY}
+            tradeCryptoSymbol={"ETH"}
+            tradeCryptoBalance={{
+              value: BigNumber.from(5000000000),
+              decimals: BigNumber.from(9),
+            }}
+            receiveCryptoSymbol={"fyETH" as CryptoSymbolOld}
+            receiveCryptoBalance={{
+              value: BigNumber.from(0),
+              decimals: BigNumber.from(9),
+            }}
+            onTransaction={() => {}}
+          />
+        )}
       </Card>
     </div>
   );
