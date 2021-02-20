93aba7cafae7991774153e3aca0a26f52513e8cc
Matthew Brown <mattfdbrown@gmail.com>
2021-02-20 11:24:40 -0800

    pass down signer

--- frontend/src/efi-ui/markets/MarketDetails/MarketDetails.tsx
+++ frontend/src/efi-ui/markets/MarketDetails/MarketDetails.tsx
@@ -9,13 +9,16 @@ import { MarketHistory } from "efi-ui/markets/MarketHistory/MarketHistory";
 import { MarketSummary } from "efi-ui/markets/MarketSummary/MarketSummary";
 import { TokenSummary } from "efi-ui/markets/TokenSummary/TokenSummary";
 import { useMarketDetails } from "efi-ui/markets/useMarketDetails";
+import { Signer } from "ethers";
 
 interface MarketDetailsProps {
+  signer: Signer | undefined;
   accountAddress: string | null | undefined;
   marketContract: BPool | undefined;
 }
 
 export const MarketDetails: FC<MarketDetailsProps> = ({
+  signer,
   accountAddress,
   marketContract,
 }) => {
@@ -48,8 +51,10 @@ export const MarketDetails: FC<MarketDetailsProps> = ({
           <div className={tw("flex", "space-x-12")}>
             <MarketHistory />
             <MarketActionsCard
+              signer={signer}
               accountAddress={accountAddress}
               market={marketDetails}
+              marketContract={marketContract}
             />
           </div>
         </div>
