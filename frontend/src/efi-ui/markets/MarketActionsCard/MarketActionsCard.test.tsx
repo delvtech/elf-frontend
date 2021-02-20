29abf00d232840e0f93b151ec1a486ef8c42104c
Matthew Brown <mattfdbrown@gmail.com>
2021-02-20 15:43:45 -0800

    lint fixes

--- frontend/src/efi-ui/markets/MarketActionsCard/MarketActionsCard.test.tsx
+++ frontend/src/efi-ui/markets/MarketActionsCard/MarketActionsCard.test.tsx
@@ -5,14 +5,18 @@ import { MarketActionsCard } from "efi-ui/markets/MarketActionsCard/MarketAction
 import { createQueryClient } from "efi/queryClient";
 import { stubbedMarkets } from "efi/markets/stubbedMarkets";
 import ContractAddresses from "efi/contracts/contractsJson";
+import { MockProvider } from "ethereum-waffle";
 
 test("should render two tabs", async () => {
+  const [signer] = new MockProvider().getWallets();
   const queryClient = createQueryClient();
   const { getByRole } = await renderWithClient(
     queryClient,
     <MarketActionsCard
+      signer={signer}
       accountAddress={ContractAddresses.userAddress}
       market={stubbedMarkets[0]}
+      marketContract={undefined}
     />
   );
 
--- frontend/src/efi-ui/markets/MarketDetails/MarketDetails.test.tsx
+++ frontend/src/efi-ui/markets/MarketDetails/MarketDetails.test.tsx
@@ -4,11 +4,12 @@ import { MarketDetails } from "efi-ui/markets/MarketDetails/MarketDetails";
 import { renderWithClient } from "efi-ui/testing/renderWithClient";
 import { createQueryClient } from "efi/queryClient";
 import ContractAddresses from "efi/contracts/contractsJson";
+import { MockProvider } from "ethereum-waffle";
 
 // TODO: add a contract and to extensive testing once we have the fixtures and hardhat added to the
 // repo
 test("should render information about the market", async () => {
-  // const [elementSigner] = new MockProvider().getWallets();
+  const [elementSigner] = new MockProvider().getWallets();
   // const BPOOL_ADDRESS = "0x1234123412341234123412341234123412341234";
   // const marketContract = BPool__factory.connect(BPOOL_ADDRESS, elementSigner);
 
@@ -16,6 +17,7 @@ test("should render information about the market", async () => {
   const { getByText } = await renderWithClient(
     queryClient,
     <MarketDetails
+      signer={elementSigner}
       accountAddress={ContractAddresses.userAddress}
       marketContract={undefined}
     />
