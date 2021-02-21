aa1f6f48ee601683d16f28c042506ac312384b57
Danny Delott <dannydelott@gmail.com>
2021-02-20 19:57:44 -0800

    Refactor hooks over to TrancheInfoButton, simpleify TranchePicker

--- frontend/src/efi-ui/invest/TranchePicker/TranchePicker.tsx
+++ frontend/src/efi-ui/invest/TranchePicker/TranchePicker.tsx
@@ -1,18 +1,10 @@
-import React, { FC, useCallback } from "react";
+import React, { FC } from "react";
 
 import { Select } from "@blueprintjs/select";
-import { Elf__factory } from "elf-contracts/types/factories/Elf__factory";
-import { ERC20__factory } from "elf-contracts/types/factories/ERC20__factory";
 import { Tranche } from "elf-contracts/types/Tranche";
-import { BigNumber } from "ethers";
-import zipWith from "lodash.zipwith";
 
 import tw from "efi-tailwindcss-classnames";
-import { getQueriesData } from "efi-ui/base/queryResults";
-import { useSmartContractReadCalls } from "efi-ui/contracts/useSmartContractReadCalls/useSmartContractReadCalls";
-import { useSmartContractsFromFactory } from "efi-ui/contracts/useSmartContractsFromFactory/useSmartContractsFromFactory";
 
-import { TrancheInfo } from "./TrancheInfo";
 import { TrancheInfoButton } from "./TrancheInfoButton";
 
 interface TranchePickerProps {
@@ -25,105 +17,26 @@ export const TranchePicker: FC<TranchePickerProps> = ({
   onTrancheChange,
   activeTrancheIndex,
 }) => {
-  const trancheSymbolResults = useSmartContractReadCalls(tranches, "symbol");
-  const trancheNameResults = useSmartContractReadCalls(tranches, "name");
-  const trancheUnlockTimestampResults = useSmartContractReadCalls(
-    tranches,
-    "unlockTimestamp"
-  );
-  const elfAddressResults = useSmartContractReadCalls(tranches, "elf");
-  const elfContracts = useSmartContractsFromFactory(
-    getQueriesData(elfAddressResults),
-    Elf__factory.connect
-  );
-  const vaultAddressResults = useSmartContractReadCalls(elfContracts, "vault");
-  const vaultContracts = useSmartContractsFromFactory(
-    getQueriesData(vaultAddressResults),
-    ERC20__factory.connect
-  );
-  const vaultNameResults = useSmartContractReadCalls(vaultContracts, "name");
-
-  const onTrancheInfoChange = useCallback(
-    (trancheInfo: TrancheInfo) => {
-      onTrancheChange(trancheInfo.tranche);
-    },
-    [onTrancheChange]
-  );
-
-  const trancheInfos = makeTrancheInfos(
-    tranches,
-    getQueriesData(trancheSymbolResults),
-    getQueriesData(trancheNameResults),
-    getQueriesData(trancheUnlockTimestampResults),
-    // TODO: stub out apy for now
-    tranches.map(() => 4.13),
-    getQueriesData(vaultNameResults)
-  );
-
   // TODO: Show a loading or disabled state of some kind
-  if (!trancheInfos.length || activeTrancheIndex === undefined) {
+  if (!tranches.length || activeTrancheIndex === undefined) {
     return null;
   }
 
-  const activeTrancheInfo = trancheInfos[activeTrancheIndex];
+  const activeTranche = tranches[activeTrancheIndex];
 
   return (
     <Select
       disabled
       popoverProps={{ minimal: true, targetClassName: tw("w-full") }}
-      items={trancheInfos}
+      items={tranches}
       filterable={false}
       className={tw("w-full", "col-span-2")}
-      itemRenderer={(
-        { name, tranche, apy, symbol, unlockTimestamp, vaultName },
-        { handleClick }
-      ) => (
-        <TrancheInfoButton
-          vaultName={vaultName}
-          tranche={tranche}
-          name={name}
-          apy={apy}
-          symbol={symbol}
-          unlockTimestamp={unlockTimestamp}
-          onClick={handleClick}
-        />
+      itemRenderer={(tranche, { handleClick }) => (
+        <TrancheInfoButton tranche={tranche} onClick={handleClick} />
       )}
-      onItemSelect={onTrancheInfoChange}
+      onItemSelect={onTrancheChange}
     >
-      <TrancheInfoButton
-        tranche={activeTrancheInfo.tranche}
-        name={activeTrancheInfo.name}
-        symbol={activeTrancheInfo.symbol}
-        vaultName={activeTrancheInfo.vaultName}
-        apy={activeTrancheInfo.apy}
-        unlockTimestamp={activeTrancheInfo.unlockTimestamp}
-      />
+      <TrancheInfoButton tranche={activeTranche} />
     </Select>
   );
 };
-function makeTrancheInfos(
-  tranches: Tranche[],
-  trancheSymbols: (string | undefined)[],
-  trancheNames: (string | undefined)[],
-  trancheUnlockTimestamps: (BigNumber | undefined)[],
-  trancheAPYs: (number | undefined)[],
-  vaultNames: (string | undefined)[]
-): TrancheInfo[] {
-  // have to use any until zipWith definition supports more than 5 inputs
-  return zipWith<any, TrancheInfo>(
-    tranches,
-    trancheSymbols,
-    trancheNames,
-    trancheUnlockTimestamps,
-    trancheAPYs,
-    vaultNames,
-    (tranche, symbol, name, unlockTimestamp, apy, vaultName) => ({
-      tranche,
-      symbol,
-      name,
-      unlockTimestamp,
-      apy,
-      vaultName,
-    })
-  );
-}
