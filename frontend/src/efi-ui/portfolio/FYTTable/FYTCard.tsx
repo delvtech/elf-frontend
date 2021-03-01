6346084ab91cb30fc70292615f9a48ede3797578
Danny Delott <dannydelott@gmail.com>
2021-03-01 13:19:41 -0800

    Finish card redesign

--- frontend/src/efi-ui/portfolio/FYTTable/FYTCard.tsx
+++ frontend/src/efi-ui/portfolio/FYTTable/FYTCard.tsx
@@ -6,11 +6,8 @@ import {
   ButtonGroup,
   Callout,
   Card,
-  Classes,
-  H1,
-  H4,
-  H5,
   Icon,
+  Intent,
   ProgressBar,
 } from "@blueprintjs/core";
 import { IconNames } from "@blueprintjs/icons";
@@ -45,16 +42,23 @@ interface FYTCardProps {
   tranche: Tranche;
 }
 
+const calloutClassName = tw(
+  "flex",
+  "flex-col",
+  "flex-1",
+  "h-full",
+  "p-8",
+  "items-center",
+  "justify-center"
+);
 export const FYTCard: FC<FYTCardProps> = ({ account, tranche }) => {
   const { isDarkMode } = useDarkMode();
   const { currency } = useCurrencyPref();
   const { data: trancheSymbol } = useSmartContractReadCall(tranche, "symbol");
-  const { data: trancheName } = useSmartContractReadCall(tranche, "name");
   const { data: unlockTimestamp } = useSmartContractReadCall(
     tranche,
     "unlockTimestamp"
   );
-  console.log("asdf", tranche.address);
   const { data: trancheDecimals } = useSmartContractReadCall(
     tranche,
     "decimals"
@@ -114,75 +118,104 @@ export const FYTCard: FC<FYTCardProps> = ({ account, tranche }) => {
   }
 
   return (
-    <Card
-      style={{ width: 512 }}
-      className={classNames(tw("p-8", "flex", "flex-col", "space-y-8"))}
-    >
-      <div className={tw("flex", "space-x-4")}>
-        <BaseAssetIcon title={baseAssetSymbol} height={72} width={72} />
-        <div className={tw("flex", "flex-col")}>
-          <H4 className={tw("mb-2", "tracking-wide")}>
-            <a
-              title={t`View tranche on etherscan`}
-              href={`https://etherscan.io/address/${tranche.address}`}
-              target="_blank"
-              rel="noreferrer noopener"
-            >
-              {trancheSymbol}
-            </a>
-          </H4>
-          <div className={tw("flex", "space-x-5", "items-center")}>
-            <H1 className={tw("mb-0")} title={trancheBalance.toLocaleString()}>
-              {trancheBalance.toFixed(6)}
-            </H1>
-            <div className={tw("flex", "flex-col")}>
-              <LabeledText
-                bold
-                text={
-                  <div className={tw("flex", "space-x-1")}>
-                    <span>{t`${exitValue.toFixed(6)} ${baseAssetSymbol}`}</span>
-                    <span
-                      className={Classes.TEXT_MUTED}
-                    >{t`(${fiatPrice})`}</span>
-                  </div>
-                }
-                label={t`Current exit value`}
-              />
+    <div>
+      <Card
+        style={{ width: 512 }}
+        className={classNames(
+          tw("p-8", "flex", "flex-col", "space-y-8", "text-base", {
+            "text-gray-700": !isDarkMode,
+            "text-white": isDarkMode,
+          })
+        )}
+      >
+        <div className={tw("flex", "space-x-4")}>
+          <BaseAssetIcon
+            className={tw("flex-shrink-0")}
+            title={baseAssetSymbol}
+            height={72}
+            width={72}
+          />
+          <div className={tw("flex", "flex-col", "space-y-2")}>
+            <span className={tw("text-xl", "font-semibold", "tracking-wide")}>
+              <a
+                title={t`View tranche on etherscan`}
+                href={`https://etherscan.io/address/${tranche.address}`}
+                target="_blank"
+                rel="noreferrer noopener"
+              >
+                {trancheSymbol}
+              </a>
+            </span>
+            <div className={tw("flex", "w-full")}>
+              <div className={tw("flex", "flex-1", "space-x-6", "justify-end")}>
+                <LabeledText
+                  textClassName={tw("text-base")}
+                  text={`${trancheAPY.toFixed(2)}%`}
+                  label={t`yearly`}
+                />
+                <LabeledText
+                  textClassName={tw("text-base")}
+                  text={`${(trancheAPY / 12).toFixed(2)}%`}
+                  label={t`monthly`}
+                />
+                <LabeledText
+                  textClassName={tw("text-base")}
+                  text={`${(trancheAPY / 365).toFixed(2)}%`}
+                  label={t`daily`}
+                />
+              </div>
             </div>
           </div>
         </div>
-      </div>
-      <div>
-        <div className={tw("mb-3")}>
-          <strong>{t`Time to maturity:`}</strong> {timeLeft}
-        </div>
-        <ProgressBar stripes={false} animate={false} value={0.5} />
-      </div>
-      <Callout>
-        <div>
-          <LabeledText
-            text={t`${(trancheAPY / 365).toFixed(2)}% daily`}
-            label={t`${(trancheAPY / 12).toFixed(2)}% monthly`}
-            subLabel={t`${trancheAPY.toFixed(2)}% yearly`}
-          />
-        </div>
-      </Callout>
-      {/* Asset */}
-      <div>
-        <LabeledText text={trancheSymbol} label={jt`via ${tableRowLink}`} />
-      </div>
+        <div className={tw("flex", "flex-col", "space-y-5", "items-center")}>
+          <div className={tw("space-y-2", "w-full")}>
+            <div>
+              <span className={tw("text-base")}>
+                {t`Reaches maturity in`} <strong>{timeLeft}</strong>
+              </span>
+            </div>
+            <ProgressBar stripes={false} animate={false} value={0.5} />
+          </div>
 
-      {/* Quick Actions */}
-      <div className={tw("flex", "flex-col", "h-full", "w-full", "space-y-2")}>
-        <Button large outlined>{t`Stake`}</Button>
-        <ButtonGroup fill>
+          <Callout className={calloutClassName}>
+            <span
+              className={classNames(tw("text-base", "mb-0"))}
+            >{t`Total balance`}</span>
+            <LabeledText
+              muted={false}
+              className={tw("flex", "justify-center", "items-center")}
+              bold
+              textClassName={tw("text-2xl")}
+              text={`${trancheBalance.toFixed(6)} FYT`}
+              label={t`1 FYT = 1 ${baseAssetSymbol} at maturity`}
+            />
+          </Callout>
+          <Callout icon={null} className={calloutClassName}>
+            <span
+              className={classNames(tw("text-base", "mb-0"))}
+            >{t`Current exit value`}</span>
+            <LabeledText
+              bold
+              muted={false}
+              className={tw("flex", "justify-center", "items-center")}
+              textClassName={tw("text-2xl")}
+              text={
+                <span>{t`${exitValue.toFixed(6)} ${baseAssetSymbol}`}</span>
+              }
+              label={fiatPrice}
+            />
+          </Callout>
+        </div>
+
+        {/* Quick Actions */}
+        <ButtonGroup className={tw("space-x-6")}>
           <Tooltip2
             inheritDarkTheme={false}
             content={t`This asset can be claimed after it has reached maturity.`}
           >
             <AnchorButton
-              large
-              outlined
+              fill
+              minimal
               disabled={
                 /*
                  * See Blueprint docs, we have to use an AnchorButton for a11y
@@ -191,18 +224,30 @@ export const FYTCard: FC<FYTCardProps> = ({ account, tranche }) => {
                 true
               }
             >
-              {t`Claim`}
+              <div className={tw("p-2", "text-base")}>{t`Claim`}</div>
             </AnchorButton>
           </Tooltip2>
-          <Button large outlined>{t`Sell`}</Button>
+          <Button fill minimal intent={Intent.PRIMARY}>
+            <div className={tw("p-2", "text-base")}>{t`Sell`}</div>
+          </Button>
+          <Button fill minimal intent={Intent.PRIMARY}>
+            <div className={tw("p-2", "text-base")}>{t`Stake`}</div>
+          </Button>
+          <AnchorButton
+            intent={Intent.PRIMARY}
+            onClick={() => navigate(`exchange/${market?.address}`)}
+            minimal
+          >
+            <div className={tw("p-2", "text-base")}>{t`Go to market`}</div>
+          </AnchorButton>
         </ButtonGroup>
-        <AnchorButton
-          onClick={() => navigate(`exchange/${market?.address}`)}
-          large
-          minimal
-        >{t`Go to market`}</AnchorButton>
-      </div>
-    </Card>
+        <div className={tw("flex", "justify-center")}>
+          <span>
+            {jt`Fixed yield is backed by ${baseAssetSymbol} deposited in ${tableRowLink}`}
+          </span>
+        </div>
+      </Card>
+    </div>
   );
 };
 
