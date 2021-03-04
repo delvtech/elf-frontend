bdd8eeee4a1c0f9044fe2e6426c7f34aea74ec63
Danny Delott <dannydelott@gmail.com>
2021-03-04 11:59:09 -0800

    Copy contracts into contracts folder

--- testnet/scripts/load-elf-contracts.sh
+++ testnet/scripts/load-elf-contracts.sh
@@ -20,7 +20,7 @@ mkdir src/contracts
 # elf-contracts before I can sync up.
 
 echo "Copying latest contracts..."
-cp -R elf-contracts/contracts/ src/
+cp -R elf-contracts/contracts/ src/contracts/
 
 # remove test contracts, these are used only for elf-contracts tests
 rm -rf src/contracts/test
