1e14ed3ad10937e293c97c908c59bd200886cc23
Matthew Brown <mattfdbrown@gmail.com>
2021-03-03 16:33:47 -0800

    cleanup initializeYieldPool

--- testnet/src/scripts/initializeYieldPool.ts
+++ testnet/src/scripts/initializeYieldPool.ts
@@ -11,26 +11,13 @@ export async function initializeYieldPool(
   elementSigner: Signer,
   vaultContract: Vault,
   baseAssetContract: WETH | USDC,
-  trancheWethContract: Tranche
+  trancheContract: Tranche
 ) {
   const elementAddress = await elementSigner.getAddress();
-  let [tokens, balances] = await vaultContract.getPoolTokens(poolId);
-  console.log("baseAssetContract", baseAssetContract.address);
-  console.log("trancheWethContract", trancheWethContract.address);
-  console.log("baseAssetContract", await baseAssetContract.name());
-  console.log("trancheWethContract", await trancheWethContract.name());
+  let [tokens] = await vaultContract.getPoolTokens(poolId);
 
-  console.log("Pool tokens should have zero balances.");
-  console.log("tokens", tokens);
-  console.log("balances", balances);
-  const weth = await hre.ethers.getContractAt("WETH", tokens[0]);
-  const fyweth = await hre.ethers.getContractAt("WETH", tokens[0]);
-  const wethName = await weth.name();
-  const fyName = await fyweth.name();
-  console.log("wethName", wethName);
-  console.log("fyName", fyName);
-
-  const maxAmountsIn = [parseEther("9500"), parseEther("10000")];
+  // TODO: make amounts configurable
+  const maxAmountsIn = [parseEther("20000"), parseEther("20000")];
   const amounts = maxAmountsIn.map((amt) => amt.toHexString());
 
   // Whether or not to use balances held in balancer.  Since The Vault has nothing, set this to false.
@@ -38,14 +25,12 @@ export async function initializeYieldPool(
 
   // Allow balancer pool to take user's fyt and base tokens
   await baseAssetContract.approve(vaultContract.address, MAX_ALLOWANCE);
-  await trancheWethContract.approve(vaultContract.address, MAX_ALLOWANCE);
+  await trancheContract.approve(vaultContract.address, MAX_ALLOWANCE);
 
   // Balancer V2 vault allows userData as a way to pass props through to pool contracts.  In our
   // case we need to pass the maxAmountsIn.
   const userData = abi.rawEncode(["uint256", "uint256"], amounts);
 
-  // const encodedPoolId: Buffer = abi.rawEncode(["bytes32"], [poolId]);
-  // console.log("encodedPoolId", encodedPoolId);
   const joinReceipt = await vaultContract.joinPool(
     poolId,
     elementAddress,
@@ -56,15 +41,5 @@ export async function initializeYieldPool(
     userData
   );
   await joinReceipt.wait(1);
-
-  // check price
-  // marketWethFYTV2Contract.onSwapGivenIn(tuple, uint256, uint256);
-
-  [tokens, balances] = await vaultContract.getPoolTokens(poolId);
-
-  console.log("Pool tokens should have initial balances.");
-  console.log("tokens", tokens);
-  console.log("balances", balances);
-
   return joinReceipt;
 }
