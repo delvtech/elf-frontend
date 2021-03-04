13943df2a527fcb805e31f47614b5561d5e811a5
Matthew Brown <mattfdbrown@gmail.com>
2021-03-03 21:22:33 -0800

    add yearn vault contract
    had to add this contract.  need to have elf-contracts pass this down as
    a mock

---
+++ testnet/src/unsyncedContracts/AYVault.sol
@@ -0,0 +1,61 @@
+// SPDX-License-Identifier: Apache-2.0
+pragma solidity ^0.8.0;
+
+import "../contracts/interfaces/IERC20.sol";
+import "../contracts/interfaces/YearnVaultV2.sol";
+
+import "../contracts/libraries/ERC20.sol";
+import "../contracts/libraries/Address.sol";
+import "../contracts/libraries/SafeERC20.sol";
+
+import "./libraries/AToken.sol";
+
+contract AYVault is ERC20 {
+    using SafeERC20 for IERC20;
+    using Address for address;
+
+    address public token;
+    uint256 internal _supply;
+
+    constructor(address _token) ERC20("a ytoken", "yToken") {
+        token = _token;
+    }
+
+    function deposit(uint256 _amount, address destination)
+        external
+        returns (uint256)
+    {
+        uint256 _shares = (_amount * 1e18) / pricePerShare(); // calculate shares
+        IERC20(token).safeTransferFrom(msg.sender, address(this), _amount); // pull deposit from sender
+        _mint(destination, _shares); // mint shares for sender
+        _supply += _shares;
+        return _shares;
+    }
+
+    function withdraw(
+        uint256 _shares,
+        address destination,
+        uint256 maxLoss
+    ) external returns (uint256) {
+        uint256 _amount = (_shares * pricePerShare()) / 1e18;
+        _burn(msg.sender, _shares);
+        _supply -= _shares;
+        IERC20(token).safeTransfer(destination, _amount);
+        return _amount;
+    }
+
+    function pricePerShare() public view returns (uint256) {
+        uint256 balance = ERC20(token).balanceOf(address(this));
+        if (balance == 0) return 1e18;
+        return (balance * 1e18) / totalSupply();
+    }
+
+    function updateShares() external {
+        uint256 balance = ERC20(token).balanceOf(address(this));
+        AToken(token).mint(address(this), balance / 10);
+    }
+
+    function totalSupply() public view returns (uint256) {
+        return _supply;
+    }
+}
