b5656486aec68b854f83dd7fa0eb088e862675cf
Danny Delott <dannydelott@gmail.com>
2021-03-07 15:12:40 -0800

    Remove TODO

--- frontend/src/efi-ui/pools/useAllPools/useAllPools.ts
+++ frontend/src/efi-ui/pools/useAllPools/useAllPools.ts
@@ -15,22 +15,5 @@ export function useAllPools(
     signerOrProvider
   );
 
-  // TODO: Use this factory based approach once the YVaultAssetProxyFactory exists
-  // 'args' are the args passed to the 'NewBPool' event.  The second one is the address of the new
-  // pool.
-  // const bPoolAddresses = data.map<string>((result) => result.args?.[1]);
-  // const signer = signerOrProvider ?? jsonRpcProvider;
-  // const bPoolContracts = bPoolAddresses.map((address) =>
-  //   BPool__factory.connect(address, signer)
-  // );
   return [fyWethPool];
 }
-
-// TODO: Use this factory based approach once the YVaultAssetProxyFactory exists
-// function fetchMarketContractAddresses(signerOrProvider?: Signer | Provider) {
-//   const signer = signerOrProvider ?? jsonRpcProvider;
-//   const { elementAddress, bFactoryAddress } = ContractAddresses;
-//   const bFactoryContract = BFactory__factory.connect(bFactoryAddress, signer);
-//   const filter = bFactoryContract.filters.LOG_NEW_POOL(elementAddress, null);
-//   return bFactoryContract.queryFilter(filter);
-// }
