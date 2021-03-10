f17449d11b74869fa31dc122e7abf6fcd23055af
Danny Delott <dannydelott@gmail.com>
2021-03-09 22:19:03 -0800

    Deprecate old Markets Table

--- frontend/src/efi-ui/markets/MarketsTable/MarketsTable.tsx
+++ frontend/src/efi-ui/markets/MarketsTable/MarketsTable.tsx
@@ -1,5 +1,4 @@
-import React, { FC, useState } from "react";
-import { useInterval } from "react-use";
+import React, { FC } from "react";
 
 import {
   Alignment,
@@ -13,18 +12,14 @@ import {
   Tag,
 } from "@blueprintjs/core";
 import { IconNames } from "@blueprintjs/icons";
-import { Link } from "@reach/router";
 import { t } from "ttag";
 
 import tw from "efi-tailwindcss-classnames";
 import { FormGroupLabel } from "efi-ui/base/FormGroupLabel/FormGroupLabel";
-import { LabeledProgressBar } from "efi-ui/base/LabeledProgressBar/LabeledProgressBar";
-import { LabeledText } from "efi-ui/base/LabeledText/LabeledText";
-import { getTimeLeft } from "efi/base/time";
-import { Market } from "efi/markets/Market";
 import { useMarketContracts } from "efi-ui/markets/useMarketContracts";
-import { useMarkets } from "efi-ui/markets/useMarkets";
-import { QueryStatus } from "react-query";
+import { useMarketsOld } from "efi-ui/markets/useMarkets";
+
+import { MarketsTableRowOld } from "./MarketsTableRow";
 
 interface MarketsTableProps {
   className?: string;
@@ -44,9 +39,12 @@ const TABLE_HEADERS: MarketsTableHeaderProps[] = [
   { label: t`Tranche State` },
 ];
 
-export const MarketsTable: FC<MarketsTableProps> = ({ className }) => {
+/**
+ * @deprecated BPools are deprecated. Use PoolsTable instead
+ */
+export const MarketsTableOld: FC<MarketsTableProps> = ({ className }) => {
   const marketContracts = useMarketContracts();
-  const marketResults = useMarkets(marketContracts);
+  const marketResults = useMarketsOld(marketContracts);
 
   if (!marketResults.length) {
     return <span>{t`no markets found`}</span>;
@@ -107,7 +105,7 @@ export const MarketsTable: FC<MarketsTableProps> = ({ className }) => {
         <tbody className={Classes.TEXT_LARGE}>
           {marketResults.map(([market, status], index) => {
             return (
-              <MarketsTableRow
+              <MarketsTableRowOld
                 key={market?.contractAddress || index}
                 market={market}
                 marketStatus={status}
@@ -137,79 +135,5 @@ const MarketsTableHeader: FC<MarketsTableHeaderProps> = ({
         alignIndicator={Alignment.RIGHT}
       />
     </th>
-  );
-};
-
-interface MarketsTableRowProps {
-  market: Market | undefined;
-  marketStatus: QueryStatus;
-}
-export const MarketsTableRow: FC<MarketsTableRowProps> = ({
-  market,
-  marketStatus,
-}) => {
-  const [timerValue, setTimerValue] = useState(Date.now());
-  // TODO: make timer helper fn
-  useInterval(() => {
-    if (!market) {
-      return;
-    }
-    setTimerValue(market.maturityDate - Date.now());
-  }, 1000);
-
-  // TODO: make this better
-  if (!market && marketStatus === "loading") {
-    return (
-      <tr>
-        <td>loading</td>
-      </tr>
-    );
-  }
-
-  if (!market) {
-    return null;
-  }
-
-  // TODO: make progress helper fn
-  const progress =
-    (Date.now() - market.startDate) / (market.maturityDate - market.startDate);
-
-  const { totalSupply } = market;
-  const startDate = new Date(market?.startDate);
-  const maturityDate = new Date(market?.maturityDate);
-  const baseAsset = market.assets[0];
-  const yieldAsset = market.assets[1];
-
-  const [daysLeft, hoursLeft, minutesLeft] = getTimeLeft(timerValue);
-  const time = t`${daysLeft} days, ${hoursLeft}, hours, ${minutesLeft} minutes`;
-
-  return (
-    <tr>
-      <td>{maturityDate.toLocaleDateString()}</td>
-      <td>
-        <Link className={tw("flex", "space-x-2")} to={market.contractAddress}>
-          <LabeledText bold text={baseAsset.symbol} label={baseAsset.name} />
-          {"-"}
-          <LabeledText bold text={yieldAsset.symbol} label={yieldAsset.name} />
-        </Link>
-      </td>
-
-      <td>${totalSupply?.toLocaleString()}</td>
-      <td>2.13%</td>
-
-      <td>{startDate.toLocaleDateString()}</td>
-
-      <td>
-        {market.state === "running" ? (
-          <LabeledProgressBar
-            progressValue={progress}
-            label={t`running`}
-            helperText={time}
-          />
-        ) : (
-          market.state
-        )}
-      </td>
-    </tr>
   );
 };
