3f856ec4ef0d3dbef4d6338625816ef6f0f3dbcc
Danny Delott <dannydelott@gmail.com>
2021-03-11 19:24:11 -0800

    Wire up Confirm Transaction button

--- frontend/src/efi-ui/invest/BuyFYTConfirmationDrawer/BuyFYTConfirmationDrawer.tsx
+++ frontend/src/efi-ui/invest/BuyFYTConfirmationDrawer/BuyFYTConfirmationDrawer.tsx
@@ -11,35 +11,36 @@ import {
 } from "@blueprintjs/core";
 import { IconNames } from "@blueprintjs/icons";
 import { Web3Provider } from "@ethersproject/providers";
+import { formatUnits, parseUnits } from "@ethersproject/units";
 import { AbstractConnector } from "@web3-react/abstract-connector";
 import classNames from "classnames";
+import { ERC20 } from "elf-contracts/types/ERC20";
 import { Tranche } from "elf-contracts/types/Tranche";
 import { Signer } from "ethers";
 import { t } from "ttag";
 
 import tw from "efi-tailwindcss-classnames";
+import { useBalancerVault } from "efi-ui/balancer/useBalancerVault";
+import { useBatchSwapGivenIn } from "efi-ui/balancer/useBatchSwapGivenIn";
 import { LabeledText } from "efi-ui/base/LabeledText/LabeledText";
 import { matchSmartContractReadCallQuery } from "efi-ui/contracts/matchSmartContractReadCallQuery/matchSmartContractReadCallQuery";
 import { useSmartContractReadCall } from "efi-ui/contracts/useSmartContractReadCall/useSmartContractReadCall";
 import { useSmartContractTransaction } from "efi-ui/contracts/useSmartContractTransaction/useSmartContractTransaction";
 import { CryptoAssetWithIcon } from "efi-ui/crypto/CryptoAssetWithIcon";
+import { useCryptoDecimals } from "efi-ui/crypto/hooks/useCryptoDecimals/useCryptoDecimals";
 import { useCryptoName } from "efi-ui/crypto/hooks/useCryptoName/useCryptoName";
 import { useCryptoSymbol } from "efi-ui/crypto/hooks/useCryptoSymbol/useCryptoSymbol";
+import { useOnSwapGivenIn } from "efi-ui/pools/useOnSwapGivenIn/useOnSwapGivenIn";
 import { useDarkMode } from "efi-ui/prefs/useDarkMode/useDarkMode";
 import { WalletConnectionCard } from "efi-ui/wallets/WalletConnectionCard/WalletConnectionCard";
 import { convertEpochSecondsToDate } from "efi/base/convertEpochSecondsToDate";
 import { formatFullDate } from "efi/base/dates";
 import { MAX_ALLOWANCE } from "efi/contracts/token";
 import { CryptoAssetType } from "efi/crypto/CryptoAsset";
+import { PoolContract } from "efi/pools/PoolContract";
 import { getConnectorName } from "efi/wallets/connectors";
 
 import { isApprovalRequiredForTransactions } from "../../crypto/isApprovalRequiredForTransactions";
-import { PoolContract } from "efi/pools/PoolContract";
-import { useBalancerVault } from "efi-ui/balancer/useBalancerVault";
-import { ERC20 } from "elf-contracts/types/ERC20";
-import { useOnSwapGivenIn } from "efi-ui/pools/useOnSwapGivenIn/useOnSwapGivenIn";
-import { useCryptoDecimals } from "efi-ui/crypto/hooks/useCryptoDecimals/useCryptoDecimals";
-import { formatUnits, parseUnits } from "@ethersproject/units";
 
 interface BuyFYTConfirmationDrawerProps {
   chainId: number | undefined;
@@ -97,10 +98,18 @@ export const BuyFYTConfirmationDrawer: FC<BuyFYTConfirmationDrawerProps> = ({
     callArgs: [account as string, balancerVault?.address as string],
   });
 
+  const amountAsBigNumber = parseUnits(amount || "0", baseAssetDecimals);
   const { data: amountOut } = useOnSwapGivenIn(
     pool,
     baseAssetContract,
-    parseUnits(amount || "0", baseAssetDecimals)
+    amountAsBigNumber
+  );
+  const onTransaction = useBatchSwapGivenIn(
+    account,
+    signer,
+    pool,
+    baseAssetContract,
+    amountAsBigNumber
   );
   const connectorName = getConnectorName(connector, library);
 
@@ -224,7 +233,7 @@ export const BuyFYTConfirmationDrawer: FC<BuyFYTConfirmationDrawerProps> = ({
             intent={hasApproval ? Intent.PRIMARY : Intent.NONE}
             large
             outlined
-            onClick={onClose}
+            onClick={onTransaction}
           >{t`Confirm transaction`}</Button>
         </div>
       </div>
