6cc62d682c1157f8897e89b9df7107061cb23b76
Danny Delott <dannydelott@gmail.com>
2021-03-22 15:28:36 -0700

    Add useOnSwapGivenOut hook

---
+++ frontend/src/efi-ui/pools/useOnSwapGivenIn/makeOnSwapGivenOutCallArgs.ts
@@ -0,0 +1,44 @@
+import { ERC20 } from "elf-contracts/types/ERC20";
+import { YieldCurvePool } from "elf-contracts/types/YieldCurvePool";
+import { BigNumber } from "ethers";
+
+import { StaticContractMethodArgs } from "efi/contracts/types";
+
+export function makeOnSwapGivenOutCallArgs(
+  poolId: string | undefined,
+  account: string | null | undefined,
+  tokenIn: ERC20 | undefined,
+  amount: BigNumber | undefined,
+  tokenOut: ERC20 | undefined,
+  poolTokenBalances: BigNumber[] | undefined,
+  latestBlockNumber: number | undefined
+): StaticContractMethodArgs<YieldCurvePool, "onSwapGivenOut"> | undefined {
+  if (
+    !account ||
+    !poolId ||
+    !tokenIn ||
+    !amount ||
+    !tokenOut ||
+    !poolTokenBalances?.length ||
+    !latestBlockNumber
+  ) {
+    return undefined;
+  }
+  const callArgs: StaticContractMethodArgs<YieldCurvePool, "onSwapGivenOut"> = [
+    {
+      poolId: poolId,
+      amountOut: amount,
+      tokenIn: tokenIn?.address,
+      tokenOut: tokenOut?.address,
+
+      from: account,
+      to: account,
+      latestBlockNumberUsed: latestBlockNumber,
+      userData: poolId,
+    },
+    poolTokenBalances?.[0],
+    poolTokenBalances?.[1],
+  ];
+
+  return callArgs;
+}
