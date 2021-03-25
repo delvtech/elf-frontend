822217ecbe2f54ee23b9db92d161035d3a440ae8
Danny Delott <dannydelott@gmail.com>
2021-03-25 13:03:42 -0700

    Remove minting step logic

--- frontend/src/efi-ui/mint/MintView/MintView.tsx
+++ frontend/src/efi-ui/mint/MintView/MintView.tsx
@@ -1,4 +1,4 @@
-import { FC, ReactNode, useMemo, useState } from "react";
+import React, { FC, ReactNode, useMemo, useState } from "react";
 
 import { Classes } from "@blueprintjs/core";
 import { Web3Provider } from "@ethersproject/providers";
@@ -11,55 +11,14 @@ import tw from "efi-tailwindcss-classnames";
 
 import { BaseAssetTabs } from "../BaseAssetTabs/BaseAssetTabs";
 import { ViewTitle } from "efi-ui/page/ViewTitle/ViewTitle";
+import { MintCard } from "efi-ui/mint/MintCard/MintCard";
 
 interface MintViewProps extends RouteComponentProps {}
-
-interface MintingStep {
-  title: string;
-  description: string;
-
-  content: ReactNode;
-}
 
 export const MintView: FC<MintViewProps> = () => {
   const { account } = useWeb3React<Web3Provider>();
 
-  // eslint-disable-next-line @typescript-eslint/no-unused-vars
   const [investmentAmount, setInvestmentAmount] = useState(0);
-
-  const [currentStepIndex] = useState(0);
-  const mintingSteps: MintingStep[] = useMemo(
-    (): MintingStep[] => [
-      {
-        title: t`Choose a base asset`,
-        description: t`To begin minting yield tokens, pick a base asset and yield position from the options below:`,
-        content: (
-          <BaseAssetTabs
-            account={account}
-            onInvestmentAmountChange={setInvestmentAmount}
-          />
-        ),
-      },
-      {
-        title: t`Set an amount`,
-        description: t`To begin minting yield tokens, pick a base asset from the following options:`,
-        content: null,
-      },
-      {
-        title: t`Set time to maturity`,
-        description: t`When do you want your yield tokens to mature?`,
-        content: null,
-      },
-      {
-        title: t`Confirmation`,
-        description: "",
-        content: null,
-      },
-    ],
-    [account]
-  );
-
-  const currentStep = mintingSteps[currentStepIndex];
 
   return (
     <div
@@ -91,16 +50,21 @@ export const MintView: FC<MintViewProps> = () => {
             "justify-center"
           )}
         >
-          <span
-            className={classNames(
-              Classes.RUNNING_TEXT,
-              Classes.TEXT_MUTED,
-              tw("text-base")
-            )}
-          >
-            {currentStep.description}
-          </span>
-          {currentStep.content}
+          <BaseAssetTabs
+            account={account}
+            onInvestmentAmountChange={setInvestmentAmount}
+          />
+          <MintCard
+            id={id}
+            assetName={assetName}
+            assetIcon={AssetIcon}
+            assetSymbol={assetSymbol}
+            assetPrice={assetPrice}
+            walletBalance={walletBalance}
+            walletBalanceFiat={walletBalanceFiat}
+            yieldPositions={yieldPositions}
+            onInvestmentAmountChange={onInvestmentAmountChange}
+          />
         </div>
       </div>
     </div>
