2e5fe2f4aa395d63521beda22610f8abe48ab179
Danny Delott <dannydelott@gmail.com>
2021-03-26 12:51:28 -0700

    Get list of base asset contracts via tranche calls not yvaultassetproxy calls

--- frontend/src/efi-ui/invest/hooks/useBaseAssetContracts.ts
+++ frontend/src/efi-ui/invest/hooks/useBaseAssetContracts.ts
@@ -3,30 +3,26 @@ import { ERC20__factory } from "elf-contracts/types/factories/ERC20__factory";
 
 import { useSmartContractReadCalls } from "efi-ui/contracts/useSmartContractReadCalls/useSmartContractReadCalls";
 import { useSmartContractsFromFactory } from "efi-ui/contracts/useSmartContractsFromFactory/useSmartContractsFromFactory";
-import { useYVaultAssetProxyContracts } from "efi-ui/contracts/useYVaultAssetProxyContracts/useYVaultAssetProxyContracts";
-import { jsonRpcProvider } from "efi/providers/jsonRpcProviders";
+import { useTrancheContracts } from "efi-ui/tranche/useTrancheContracts";
+import { getQueriesData } from "efi-ui/base/queryResults";
 
 /**
- * Gets a list of all the tranche contracts.
+ * Gets a list of all the base asset contracts.
  */
 export function useBaseAssetContracts(): (ERC20 | undefined)[] {
   // The elf contracts contain the base asset
-  const elfContracts = useYVaultAssetProxyContracts(jsonRpcProvider);
+  const tranches = useTrancheContracts();
 
   // The token is the address to the base asset
-  const baseAssetAddressesResult = useSmartContractReadCalls(
-    elfContracts,
-    "token"
-  );
-  const baseAssetAddresses = baseAssetAddressesResult.map(
-    (result) => result.data
+  const underlyingTokenResults = useSmartContractReadCalls(
+    tranches,
+    "underlying"
   );
 
   // All base assets are known to be ERC20s
   const baseAssetContracts = useSmartContractsFromFactory(
-    baseAssetAddresses,
-    ERC20__factory.connect,
-    jsonRpcProvider
+    getQueriesData(underlyingTokenResults),
+    ERC20__factory.connect
   );
 
   return baseAssetContracts;
