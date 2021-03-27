6ab5586160379eb315c854494ef39934e662dbb0
Danny Delott <dannydelott@gmail.com>
2021-03-26 19:56:19 -0700

    Allow cryptoasset to be undefined

--- frontend/src/efi-ui/crypto/hooks/useCryptoDecimals/useCryptoDecimals.ts
+++ frontend/src/efi-ui/crypto/hooks/useCryptoDecimals/useCryptoDecimals.ts
@@ -2,8 +2,8 @@ import { CryptoAsset, findTokenContract } from "efi/crypto/CryptoAsset";
 import { useSmartContractReadCall } from "efi-ui/contracts/useSmartContractReadCall/useSmartContractReadCall";
 import { NUM_ETH_DECIMALS } from "efi/crypto/ethereum";
 
-export function useCryptoDecimals(asset: CryptoAsset): number {
-  const tokenContract = findTokenContract(asset);
+export function useCryptoDecimals(asset: CryptoAsset | undefined): number {
+  const tokenContract = asset ? findTokenContract(asset) : undefined;
   const { data: tokenDecimals } = useSmartContractReadCall(
     tokenContract,
     "decimals"
