c68c709f3d5c1db2eaa2fb5d5a62074cc7ecfc34
Matthew Brown <mattfdbrown@gmail.com>
2021-03-30 17:33:34 -0700

    fix tests

--- frontend/src/efi/money/formatMoney.test.ts
+++ frontend/src/efi/money/formatMoney.test.ts
@@ -8,25 +8,25 @@ test("should format undefined balance correctly", () => {
 
 test("should format zero balance correctly", () => {
   const cents = new Money(0, Currencies.USD);
-  expect(formatMoney(cents)).toEqual("0.00");
+  expect(formatMoney(cents)).toEqual("$0.00");
 });
 
 test("should format micro balances correctly", () => {
   const cents = new Money(10, Currencies.USD);
-  expect(formatMoney(cents)).toEqual("0.10");
+  expect(formatMoney(cents)).toEqual("$0.10");
 });
 
 test("should format small balances correctly", () => {
   const cents = new Money(100, Currencies.USD);
-  expect(formatMoney(cents)).toEqual("1.00");
+  expect(formatMoney(cents)).toEqual("$1.00");
 });
 
 test("should format medium balances correctly", () => {
   const cents = new Money(10000, Currencies.USD);
-  expect(formatMoney(cents)).toEqual("100.00");
+  expect(formatMoney(cents)).toEqual("$100.00");
 });
 
 test("should format large balances correctly", () => {
   const cents = new Money(1000000, Currencies.USD);
-  expect(formatMoney(cents)).toEqual("10,000.00");
+  expect(formatMoney(cents)).toEqual("$10,000.00");
 });
