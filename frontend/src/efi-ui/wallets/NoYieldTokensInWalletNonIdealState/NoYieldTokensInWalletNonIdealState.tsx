5c2b5ecc6d9daa910f5871aa16171c7823ea497c
Danny Delott <dannydelott@gmail.com>
2021-04-08 21:46:49 -0700

    Renaming

--- frontend/src/efi-ui/wallets/NoYieldTokensInWalletNonIdealState/NoYieldTokensInWalletNonIdealState.tsx
+++ frontend/src/efi-ui/wallets/NoYieldTokensInWalletNonIdealState/NoYieldTokensInWalletNonIdealState.tsx
@@ -1,24 +1,24 @@
-import React, { FC, useCallback } from "react";
+import React, { ReactElement, useCallback } from "react";
 
 import { Button, NonIdealState } from "@blueprintjs/core";
 import { IconNames } from "@blueprintjs/icons";
 import { t } from "ttag";
 
+import tw from "efi-tailwindcss-classnames";
 import { useNavigation } from "efi-ui/navigation/hooks/useTab";
 import { Navigation } from "efi-ui/navigation/navigation";
 
-interface NoITsInWalletNonIdealStateProps {}
-
-export const NoInterestTokensInWalletNonIdealState: FC<NoITsInWalletNonIdealStateProps> = () => {
+export function NoYieldTokensInWalletNonIdealState(): ReactElement {
   const { changeTab } = useNavigation();
   const goToMint = useCallback(() => changeTab(Navigation.MINT), [changeTab]);
   return (
     <NonIdealState
       icon={IconNames.BANK_ACCOUNT}
-      description={t`This wallet does not contain any Interest Tokens.`}
+      className={tw("text-base")}
+      description={t`This wallet does not contain any Yield Tokens.`}
       action={
         <Button outlined large onClick={goToMint}>{t`Go to Mint`}</Button>
       }
     />
   );
-};
+}
