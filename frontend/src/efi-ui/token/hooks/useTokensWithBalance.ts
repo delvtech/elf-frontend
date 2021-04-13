2e5d44f97a77120b03fd968d240217a5ea885129
Danny Delott <dannydelott@gmail.com>
2021-04-13 15:47:51 -0700

    add hook to get subset of tokens that have a balance

---
+++ frontend/src/efi-ui/token/hooks/useTokensWithBalance.ts
@@ -0,0 +1,44 @@
+import { Provider } from "@ethersproject/providers";
+import { ERC20 } from "elf-contracts/types/ERC20";
+import { ERC20Permit } from "elf-contracts/types/ERC20Permit";
+import { BigNumber } from "ethers";
+import zip from "lodash.zip";
+
+import { getQueriesData } from "efi-ui/base/queryResults";
+import {
+  useSmartContractReadCalls,
+  UseSmartContractReadCallsOptions,
+} from "efi-ui/contracts/useSmartContractReadCalls/useSmartContractReadCalls";
+
+export function useTokensWithBalance<TContract extends ERC20>(
+  account: string | null | undefined,
+  tokens: (TContract | undefined)[],
+  provider?: Provider
+): { token: TContract; balanceOf: BigNumber }[] {
+  const balanceOfArgs: UseSmartContractReadCallsOptions<
+    ERC20 | ERC20Permit,
+    "balanceOf"
+  >[] = tokens.map(() => ({
+    callArgs: [account as string],
+    enabled: !!account,
+  }));
+
+  const tokenBalanceOfResults = useSmartContractReadCalls(
+    tokens,
+    "balanceOf",
+    balanceOfArgs
+  );
+
+  const loadedData = zip(
+    tokens,
+    getQueriesData(tokenBalanceOfResults)
+  ).filter((values): values is [TContract, BigNumber] =>
+    values.every((value) => !!value)
+  );
+
+  const tokensWithBalance = loadedData
+    .filter(([, balanceOf]) => balanceOf.gt(0))
+    .map(([token, balanceOf]) => ({ token, balanceOf }));
+
+  return tokensWithBalance;
+}
