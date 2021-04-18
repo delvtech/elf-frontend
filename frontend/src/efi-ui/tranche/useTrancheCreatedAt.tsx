0346e3040b040010ad342db25044ae0bd4207b91
Matthew Brown <mattfdbrown@gmail.com>
2021-04-17 20:10:58 -0700

    refactor more hooks

--- frontend/src/efi-ui/tranche/useTrancheCreatedAt.tsx
+++ frontend/src/efi-ui/tranche/useTrancheCreatedAt.tsx
@@ -1,42 +1,60 @@
 import { QueryObserverResult, useQuery } from "react-query";
 
-import { Provider } from "@ethersproject/providers";
 import { TrancheFactory__factory } from "elf-contracts/types/factories/TrancheFactory__factory";
 import { Tranche } from "elf-contracts/types/Tranche";
-import { TrancheFactory } from "elf-contracts/types/TrancheFactory";
-import { Signer } from "ethers";
 
+import { useSmartContractFromFactory } from "efi-ui/contracts/useSmartContractFromFactory/useSmartContractFromFactory";
+import { useSmartContractQuery } from "efi-ui/contracts/useSmartContractQuery/useSmartContractQuery";
 import ContractAddresses from "efi/contracts/contractsJson";
+import { Block } from "efi/crypto/fetchEthBlocks";
 import { jsonRpcProvider } from "efi/providers/jsonRpcProviders";
 
 const { trancheFactoryAddress } = ContractAddresses;
 
 export function useTrancheCreatedAt(
-  tranche: Tranche | undefined,
-  signerOrProvider?: Signer | Provider
-): QueryObserverResult<number> {
-  const trancheFactoryContract = TrancheFactory__factory.connect(
+  tranche: Tranche | undefined
+): number | undefined {
+  const trancheFactoryContract = useSmartContractFromFactory(
     trancheFactoryAddress,
-    signerOrProvider ?? jsonRpcProvider
+    TrancheFactory__factory.connect
   );
 
-  const filterOptions: Parameters<
-    TrancheFactory["filters"]["TrancheCreated"]
-  > = [tranche?.address ?? null, null, null];
-  const filterQuery = trancheFactoryContract.filters.TrancheCreated(
-    ...filterOptions
+  const { data: events } = useSmartContractQuery(
+    trancheFactoryContract,
+    "TrancheCreated",
+    { callArgs: [tranche?.address as string, null, null], enabled: !!tranche }
   );
+  const blockNumber = events?.[0].blockNumber;
 
-  const queryResult = useQuery<number>({
-    queryKey: [
-      ["trancheFactory", "queryFilter", "TrancheCreated"],
-      { filterOptions },
-    ],
+  const { data: block } = useBlock(blockNumber);
+
+  if (!block) {
+    return;
+  }
+
+  return +block.timestamp;
+}
+
+// TODO: generalize to make a useProviderCall just like we have useSmartContractReadCall
+/**
+ * returns a block for a given block number
+ * @param {number} blockNumber integer block number
+ * @returns {Block}
+ */
+export function useBlock(
+  blockNumber: number | undefined
+): QueryObserverResult<Block> {
+  if (!Number.isInteger(blockNumber)) {
+    console.error("Error using useBlock: blockNumber must be an integer");
+  }
+
+  const queryResult = useQuery({
+    queryKey: [["provider", "getBlock"], { blockNumber }],
     queryFn: async () => {
-      const events = await trancheFactoryContract.queryFilter(filterQuery);
-      const block = await jsonRpcProvider.getBlock(events?.[0].blockNumber);
-      return block.timestamp;
+      return jsonRpcProvider.getBlock(blockNumber as number);
     },
-  });
+    enabled: blockNumber !== undefined && Number.isInteger(blockNumber),
+  }) as QueryObserverResult<Block>;
+
   return queryResult;
 }
