01eb06820d2768709ab8da762dde0d8293ab8e6e
Matthew Brown <mattfdbrown@gmail.com>
2021-04-18 19:44:37 -0700

    delay clearing the txhash by 10s in dev

--- frontend/src/efi-ui/transactions/usePendingTransaction/usePendingTransaction.ts
+++ frontend/src/efi-ui/transactions/usePendingTransaction/usePendingTransaction.ts
@@ -1,9 +1,20 @@
 import { usePendingTransactionPref } from "efi-ui/prefs/usePendingTransactionPref/usePendingTransactionPref";
 import { jsonRpcProvider } from "efi/providers/jsonRpcProviders";
+import { useEffect, useState } from "react";
 import { useInterval } from "react-use";
 
+const __DEVELOPMENT__ = process.env.NODE_ENV === "development";
+const CLEAR_PENDING_DELAY = __DEVELOPMENT__ ? 10 * 1000 : 0;
 export function usePendingTransaction(): string | undefined {
   const { transactionHash, setTransaction } = usePendingTransactionPref();
+
+  const [newHashValue, setNewValue] = useState(transactionHash);
+  useEffect(() => {
+    if (!newHashValue && transactionHash) {
+      setTimeout(() => setTransaction(undefined), CLEAR_PENDING_DELAY);
+    }
+  }, [newHashValue, setTransaction, transactionHash]);
+
   useInterval(async () => {
     if (!transactionHash) {
       return;
@@ -15,7 +26,7 @@ export function usePendingTransaction(): string | undefined {
     // if the transaction is included in a block then it was successful, so we clear the pending tx
     // hash.
     if (blockHash) {
-      setTransaction(undefined);
+      setNewValue(undefined);
     }
   }, 2000);
 
