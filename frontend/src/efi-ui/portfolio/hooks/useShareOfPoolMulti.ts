0b446b23dcd4d836663d9e49e372b2b77d0d2cec
Danny Delott <dannydelott@gmail.com>
2021-04-20 11:30:28 -0700

    Add non-multi hook

--- frontend/src/efi-ui/portfolio/hooks/useShareOfPoolMulti.ts
+++ frontend/src/efi-ui/portfolio/hooks/useShareOfPoolMulti.ts
@@ -1,14 +1,36 @@
+import { BigNumber } from "ethers";
 import { formatUnits } from "ethers/lib/utils";
 import zip from "lodash.zip";
 
 import { BALANCER_POOL_LP_TOKEN_DECIMALS } from "efi-balancer/pools";
 import { getQueriesData } from "efi-ui/base/queryResults";
+import { useSmartContractReadCall } from "efi-ui/contracts/useSmartContractReadCall/useSmartContractReadCall";
 import { useSmartContractReadCalls } from "efi-ui/contracts/useSmartContractReadCalls/useSmartContractReadCalls";
-import { useTokenBalanceOfMulti } from "efi-ui/token/hooks/useTokenBalanceOf";
+import {
+  useTokenBalanceOf,
+  useTokenBalanceOfMulti,
+} from "efi-ui/token/hooks/useTokenBalanceOf";
 import { PoolContract } from "efi/pools/PoolContract";
 
 /**
- * Calculates the amount of each pool the user owns.
+ * Calculates the amount of the pool the account owns.
+ */
+export function useShareOfPool(
+  pool: PoolContract,
+  account: string | null | undefined
+): number | undefined {
+  const { data: lpBalanceOf } = useTokenBalanceOf(pool, account);
+  const { data: totalSupply } = useSmartContractReadCall(pool, "totalSupply");
+
+  if (!lpBalanceOf || !totalSupply) {
+    return undefined;
+  }
+
+  return calculateShareOfPool(lpBalanceOf, totalSupply);
+}
+
+/**
+ * Calculates the amount of each pool the account owns.
  */
 export function useShareOfPoolMulti(
   pools: (PoolContract | undefined)[],
@@ -24,18 +46,21 @@ export function useShareOfPoolMulti(
     if (!lpBalanceOf || !totalSupply) {
       return undefined;
     }
-
-    const balanceOfNumber = +formatUnits(
-      lpBalanceOf,
-      BALANCER_POOL_LP_TOKEN_DECIMALS
-    );
-    const totalSupplyNumber = +formatUnits(
-      totalSupply,
-      BALANCER_POOL_LP_TOKEN_DECIMALS
-    );
-    const shareOfPool = balanceOfNumber / totalSupplyNumber;
-    return shareOfPool;
+    return calculateShareOfPool(lpBalanceOf, totalSupply);
   });
 
   return shareOfPools;
+}
+
+function calculateShareOfPool(lpBalanceOf: BigNumber, totalSupply: BigNumber) {
+  const balanceOfNumber = +formatUnits(
+    lpBalanceOf,
+    BALANCER_POOL_LP_TOKEN_DECIMALS
+  );
+  const totalSupplyNumber = +formatUnits(
+    totalSupply,
+    BALANCER_POOL_LP_TOKEN_DECIMALS
+  );
+  const shareOfPool = balanceOfNumber / totalSupplyNumber;
+  return shareOfPool;
 }
