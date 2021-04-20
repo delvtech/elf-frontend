9176788616b1a62aa7fa4223368cd5b73164b179
Danny Delott <dannydelott@gmail.com>
2021-04-20 01:04:34 -0700

    Add useTotalLiquidityProvidedMulti

--- frontend/src/efi-ui/portfolio/hooks/useConvergentCurvePoolsWithLPBalance.ts
+++ frontend/src/efi-ui/portfolio/hooks/useConvergentCurvePoolsWithLPBalance.ts
@@ -1,23 +1,14 @@
+import { useMemo } from "react";
+
+import { ConvergentCurvePool } from "elf-contracts/types/ConvergentCurvePool";
+
 import { useConvergentCurvePools } from "efi-ui/pools/useConvergentCurvePools/useConvergentCurvePools";
 import { useTokensWithBalance } from "efi-ui/token/hooks/useTokensWithBalance";
-import { ConvergentCurvePool } from "elf-contracts/types/ConvergentCurvePool";
-import { BigNumber } from "ethers";
-import { useMemo } from "react";
 
 export function useConvergentCurvePoolsWithLPBalance(
   account: string | null | undefined
-): {
-  pool: ConvergentCurvePool;
-  lpBalance: BigNumber;
-}[] {
+): ConvergentCurvePool[] {
   const allConvergentCurvePools = useConvergentCurvePools();
   const poolsWithLP = useTokensWithBalance(account, allConvergentCurvePools);
-  return useMemo(
-    () =>
-      poolsWithLP.map(({ balanceOf, token }) => ({
-        pool: token,
-        lpBalance: balanceOf,
-      })),
-    [poolsWithLP]
-  );
+  return useMemo(() => poolsWithLP.map(({ token }) => token), [poolsWithLP]);
 }
