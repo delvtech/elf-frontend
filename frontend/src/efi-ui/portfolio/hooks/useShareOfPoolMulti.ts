c20037901c419b6605f5ea6da9703c72db93a1b0
Danny Delott <dannydelott@gmail.com>
2021-04-20 00:32:34 -0700

    Add multis

---
+++ frontend/src/efi-ui/portfolio/hooks/useShareOfPoolMulti.ts
@@ -0,0 +1,40 @@
+import zip from "lodash.zip";
+import { getQueriesData } from "efi-ui/base/queryResults";
+import { PoolContract } from "efi/pools/PoolContract";
+import { useSmartContractReadCalls } from "efi-ui/contracts/useSmartContractReadCalls/useSmartContractReadCalls";
+import { formatUnits } from "@ethersproject/units";
+import { BALANCER_POOL_LP_TOKEN_DECIMALS } from "efi-balancer/pools";
+import { useTokenBalanceOfMulti } from "efi-ui/token/hooks/useTokenBalanceOf";
+
+/**
+ * Calculates the amount of each pool the user owns.
+ */
+export function useShareOfPoolMulti(
+  pools: (PoolContract | undefined)[],
+  account: string | null | undefined
+): (number | undefined)[] {
+  const lpBalanceOfResults = useTokenBalanceOfMulti(pools, account);
+  const totalSupplyResults = useSmartContractReadCalls(pools, "totalSupply");
+
+  const shareOfPools = zip(
+    getQueriesData(lpBalanceOfResults),
+    getQueriesData(totalSupplyResults)
+  ).map(([lpBalanceOf, totalSupply]) => {
+    if (!lpBalanceOf || !totalSupply) {
+      return undefined;
+    }
+
+    const balanceOfNumber = +formatUnits(
+      lpBalanceOf,
+      BALANCER_POOL_LP_TOKEN_DECIMALS
+    );
+    const totalSupplyNumber = +formatUnits(
+      totalSupply,
+      BALANCER_POOL_LP_TOKEN_DECIMALS
+    );
+    const shareOfPool = balanceOfNumber / totalSupplyNumber;
+    return shareOfPool;
+  });
+
+  return shareOfPools;
+}
