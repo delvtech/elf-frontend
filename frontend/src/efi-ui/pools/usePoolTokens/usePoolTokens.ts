4c7e8dd2eda6d82d916d1c7637e2809ba925997d
Danny Delott <dannydelott@gmail.com>
2021-04-20 15:40:57 -0700

    Inline return signature to avoid file hopping

--- frontend/src/efi-ui/pools/usePoolTokens/usePoolTokens.ts
+++ frontend/src/efi-ui/pools/usePoolTokens/usePoolTokens.ts
@@ -3,13 +3,19 @@ import { Vault } from "elf-contracts/types/Vault";
 import { useBalancerVault } from "efi-ui/balancer/useBalancerVault";
 import { getQueryData } from "efi-ui/base/queryResults";
 import { useSmartContractReadCall } from "efi-ui/contracts/useSmartContractReadCall/useSmartContractReadCall";
-import { UsePoolTokensQueryResult } from "efi-ui/pools/usePoolTokens/UsePoolTokensQueryResult";
 import { ContractMethodArgs } from "efi/contracts/types";
 import { PoolContract } from "efi/pools/PoolContract";
+import { QueryObserverResult } from "react-query";
+import { BigNumber } from "ethers";
 
 export function usePoolTokens(
   pool: PoolContract | undefined
-): UsePoolTokensQueryResult {
+): QueryObserverResult<
+  [
+    string[], // addresses
+    BigNumber[] // balances
+  ]
+> {
   const balancerVault = useBalancerVault();
   const poolIdResult = useSmartContractReadCall(pool, "getPoolId");
   const poolId = getQueryData(poolIdResult);
