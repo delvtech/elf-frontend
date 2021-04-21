3678bc5e93552fc212df80dbef9e25d7eb7d7a82
Danny Delott <dannydelott@gmail.com>
2021-04-21 14:17:19 -0700

    Make getter util for the smart contract registry

--- frontend/src/efi-ui/contracts/SmartContractsRegistry.ts
+++ frontend/src/efi-ui/contracts/SmartContractsRegistry.ts
@@ -1,1 +1,34 @@
-export const SMART_CONTRACTS_REGISTRY: Record<string, unknown> = {};
+import { Provider } from "@ethersproject/providers";
+import { FactoryConnectFn } from "efi/contracts/FactoryConnectFn";
+import { jsonRpcProvider } from "efi/providers/jsonRpcProviders";
+import { Contract, Signer } from "ethers";
+
+// Do not export this from this file
+const SMART_CONTRACTS_REGISTRY: Record<string, unknown> = {};
+
+/**
+ * Gets registered smart contract instance, or creates, registers, and returns
+ * it if it doesn't exist.
+ */
+export function getSmartContractFromRegistry<TReturnContract extends Contract>(
+  address: string | undefined,
+  factoryConnect: FactoryConnectFn<TReturnContract>,
+  signerOrProvider?: Signer | Provider
+): TReturnContract | undefined {
+  if (!address) {
+    return undefined;
+  }
+
+  // Pull from cache if we have the instance already
+  const cachedContract = SMART_CONTRACTS_REGISTRY[address];
+  if (cachedContract) {
+    return cachedContract as TReturnContract;
+  }
+
+  // Otherwise populate cache and return it
+  SMART_CONTRACTS_REGISTRY[address] = factoryConnect(
+    address,
+    signerOrProvider ?? jsonRpcProvider
+  );
+  return SMART_CONTRACTS_REGISTRY[address] as TReturnContract;
+}
--- frontend/src/efi-ui/contracts/useSmartContractFromFactory/useSmartContractFromFactory.ts
+++ frontend/src/efi-ui/contracts/useSmartContractFromFactory/useSmartContractFromFactory.ts
@@ -3,7 +3,7 @@ import { useMemo } from "react";
 import { Provider } from "@ethersproject/providers";
 import { Contract, Signer } from "ethers";
 
-import { SMART_CONTRACTS_REGISTRY } from "efi-ui/contracts/SmartContractsRegistry";
+import { getSmartContractFromRegistry } from "efi-ui/contracts/SmartContractsRegistry";
 import { FactoryConnectFn } from "efi/contracts/FactoryConnectFn";
 import { jsonRpcProvider } from "efi/providers/jsonRpcProviders";
 
@@ -14,7 +14,7 @@ export function useSmartContractFromFactory<TReturnContract extends Contract>(
 ): TReturnContract | undefined {
   const signer = signerOrProvider ?? jsonRpcProvider;
   const contract = useMemo(() => {
-    return lookupSmartContract<TReturnContract>(
+    return getSmartContractFromRegistry<TReturnContract>(
       address,
       factoryConnect,
       signer
@@ -35,33 +35,14 @@ export function useSmartContractFromFactoryMulti<
   const contracts = useMemo(
     () =>
       addresses.map((address) =>
-        lookupSmartContract<TReturnContract>(address, factoryConnect, signer)
+        getSmartContractFromRegistry<TReturnContract>(
+          address,
+          factoryConnect,
+          signer
+        )
       ),
     [addresses, factoryConnect, signer]
   );
 
   return contracts;
-}
-
-function lookupSmartContract<TReturnContract extends Contract>(
-  address: string | undefined,
-  factoryConnect: FactoryConnectFn<TReturnContract>,
-  signerOrProvider?: Signer | Provider
-): TReturnContract | undefined {
-  if (!address) {
-    return undefined;
-  }
-
-  // Pull from cache if we have the instance already
-  const cachedContract = SMART_CONTRACTS_REGISTRY[address];
-  if (cachedContract) {
-    return cachedContract as TReturnContract;
-  }
-
-  // Otherwise populate cache and return it
-  SMART_CONTRACTS_REGISTRY[address] = factoryConnect(
-    address,
-    signerOrProvider ?? jsonRpcProvider
-  );
-  return SMART_CONTRACTS_REGISTRY[address] as TReturnContract;
 }
