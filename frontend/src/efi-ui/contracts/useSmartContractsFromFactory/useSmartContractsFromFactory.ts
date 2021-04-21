4d61c7ebad701bf95244ad3ce741cde87cdceef6
Danny Delott <dannydelott@gmail.com>
2021-04-20 23:14:53 -0700

    Add global smart contract registry to avoid new instances causing rendering thrashing in portfolio. This was profiled by calling useWhyDidYouUpdate in the Portfolio and seeing that the tranche contract instance was being recreated a bajillion times

--- frontend/src/efi-ui/contracts/useSmartContractsFromFactory/useSmartContractsFromFactory.ts
+++ frontend/src/efi-ui/contracts/useSmartContractsFromFactory/useSmartContractsFromFactory.ts
@@ -5,6 +5,7 @@ import { Contract, Signer } from "ethers";
 
 import { FactoryConnectFn } from "efi/contracts/FactoryConnectFn";
 import { jsonRpcProvider } from "efi/providers/jsonRpcProviders";
+import { SMART_CONTRACTS_REGISTRY } from "efi-ui/contracts/SmartContractsRegistry";
 
 export function useSmartContractsFromFactory<TReturnContract extends Contract>(
   addresses: (string | undefined)[],
@@ -14,14 +15,34 @@ export function useSmartContractsFromFactory<TReturnContract extends Contract>(
   const signer = signerOrProvider ?? jsonRpcProvider;
   const contracts = useMemo(
     () =>
-      addresses.map((address) => {
-        if (!address) {
-          return undefined;
-        }
-        return factoryConnect(address, signer);
-      }),
+      addresses.map((address) =>
+        lookupSmartContract<TReturnContract>(address, factoryConnect, signer)
+      ),
     [addresses, factoryConnect, signer]
   );
 
   return contracts;
+}
+
+function lookupSmartContract<TReturnContract extends Contract>(
+  address: string | undefined,
+  factoryConnect: FactoryConnectFn<TReturnContract>,
+  signerOrProvider?: Signer | Provider
+): TReturnContract | undefined {
+  if (!address) {
+    return undefined;
+  }
+
+  // Pull from cache if we have the instance already
+  const cachedContract = SMART_CONTRACTS_REGISTRY[address];
+  if (cachedContract) {
+    return cachedContract as TReturnContract;
+  }
+
+  // Otherwise populate cache and return it
+  SMART_CONTRACTS_REGISTRY[address] = factoryConnect(
+    address,
+    signerOrProvider ?? jsonRpcProvider
+  );
+  return SMART_CONTRACTS_REGISTRY[address] as TReturnContract;
 }
