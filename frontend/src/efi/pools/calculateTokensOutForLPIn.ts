8f19a2bed05ed4fb167f6d0bdf946fa8623b222f
Matthew Brown <mattfdbrown@gmail.com>
2021-04-21 22:58:33 -0700

    remove old one

--- frontend/src/efi/pools/calculateTokensOutForLPIn.ts
+++ frontend/src/efi/pools/calculateTokensOutForLPIn.ts
@@ -1,23 +1,7 @@
-// TODO: Improve this with Fixed Point math as it currently creates "dust"
-
 import {
   clipFixNumberToStringDecimals,
   getSafeFixedNumber,
 } from "efi/math/fixedPoint";
-
-// because of how javascript handles numbers.
-export function calculateTokensOutForLPIn(
-  lpIn: number,
-  xReserves: number,
-  yReserves: number,
-  totalSupply: number
-): { xNeeded: number; yNeeded: number } {
-  // solve for y_needed: lp_out = ((x_reserves / y_reserves) * y_needed * total_supply)/x_reserves
-  const yNeeded = (lpIn * xReserves) / ((xReserves / yReserves) * totalSupply);
-  // solve for x_needed: x_reserves/y_reserves = x_needed/y_needed
-  const xNeeded = (xReserves / yReserves) * yNeeded;
-  return { xNeeded, yNeeded };
-}
 
 /**
  * calculates the tokens that would be recieved for an exact amount of LP in.  uses fixedpoint math
