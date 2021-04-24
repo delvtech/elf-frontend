14ab6884ab58df704c063c9071ff5f029747f7a6
Danny Delott <dannydelott@gmail.com>
2021-04-24 13:58:48 -0700

    fix test

--- frontend/src/efi-ui/exchange/PoolsView/PoolsView.test.tsx
+++ frontend/src/efi-ui/exchange/PoolsView/PoolsView.test.tsx
@@ -8,5 +8,5 @@ test("should render with a title", async () => {
   const queryClient = createQueryClient();
   const { getByText } = await renderWithClient(queryClient, <PoolsView />);
 
-  expect(getByText("Element Exchange")).toBeVisible();
+  expect(getByText("Element Pools")).toBeVisible();
 });
