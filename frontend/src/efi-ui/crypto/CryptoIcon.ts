4650894fd42f406c7af352b93f20830915149f69
Matthew Brown <mattfdbrown@gmail.com>
2021-04-25 14:00:16 -0700

    remove deprecated stuff

--- frontend/src/efi-ui/crypto/CryptoIcon.ts
+++ frontend/src/efi-ui/crypto/CryptoIcon.ts
@@ -1,12 +1,5 @@
-import daiLogo from "efi-static-assets/logos/DAI-logo.png";
-import ethLogo from "efi-static-assets/logos/ETH-logo.png";
-import tusdLogo from "efi-static-assets/logos/TUSD-logo.png";
-import usdcLogo from "efi-static-assets/logos/USDC-logo.png";
-import usdtLogo from "efi-static-assets/logos/USDT-logo.png";
-import wethLogo from "efi-static-assets/logos/WETH-logo.png";
-import yfiLogo from "efi-static-assets/logos/YFI-logo.png";
 import { EthIcon, TokenIcon, UsdcIcon, WethIcon } from "efi-ui/token/TokenIcon";
-import { CryptoSymbol, CryptoSymbolOld } from "efi/crypto/CryptoSymbol";
+import { CryptoSymbol } from "efi/crypto/CryptoSymbol";
 
 /**
  * Lookup object to get the svg icon for a given symbol. NOTE: all keys are in
@@ -29,19 +22,3 @@ export function findAssetIcon(
   const iconKey = symbol.toUpperCase() as CryptoSymbol;
   return CryptoIconSvg[iconKey];
 }
-
-/**
- * @deprecated use CryptoIconSvg instead
- */
-export const CryptoIcon: Record<CryptoSymbolOld, string> = {
-  DAI: daiLogo,
-  yDAI: daiLogo,
-  ETH: ethLogo,
-  WETH: wethLogo,
-  USDC: usdcLogo,
-  yETH: ethLogo,
-  YFI: yfiLogo,
-  yTUSD: tusdLogo,
-  yUSDC: usdcLogo,
-  yUSDT: usdtLogo,
-};
--- frontend/src/efi/crypto/CryptoSymbol.ts
+++ frontend/src/efi/crypto/CryptoSymbol.ts
@@ -1,19 +1,1 @@
-/**
- * The names of all crypto assets supported in Element.
- *
- * @deprecated use CryptoSymbol instead
- */
-export type CryptoSymbolOld =
-  | "USDC"
-  | "DAI"
-  | "ETH"
-  | "yETH"
-  | "WETH"
-  | "yDAI"
-  | "YFI"
-  | "yTUSD"
-  | "yUSDC"
-  | "yUSDT"
-  | "yETH";
-
 export type CryptoSymbol = "USDC" | "ETH" | "WETH";
