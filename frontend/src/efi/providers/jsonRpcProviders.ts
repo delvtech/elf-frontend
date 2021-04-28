2552483a6ed4c84a11a6a73ae617bb8937a8d1fa
Danny Delott <dannydelott@gmail.com>
2021-04-28 11:02:29 -0700

    Wire up chain id and rpc host correctly based on the chain id specified in addresses.json

--- frontend/src/efi/providers/jsonRpcProviders.ts
+++ frontend/src/efi/providers/jsonRpcProviders.ts
@@ -1,6 +1,12 @@
+import { AddressesJson } from "efi/addresses";
+import { ChainId } from "efi/crypto/ethereum";
 import { providers } from "ethers";
 
-// TODO: Get this from the environment
-const RPC_HOST = "http://127.0.0.1:8545";
+// Default rpc host to local, but check the chain id in the addresses.json for
+// final say
+let RPC_HOST = "http://127.0.0.1:8545";
+if (AddressesJson.chainId === ChainId.GOERLI) {
+  RPC_HOST = process.env.REACT_APP_RPC_HOST_GOERLI as string;
+}
 
 export const jsonRpcProvider = new providers.JsonRpcProvider(RPC_HOST);
