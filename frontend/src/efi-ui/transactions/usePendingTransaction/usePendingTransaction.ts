d1458b8fb1185ee7396e2a38ea2b401adcc1907e
Danny Delott <dannydelott@gmail.com>
2021-04-30 12:12:46 -0700

    Add address and methodname to pending transaction pref

--- frontend/src/efi-ui/transactions/usePendingTransaction/usePendingTransaction.ts
+++ frontend/src/efi-ui/transactions/usePendingTransaction/usePendingTransaction.ts
@@ -1,37 +1,53 @@
-import { usePendingTransactionPref } from "efi-ui/prefs/usePendingTransactionPref/usePendingTransactionPref";
+import { useEffect } from "react";
+
+import { Provider } from "@ethersproject/providers";
+
+import {
+  PendingTransactionPref,
+  usePendingTransactionPref,
+} from "efi-ui/prefs/usePendingTransactionPref/usePendingTransactionPref";
 import { jsonRpcProvider } from "efi/providers/jsonRpcProviders";
-import { useEffect, useState } from "react";
-import { useInterval } from "react-use";
 
 const __DEVELOPMENT__ = process.env.NODE_ENV === "development";
 const CLEAR_PENDING_DELAY = __DEVELOPMENT__ ? 10 * 1000 : 0;
-export function usePendingTransaction(): string | undefined {
-  const { transactionHash, setTransaction } = usePendingTransactionPref();
+/**
+ * Returns a pending transaction containing the contract address, method, and
+ * hash of the last transaction the user initiated in the app.  When subscribed
+ * to, this hook will poll every 2 seconds and clear itself out when the
+ * transaction hash is found inside the next block.
+ *
+ * If you're creating a new transaction, use
+ * `useSmartContractTransactionPersisted` to ensure this pref is set.
+ */
+export function usePendingTransaction(): PendingTransactionPref {
+  const {
+    contractAddress,
+    methodName,
+    transactionHash,
+    clearPendingTransactionPref,
+  } = usePendingTransactionPref();
 
-  const [newHashValue, setNewValue] = useState(transactionHash);
+  useClearPendingTransactionOnMined(
+    transactionHash,
+    clearPendingTransactionPref,
+    jsonRpcProvider
+  );
+
+  return { contractAddress, methodName, transactionHash };
+}
+
+function useClearPendingTransactionOnMined(
+  transactionHash: string | undefined,
+  clearPendingTransactionPref: () => void,
+  provider: Provider | undefined
+) {
   useEffect(() => {
-    if (!newHashValue && transactionHash) {
-      setTimeout(() => setTransaction(undefined), CLEAR_PENDING_DELAY);
-    }
-  }, [newHashValue, setTransaction, transactionHash]);
-
-  useInterval(async () => {
     if (!transactionHash) {
       return;
     }
 
-    const transaction = await jsonRpcProvider.getTransaction(transactionHash);
-    if (!transaction) {
-      return;
-    }
-    const { blockHash } = transaction;
-
-    // if the transaction is included in a block then it was successful, so we clear the pending tx
-    // hash.
-    if (blockHash) {
-      setNewValue(undefined);
-    }
-  }, 2000);
-
-  return transactionHash;
+    provider?.once(transactionHash, () => {
+      setTimeout(() => clearPendingTransactionPref(), CLEAR_PENDING_DELAY);
+    });
+  }, [clearPendingTransactionPref, provider, transactionHash]);
 }
