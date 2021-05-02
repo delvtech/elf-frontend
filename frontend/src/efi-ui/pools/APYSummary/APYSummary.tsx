1b1b647c08bfaedcfc83cdb41cb23d2ad4f933ce
Matthew Brown <mattfdbrown@gmail.com>
2021-05-01 20:36:12 -0700

    move to their own files

--- frontend/src/efi-ui/pools/APYSummary/APYSummary.tsx
+++ frontend/src/efi-ui/pools/APYSummary/APYSummary.tsx
@@ -3,30 +3,23 @@ import React, { ReactElement } from "react";
 import { Classes } from "@blueprintjs/core";
 import classNames from "classnames";
 import { ERC20 } from "elf-contracts/types/ERC20";
-import { YVaultAssetProxy__factory } from "elf-contracts/types/factories/YVaultAssetProxy__factory";
 import { t } from "ttag";
 
 import tw from "efi-tailwindcss-classnames";
-import { getQueryData } from "efi-ui/base/queryResults";
-import { getSmartContractFromRegistry } from "efi-ui/contracts/SmartContractsRegistry";
 import { useSmartContractReadCall } from "efi-ui/contracts/useSmartContractReadCall/useSmartContractReadCall";
 import { useFeeVolumeFiatForPool } from "efi-ui/pools/useFeeVolumeForPool/useFeeVolumeForPool";
-import { useParseSortedTokensForPool } from "efi-ui/pools/useParsedTokensForPool/useParsedTokensForPool";
 import { usePoolSpotPrice } from "efi-ui/pools/usePoolSpotPrice/usePoolSpotPrice";
 import { useTotalFiatLiquidityForPool } from "efi-ui/pools/useTotalFiatLiquidityForPool.ts/useTotalFiatLiquidityForPool";
 import { useTrancheForPool } from "efi-ui/pools/useTrancheForPool/useTrancheForPool";
-import { useCurrencyPref } from "efi-ui/prefs/useCurrency/useCurencyPref";
-import { useTokenDecimals } from "efi-ui/token/hooks/useTokenDecimals";
-import { useTokenPrice } from "efi-ui/token/hooks/useTokenPrice";
 import { useTokenSymbol } from "efi-ui/token/hooks/useTokenSymbol";
 import { useYearnVault } from "efi-ui/yearn/useYearnVault";
 import { formatPercent } from "efi/base/formatPercent";
 import { ONE_DAY_IN_SECONDS, ONE_YEAR_IN_SECONDS } from "efi/base/time";
-import { convertToFiatBalance } from "efi/money/convertToFiatBalance";
 import { isWeightedPool, PoolContract } from "efi/pools/PoolContract";
 import { TermAssetType } from "efi/tranche/TermAssetType";
 import { useBaseAssetForPool } from "efi-ui/pools/useBaseAssetForPool/useBaseAssetForPool";
 import { formatMoney } from "efi/money/formatMoney";
+import { useAccumulatedFiatInterestForTranche } from "../useAccumulatedFiatInterestForTranche";
 
 interface APYSummaryProps {
   pool: PoolContract | undefined;
@@ -159,64 +152,4 @@ function useTokenYield(
 
   const tokenYield = termAssetType === "principal" ? fixedAPY : variableAPY;
   return tokenYield;
-}
-
-function useAccumulatedInterestForTranche(pool: PoolContract | undefined) {
-  const trancheContract = useTrancheForPool(pool);
-  const { data: decimals } = useTokenDecimals(trancheContract);
-
-  // this is the amount of underlying that has been deposited into the tranche.
-  const { data: balanceOfUnderlying } = useSmartContractReadCall(
-    trancheContract,
-    "valueSupplied"
-  );
-
-  const vaultAssetProxyAddress = useSmartContractReadCall(
-    trancheContract,
-    "position"
-  );
-  const yVaultAssetProxy = getSmartContractFromRegistry(
-    getQueryData(vaultAssetProxyAddress),
-    // TODO: The vault asset proxy might not necessarily by a YVaultAssetProxy, so
-    // we'll need to make a static object of well-known addresses and factory constructors.
-    YVaultAssetProxy__factory.connect
-  );
-
-  // the wrapped position has shares of a yearn vault.  this returns the base asset value of the
-  // shares that this tranche has.  the method is poorly named.
-  const { data: valueOfSharesInUnderlying } = useSmartContractReadCall(
-    yVaultAssetProxy,
-    "balanceOfUnderlying",
-    {
-      enabled: !!trancheContract,
-      callArgs: [trancheContract?.address as string],
-    }
-  );
-
-  if (!valueOfSharesInUnderlying || !balanceOfUnderlying || !decimals) {
-    return undefined;
-  }
-
-  return valueOfSharesInUnderlying.sub(balanceOfUnderlying);
-}
-
-function useAccumulatedFiatInterestForTranche(
-  baseAssetContract: ERC20 | undefined,
-  pool: PoolContract | undefined
-) {
-  const { currency } = useCurrencyPref();
-  const [baseAssetPrice] = useTokenPrice(baseAssetContract, currency);
-  const { data: baseAssetDecimals } = useTokenDecimals(baseAssetContract);
-  const accumulatedInterest = useAccumulatedInterestForTranche(pool);
-
-  let accumulatedInterestFiat;
-  if (baseAssetPrice && baseAssetDecimals && accumulatedInterest) {
-    accumulatedInterestFiat = convertToFiatBalance(
-      baseAssetPrice,
-      accumulatedInterest,
-      baseAssetDecimals
-    );
-  }
-
-  return accumulatedInterestFiat;
 }
