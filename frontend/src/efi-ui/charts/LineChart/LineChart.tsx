d3ebd23359b06078ce63ee5c1ce013261368b05c
Matthew Brown <mattfdbrown@gmail.com>
2021-05-02 23:57:29 -0700

    line chart

---
+++ frontend/src/efi-ui/charts/LineChart/LineChart.tsx
@@ -0,0 +1,60 @@
+// install (please make sure versions match peerDependencies)
+// yarn add @nivo/core @nivo/line
+import { ResponsiveLine } from "@nivo/line";
+import { ReactElement } from "react";
+// make sure parent container have a defined height when using
+// responsive component, otherwise height will be 0 and
+// no chart will be rendered.
+// website examples showcase many properties,
+// you'll often use just a few of them.
+export function LineChart({
+  data /* see data tab */,
+}: LineChartProps): ReactElement {
+  return (
+    <ResponsiveLine
+      data={data}
+      margin={{ top: 50, right: 110, bottom: 50, left: 60 }}
+      xScale={{ type: "linear" }}
+      yScale={{
+        type: "linear",
+        min: "auto",
+        max: "auto",
+        stacked: true,
+        reverse: false,
+      }}
+      yFormat=" >-.2f"
+      curve="basis"
+      axisTop={null}
+      axisRight={null}
+      axisBottom={{
+        orient: "bottom",
+        tickSize: 5,
+        tickPadding: 5,
+        tickRotation: 0,
+        legend: "transportation",
+        legendOffset: 36,
+        legendPosition: "middle",
+      }}
+      axisLeft={{
+        orient: "left",
+        tickSize: 5,
+        tickPadding: 5,
+        tickRotation: 0,
+        legend: "count",
+        legendOffset: -40,
+        legendPosition: "middle",
+      }}
+      enablePoints={false}
+      pointSize={10}
+      pointColor={{ theme: "background" }}
+      pointBorderWidth={2}
+      pointBorderColor={{ from: "serieColor" }}
+      pointLabelYOffset={-12}
+      enableArea={true}
+      areaBlendMode="multiply"
+      crosshairType="x"
+      useMesh={true}
+      legends={[]}
+    />
+  );
+}
