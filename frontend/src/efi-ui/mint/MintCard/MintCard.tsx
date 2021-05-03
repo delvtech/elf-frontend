486baa6f037f005e011f4fde59f5b240a8820ab0
Danny Delott <dannydelott@gmail.com>
2021-05-02 22:52:53 -0700

    Don't render the drawer if it's not open

--- frontend/src/efi-ui/mint/MintCard/MintCard.tsx
+++ frontend/src/efi-ui/mint/MintCard/MintCard.tsx
@@ -209,7 +209,7 @@ export function MintCard({
         </Button>
       </Card>
 
-      {!activeBaseAsset ? null : (
+      {!activeBaseAsset || !isDrawerOpen ? null : (
         <MintTransactionConfirmationDrawer
           baseAsset={activeBaseAsset}
           baseAssetIcon={baseAssetIcon}
