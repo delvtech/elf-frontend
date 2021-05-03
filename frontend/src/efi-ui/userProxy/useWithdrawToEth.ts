5830be224600d85e3a2daf28fdbe44ea23a40c93
Matthew Brown <mattfdbrown@gmail.com>
2021-05-03 06:24:36 -0700

    add permit data to call

--- frontend/src/efi-ui/userProxy/useWithdrawToEth.ts
+++ frontend/src/efi-ui/userProxy/useWithdrawToEth.ts
@@ -1,63 +1,106 @@
-import { BigNumber, BigNumberish, BytesLike, Signer } from "ethers";
-import { ContractMethodArgs } from "efi/contracts/types";
-import { UserProxy } from "elf-contracts/types/UserProxy";
-import { Tranche } from "elf-contracts/types/Tranche";
-import { useTrancheUnlockTimestamp } from "efi-ui/tranche/useTrancheUnlockTimestamp";
-import { useTranchePosition } from "efi-ui/tranche/useTranchePosition";
-import { useUserProxy } from "efi-ui/mint/hooks/userProxy";
-import { useSmartContractTransactionPersisted } from "efi-ui/transactions/useSmartContractTransactionPersisted/useSmartContractTransactionPersisted";
 import { useCallback } from "react";
 
-interface PermitCallData {
-  tokenContract: string;
-  who: string;
-  amount: BigNumberish;
-  expiration: BigNumberish;
-  r: BytesLike;
-  s: BytesLike;
-  v: BigNumberish;
-}
-export function useWithdrawInterestToEth(
+import { ERC20Permit } from "elf-contracts/types/ERC20Permit";
+import { ERC20Permit__factory } from "elf-contracts/types/factories/ERC20Permit__factory";
+import { Tranche } from "elf-contracts/types/Tranche";
+import { UserProxy } from "elf-contracts/types/UserProxy";
+import { BigNumber, Signer } from "ethers";
+
+import { fetchPermitData, PermitCallData } from "efi-ui/base/fetchPermitData";
+import { getSmartContractFromRegistry } from "efi-ui/contracts/SmartContractsRegistry";
+import { useSmartContractReadCall } from "efi-ui/contracts/useSmartContractReadCall/useSmartContractReadCall";
+import { useUserProxy } from "efi-ui/mint/hooks/userProxy";
+import { useTranchePosition } from "efi-ui/tranche/useTranchePosition";
+import { useTrancheUnlockTimestamp } from "efi-ui/tranche/useTrancheUnlockTimestamp";
+import { useSmartContractTransactionPersisted } from "efi-ui/transactions/useSmartContractTransactionPersisted/useSmartContractTransactionPersisted";
+import { ContractMethodArgs } from "efi/contracts/types";
+
+export function useWithdrawToEth(
   signer: Signer | undefined,
   tranche: Tranche | undefined,
-  amountPT: BigNumber | undefined,
-  amountYT: BigNumber | undefined
+  // note both are required.  set to BigNumber.from(0) if you don't want that one.
+  amountPrinicpalToken: BigNumber,
+  amountYieldToken: BigNumber
 ): () => void {
   const { data: expiration } = useTrancheUnlockTimestamp(tranche);
   const { data: position } = useTranchePosition(tranche);
 
-  const withdrawInterestToEthCallArgs = makeWithdrawInterestToEthCallArgs(
-    expiration,
-    position,
-    amountPT,
-    amountYT,
-    // TODO: add permit call data
-    []
+  const principalTokenContract = tranche as ERC20Permit;
+  const { data: yieldTokenAddress } = useSmartContractReadCall(
+    tranche,
+    "interestToken"
+  );
+  const yieldTokenContract = getSmartContractFromRegistry(
+    yieldTokenAddress,
+    ERC20Permit__factory.connect
   );
 
   const userProxy = useUserProxy();
-  const {
-    mutate: withdrawInterestToEth,
-  } = useSmartContractTransactionPersisted(userProxy, "withdrawWeth", signer);
+  const { mutate: withdrawToEth } = useSmartContractTransactionPersisted(
+    userProxy,
+    "withdrawWeth",
+    signer
+  );
 
-  return useCallback(() => {
-    if (!withdrawInterestToEthCallArgs) {
+  return useCallback(async () => {
+    if (
+      !signer ||
+      !userProxy ||
+      !expiration ||
+      !position ||
+      !amountPrinicpalToken ||
+      !amountYieldToken ||
+      !yieldTokenContract
+    ) {
       return;
     }
-    withdrawInterestToEth(withdrawInterestToEthCallArgs);
-  }, [withdrawInterestToEth, withdrawInterestToEthCallArgs]);
+
+    const signerAddress = await signer.getAddress();
+    const ptPermitData = await fetchPermitData(
+      signer,
+      principalTokenContract,
+      signerAddress,
+      userProxy.address,
+      amountPrinicpalToken,
+      "1"
+    );
+    const ytPermitData = await fetchPermitData(
+      signer,
+      yieldTokenContract,
+      signerAddress,
+      userProxy.address,
+      amountYieldToken,
+      "1"
+    );
+
+    const withdrawInterestToEthCallArgs = makeWithdrawInterestToEthCallArgs(
+      expiration,
+      position,
+      amountPrinicpalToken,
+      amountYieldToken,
+      [ptPermitData, ytPermitData]
+    );
+
+    withdrawToEth(withdrawInterestToEthCallArgs);
+  }, [
+    amountPrinicpalToken,
+    amountYieldToken,
+    expiration,
+    position,
+    principalTokenContract,
+    signer,
+    userProxy,
+    withdrawToEth,
+    yieldTokenContract,
+  ]);
 }
 export function makeWithdrawInterestToEthCallArgs(
-  expiration: BigNumber | undefined,
-  position: string | undefined,
-  amountPT: BigNumber | undefined,
-  amountYT: BigNumber | undefined,
+  expiration: BigNumber,
+  position: string,
+  amountPT: BigNumber,
+  amountYT: BigNumber,
   permitCallData: PermitCallData[]
-): ContractMethodArgs<UserProxy, "withdrawWeth"> | undefined {
-  if (!expiration || !position || !amountPT || !amountYT) {
-    return undefined;
-  }
-
+): ContractMethodArgs<UserProxy, "withdrawWeth"> {
   const callArgs: ContractMethodArgs<UserProxy, "withdrawWeth"> = [
     expiration,
     position,
