d9181804f08166d4645d4c087877b2768b46f6ce
Matthew Brown <mattfdbrown@gmail.com>
2021-05-07 16:05:40 -0500

    hook up to usePoolSpotPrice

--- frontend/src/efi-ui/balancer/useQueryBatchSwap/useQueryBatchSwap.ts
+++ frontend/src/efi-ui/balancer/useQueryBatchSwap/useQueryBatchSwap.ts
@@ -1,27 +1,29 @@
 import { QueryObserverResult } from "react-query";
 
+import { ConvergentCurvePool } from "elf-contracts/types/ConvergentCurvePool";
 import { BigNumber } from "ethers";
+import { formatEther, formatUnits } from "ethers/lib/utils";
 
 import { SwapKind } from "efi-ui/balancer/SwapKind";
 import { useBalancerVault } from "efi-ui/balancer/useBalancerVault";
 import { makeQueryBatchSwapCallArgs } from "efi-ui/balancer/useQueryBatchSwap/makeQueryBatchSwapCallArgs";
 import { getQueryData } from "efi-ui/base/queryResults";
 import { useSmartContractReadCall } from "efi-ui/contracts/useSmartContractReadCall/useSmartContractReadCall";
+import { usePoolTokens } from "efi-ui/pools/usePoolTokens/usePoolTokens";
+import { useTokenDecimals } from "efi-ui/token/hooks/useTokenDecimals";
+import {
+  calcSwapInGivenOutCCPoolUNSAFE,
+  calcSwapInGivenOutWeightedPoolUNSAFE,
+  calcSwapOutGivenInCCPoolUNSAFE,
+  calcSwapOutGivenInWeightedPoolUNSAFE,
+} from "efi/pools/calcPoolSwap";
+import { parseSortedTokensForPool } from "efi/pools/parseSortedTokensForPool";
 import {
   isConvergentCurvePool,
   isWeightedPool,
   PoolContract,
 } from "efi/pools/PoolContract";
-import { usePoolTokens } from "efi-ui/pools/usePoolTokens/usePoolTokens";
-import { parseSortedTokensForPool } from "efi/pools/parseSortedTokensForPool";
-import {
-  calcSwapInGivenOutWeightedPoolUNSAFE,
-  calcSwapOutGivenInCCPoolUNSAFE,
-  calcSwapOutGivenInWeightedPoolUNSAFE,
-} from "efi/pools/calcPoolSwap";
-import { useTokenDecimals } from "efi-ui/token/hooks/useTokenDecimals";
-import { formatEther, formatUnits } from "ethers/lib/utils";
-import { ConvergentCurvePool } from "elf-contracts/types/ConvergentCurvePool";
+import { clipStringValueToDecimals } from "efi/math/fixedPoint";
 
 /**
  * Useful for previewing a swap in the balancer V2 vault.
@@ -37,7 +39,7 @@ import { ConvergentCurvePool } from "elf-contracts/types/ConvergentCurvePool";
  * incur additional fees and costs. To check spot price, use usePoolSpotPrice
  * instead.
  */
-export function useQueryBatchSwapOld(
+export function useQueryBatchSwap(
   kind: SwapKind,
   pool: PoolContract | undefined,
   tokenInAddress: string | undefined,
@@ -69,13 +71,13 @@ export function useQueryBatchSwapOld(
   return queryBatchSwapResults;
 }
 
-export function useQueryBatchSwap(
+export function useQueryBatchSwapNew(
   kind: SwapKind,
   pool: PoolContract | undefined,
   tokenInAddress: string | undefined,
   tokenOutAddress: string | undefined,
   amount: BigNumber | undefined
-): number | undefined {
+): [string, string] | undefined {
   const { data } = usePoolTokens(pool);
   const [tokens, balances] = data ?? [[], []];
   const { baseAssetContract } = parseSortedTokensForPool(tokens);
@@ -111,26 +113,10 @@ export function useQueryBatchSwap(
     }
   );
 
-  if (
-    !amount ||
-    !tokenInAddress ||
-    !tokenOutAddress ||
-    !totalSupplyBN ||
-    !unitSecondsBN ||
-    !expirationBN
-  ) {
+  // do weighted pools first since they don't need as many variables
+  if (!amount || !tokenInAddress || !tokenOutAddress || !decimals) {
     return undefined;
   }
-
-  const amountIn = formatUnits(amount, decimals);
-  const amountOut = formatUnits(amount, decimals);
-
-  const nowInSeconds = Math.round(Date.now() / 1000);
-  const timeRemainingSeconds = expirationBN
-    ? expirationBN.toNumber() - nowInSeconds
-    : 0;
-  const tParamSeconds = unitSecondsBN?.toNumber() ?? 1;
-
   const tokenInReserves = formatUnits(
     balancesByAddress[tokenInAddress],
     decimals
@@ -141,10 +127,55 @@ export function useQueryBatchSwap(
     decimals
   );
 
+  const amountIn = formatUnits(amount, decimals);
+  const amountOut = formatUnits(amount, decimals);
+
+  if (isWPool && kind === SwapKind.GIVEN_IN) {
+    const calcOutNumber = calcSwapOutGivenInWeightedPoolUNSAFE(
+      amountIn,
+      tokenOutReserves,
+      tokenInReserves
+    );
+    const calcOut =
+      clipStringValueToDecimals(calcOutNumber.toString(), decimals) ?? "0";
+
+    return [amountIn, calcOut];
+  }
+
+  if (isWPool && kind === SwapKind.GIVEN_OUT) {
+    const calcInNumber = calcSwapInGivenOutWeightedPoolUNSAFE(
+      amountOut,
+      tokenOutReserves,
+      tokenInReserves
+    );
+    const calcIn =
+      clipStringValueToDecimals(calcInNumber.toString(), decimals) ?? "0";
+
+    return [calcIn, amountOut];
+  }
+
+  if (
+    !amount ||
+    !tokenInAddress ||
+    !tokenOutAddress ||
+    !totalSupplyBN ||
+    !unitSecondsBN ||
+    !expirationBN ||
+    !decimals
+  ) {
+    return undefined;
+  }
+
+  const nowInSeconds = Math.round(Date.now() / 1000);
+  const timeRemainingSeconds = expirationBN
+    ? expirationBN.toNumber() - nowInSeconds
+    : 0;
+  const tParamSeconds = unitSecondsBN?.toNumber() ?? 1;
+
   const totalSupply = formatEther(totalSupplyBN);
 
   if (isCCPool && kind === SwapKind.GIVEN_IN) {
-    const out = calcSwapOutGivenInCCPoolUNSAFE(
+    const calcOutNumber = calcSwapOutGivenInCCPoolUNSAFE(
       amountIn, // xAmount,
       tokenInReserves, // xReserves,
       tokenOutReserves, // yReserves,
@@ -153,11 +184,15 @@ export function useQueryBatchSwap(
       tParamSeconds, // tParamSeconds,
       tokenInAddress === baseAssetContract?.address // baseAssetIn
     );
-    return out;
+
+    const calcOut =
+      clipStringValueToDecimals(calcOutNumber.toString(), decimals) ?? "0";
+
+    return [amountIn, calcOut];
   }
 
   if (isCCPool && kind === SwapKind.GIVEN_OUT) {
-    const out = calcSwapOutGivenInCCPoolUNSAFE(
+    const calcInNumber = calcSwapInGivenOutCCPoolUNSAFE(
       amountOut, // xAmount,
       tokenInReserves, // xReserves,
       tokenOutReserves, // yReserves,
@@ -166,26 +201,12 @@ export function useQueryBatchSwap(
       tParamSeconds, // tParamSeconds,
       tokenInAddress === baseAssetContract?.address // baseAssetIn
     );
-    return out;
+
+    const calcIn =
+      clipStringValueToDecimals(calcInNumber.toString(), decimals) ?? "0";
+    return [calcIn, amountOut];
   }
 
-  if (isWPool && kind === SwapKind.GIVEN_IN) {
-    const out = calcSwapOutGivenInWeightedPoolUNSAFE(
-      amountIn,
-      tokenOutReserves,
-      tokenInReserves
-    );
-    return out;
-  }
-
-  if (isWPool && kind === SwapKind.GIVEN_OUT) {
-    const out = calcSwapInGivenOutWeightedPoolUNSAFE(
-      amountOut,
-      tokenOutReserves,
-      tokenInReserves
-    );
-    return out;
-  }
   // const queryBatchSwapResults = useSmartContractReadCall(
   //   balancerVault,
   //   "queryBatchSwap",
