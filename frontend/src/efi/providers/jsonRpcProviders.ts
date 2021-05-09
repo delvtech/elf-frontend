4a72fe3ad7639227e93e16f577071c89741b3b49
Matthew Brown <mattfdbrown@gmail.com>
2021-05-09 15:19:43 -0500

    fix tests

--- frontend/src/efi/providers/jsonRpcProviders.ts
+++ frontend/src/efi/providers/jsonRpcProviders.ts
@@ -8,12 +8,24 @@ import { providers } from "ethers";
 // final say
 const LOCAL_RPC_HOST = "http://127.0.0.1:8545";
 export const localhostProvider = new providers.JsonRpcProvider(LOCAL_RPC_HOST);
-export const alchemyProvider = new AlchemyWebSocketProvider(
-  AddressesJson.chainId,
-  process.env.REACT_APP_GOERLI_ALCHEMY_KEY
-);
+export const jsonRpcProvider = getProvider();
 
-export const jsonRpcProvider =
-  AddressesJson.chainId === ChainId.GOERLI
-    ? alchemyProvider
-    : localhostProvider;
+function getProvider() {
+  // always use localhostProvider for tests
+  if (process.env.NODE_ENV === "test") {
+    return localhostProvider;
+  }
+
+  // otherwise, if a chain id is provided, we'll use the corresponding alchemy provider.  right now
+  // this is only goerli.
+  if (AddressesJson.chainId === ChainId.GOERLI) {
+    const alchemyProvider = new AlchemyWebSocketProvider(
+      AddressesJson.chainId,
+      process.env.REACT_APP_GOERLI_ALCHEMY_KEY
+    );
+    return alchemyProvider;
+  }
+
+  // default to localhost
+  return localhostProvider;
+}
