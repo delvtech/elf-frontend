9015cf8901b5aee4311b5362340416f3b39fca92
Matthew Brown <mattfdbrown@gmail.com>
2021-05-10 13:26:34 -0500

    use alchemy's websocket provider

--- frontend/src/efi/providers/jsonRpcProviders.ts
+++ frontend/src/efi/providers/jsonRpcProviders.ts
@@ -1,15 +1,23 @@
-import { AlchemyWebSocketProvider } from "@ethersproject/providers";
+import { Provider } from "@ethersproject/providers";
+import { providers } from "ethers";
 
 import { AddressesJson } from "efi/addresses";
 import { ChainId } from "efi/ethereum";
-import { providers } from "ethers";
+
+const LOCAL_RPC_HOST = "http://127.0.0.1:8545";
+const ALCHEMY_GOERLI_KEY = process.env.REACT_APP_GOERLI_ALCHEMY_KEY as string;
+export const localhostProvider = new providers.JsonRpcProvider(LOCAL_RPC_HOST);
+
+// TODO: improve on this to dynamically grab the correct chainid/key.  hardcoded to goerli for now.
+export const alchemyWebSocketProvider = new providers.AlchemyWebSocketProvider(
+  ChainId.GOERLI,
+  ALCHEMY_GOERLI_KEY
+);
+
+export const jsonRpcProvider = getProvider();
 
 // Default rpc host to local, but check the chain id in the addresses.json for
 // final say
-const LOCAL_RPC_HOST = "http://127.0.0.1:8545";
-export const localhostProvider = new providers.JsonRpcProvider(LOCAL_RPC_HOST);
-export const jsonRpcProvider = getProvider();
-
 function getProvider() {
   // always use localhostProvider for tests
   if (process.env.NODE_ENV === "test") {
@@ -19,11 +27,7 @@ function getProvider() {
   // otherwise, if a chain id is provided, we'll use the corresponding alchemy provider.  right now
   // this is only goerli.
   if (AddressesJson.chainId === ChainId.GOERLI) {
-    const alchemyProvider = new AlchemyWebSocketProvider(
-      AddressesJson.chainId,
-      process.env.REACT_APP_GOERLI_ALCHEMY_KEY
-    );
-    return alchemyProvider;
+    return alchemyWebSocketProvider as Provider;
   }
 
   // default to localhost
