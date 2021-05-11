153cc5e74df57f02965aa644f7aba85d44e50e56
Danny Delott <dannydelott@gmail.com>
2021-05-10 17:45:49 -0700

    Add object for tranches by base asset

--- frontend/src/efi/tranche/tranches.ts
+++ frontend/src/efi/tranche/tranches.ts
@@ -5,11 +5,25 @@ import { principalTokenInfos, yieldTokenInfos } from "efi/tokenlists";
 import { Tranche } from "elf-contracts/types/Tranche";
 import { InterestToken__factory } from "elf-contracts/types/factories/InterestToken__factory";
 import { InterestToken } from "elf-contracts/types/InterestToken";
+import { mapValues } from "lodash";
 
 export const TrancheContracts = getSmartContractFromRegistryMulti(
   principalTokenInfos.map(({ address }) => address),
   Tranche__factory.connect
 ) as Tranche[];
+
+export const TrancheContractsByBaseAsset = mapValues(
+  PrincipalTokenInfosByBaseAsset,
+  (principalTokenInfos) => {
+    return principalTokenInfos.map(
+      ({ address }) =>
+        getSmartContractFromRegistry(
+          address,
+          Tranche__factory.connect
+        ) as Tranche
+    );
+  }
+);
 
 export const InterestTokenContracts = getSmartContractFromRegistryMulti(
   yieldTokenInfos.map(({ address }) => address),
