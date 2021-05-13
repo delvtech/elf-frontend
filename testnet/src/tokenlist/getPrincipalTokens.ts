9a5c3967375fe1d957527e6d370099ae8a99c228
Danny Delott <dannydelott@gmail.com>
2021-05-12 18:40:58 -0700

    Run prettier

--- testnet/src/tokenlist/getPrincipalTokens.ts
+++ testnet/src/tokenlist/getPrincipalTokens.ts
@@ -3,43 +3,77 @@ import hre from "hardhat";
 import zip from "lodash.zip";
 
 import { Tranche__factory } from "src/types/factories/Tranche__factory";
-import {
-  TrancheFactory__factory,
-} from "src/types/factories/TrancheFactory__factory";
+import { TrancheFactory__factory } from "src/types/factories/TrancheFactory__factory";
 
 import { ERC20__factory } from "src/types/factories/ERC20__factory";
 import { Tranche } from "src/types/Tranche";
 import { TokenListTag } from "src/tokenlist/tags";
 
 export const provider = hre.ethers.provider;
-export async function getPrincipalTokens(trancheFactoryAddress: string, chainId: number, safelist: string[]) {
-  const trancheFactory = TrancheFactory__factory.connect(trancheFactoryAddress, provider);
+export async function getPrincipalTokens(
+  trancheFactoryAddress: string,
+  chainId: number,
+  safelist: string[]
+) {
+  const trancheFactory = TrancheFactory__factory.connect(
+    trancheFactoryAddress,
+    provider
+  );
   const filter = trancheFactory.filters.TrancheCreated(null, null, null);
   const events = await trancheFactory.queryFilter(filter);
-  const trancheAddresses = (events.map(
+  const trancheAddresses = events.map(
     (event) =>
       // The first arg is the trancheAddress
       event.args?.[0]
-  ) as string[]);
-  const safeTrancheAddresses = trancheAddresses.filter(address => safelist.includes(address));
+  ) as string[];
+  const safeTrancheAddresses = trancheAddresses.filter((address) =>
+    safelist.includes(address)
+  );
 
-  const safeTranches = safeTrancheAddresses.map((address) => Tranche__factory.connect(address, provider));
+  const safeTranches = safeTrancheAddresses.map((address) =>
+    Tranche__factory.connect(address, provider)
+  );
 
   // Create the principal token name, eg: "ETH Principal Token"
   const principalTokenNames = await getPrincipalTokenName(safeTranches);
   // Create the principal token symbol, eg: "eP-ELFyWETH"
   const principalTokenSymbols = await getPrincipalTokenSymbols(safeTranches);
-  const decimals = await Promise.all(safeTranches.map(tranche => tranche.decimals()));
-  const underlyingAddresses = await Promise.all(safeTranches.map(tranche => tranche.underlying()));
-  const unlockTimestamps = await Promise.all(safeTranches.map(tranche => tranche.unlockTimestamp()));
-  const interestTokens = await Promise.all(safeTranches.map(tranche => tranche.interestToken()));
-  const positions = await Promise.all(safeTranches.map(tranche => tranche.position()));
-
-
+  const decimals = await Promise.all(
+    safeTranches.map((tranche) => tranche.decimals())
+  );
+  const underlyingAddresses = await Promise.all(
+    safeTranches.map((tranche) => tranche.underlying())
+  );
+  const unlockTimestamps = await Promise.all(
+    safeTranches.map((tranche) => tranche.unlockTimestamp())
+  );
+  const interestTokens = await Promise.all(
+    safeTranches.map((tranche) => tranche.interestToken())
+  );
+  const positions = await Promise.all(
+    safeTranches.map((tranche) => tranche.position())
+  );
 
   const principalTokensList: TokenInfo[] = zip<any>(
-    safeTrancheAddresses, principalTokenSymbols, principalTokenNames, decimals, underlyingAddresses, unlockTimestamps, interestTokens, positions)
-    .map(([address, symbol, name, decimal, underlying, unlockTimestamp, interestToken, position]): TokenInfo => {
+    safeTrancheAddresses,
+    principalTokenSymbols,
+    principalTokenNames,
+    decimals,
+    underlyingAddresses,
+    unlockTimestamps,
+    interestTokens,
+    positions
+  ).map(
+    ([
+      address,
+      symbol,
+      name,
+      decimal,
+      underlying,
+      unlockTimestamp,
+      interestToken,
+      position,
+    ]): TokenInfo => {
       return {
         chainId,
         address: address as string,
@@ -56,30 +90,42 @@ export async function getPrincipalTokens(trancheFactoryAddress: string, chainId:
         // TODO: What logo do we want to show for interest tokens?
         // logoURI: ""
       };
-    });
+    }
+  );
 
   return { tranches: safeTranches, principalTokensList };
-
 }
 async function getPrincipalTokenSymbols(tranches: Tranche[]) {
-    const underlyingAddresses = await Promise.all(tranches.map(tranche => tranche.underlying()));
-    const underlyingContracts = underlyingAddresses.map(address => ERC20__factory.connect(address, provider));
-    const underlyingSymbols = await Promise.all(underlyingContracts.map(underlying => underlying.symbol()));
-    const principalTokenSymbols = underlyingSymbols.map(symbol => {
-        const name = symbol === 'WETH' ? 'eP-ETH' : `eP-${symbol}`;
-        return name;
-    });
-    return principalTokenSymbols;
+  const underlyingAddresses = await Promise.all(
+    tranches.map((tranche) => tranche.underlying())
+  );
+  const underlyingContracts = underlyingAddresses.map((address) =>
+    ERC20__factory.connect(address, provider)
+  );
+  const underlyingSymbols = await Promise.all(
+    underlyingContracts.map((underlying) => underlying.symbol())
+  );
+  const principalTokenSymbols = underlyingSymbols.map((symbol) => {
+    const name = symbol === "WETH" ? "eP-ETH" : `eP-${symbol}`;
+    return name;
+  });
+  return principalTokenSymbols;
 }
 
 async function getPrincipalTokenName(tranches: Tranche[]) {
-    const underlyingAddresses = await Promise.all(tranches.map(tranche => tranche.underlying()));
-    const underlyingContracts = underlyingAddresses.map(address => ERC20__factory.connect(address, provider));
-    const underlyingSymbols = await Promise.all(underlyingContracts.map(underlying => underlying.symbol()));
-    const principalTokenNames = underlyingSymbols.map(symbol => {
-        const name = symbol === 'WETH' ? 'ETH Principal Token' : `${symbol} Principal Token`;
-        return name;
-    });
-    return principalTokenNames;
+  const underlyingAddresses = await Promise.all(
+    tranches.map((tranche) => tranche.underlying())
+  );
+  const underlyingContracts = underlyingAddresses.map((address) =>
+    ERC20__factory.connect(address, provider)
+  );
+  const underlyingSymbols = await Promise.all(
+    underlyingContracts.map((underlying) => underlying.symbol())
+  );
+  const principalTokenNames = underlyingSymbols.map((symbol) => {
+    const name =
+      symbol === "WETH" ? "ETH Principal Token" : `${symbol} Principal Token`;
+    return name;
+  });
+  return principalTokenNames;
 }
-
