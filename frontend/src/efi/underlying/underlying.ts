fb927102241e54ab318b563be206e7cfaa97f2d1
Danny Delott <dannydelott@gmail.com>
2021-05-18 11:35:49 -0700

    Refactor EarnCard away from usePoolPairedToken in favor of tokenlist

---
+++ frontend/src/efi/underlying/underlying.ts
@@ -0,0 +1,25 @@
+import { getSmartContractFromRegistry } from "efi/contracts/SmartContractsRegistry";
+import { AddressesJson } from "efi/addresses";
+import { WETH__factory } from "elf-contracts/types/factories/WETH__factory";
+import { USDC__factory } from "elf-contracts/types/factories/USDC__factory";
+
+const {
+  addresses: { wethAddress, usdcAddress },
+} = AddressesJson;
+
+const wethContract = getSmartContractFromRegistry(
+  wethAddress,
+  WETH__factory.connect
+);
+const usdcContract = getSmartContractFromRegistry(
+  usdcAddress,
+  USDC__factory.connect
+);
+
+/**
+ * Lookup the contract instance for a underlying's address.
+ */
+export const UnderlyingContracts = Object.freeze({
+  [wethAddress]: wethContract,
+  [usdcAddress]: usdcContract,
+});
