32a46a6c13e093ab64b2b0b1d31fd1394a816c26
Danny Delott <dannydelott@gmail.com>
2021-06-03 21:48:27 -0700

    Wire up current apy to Buy tab

--- frontend/src/efi-ui/save/SavePortfolioList/BuyPrincipalTokensForm.tsx
+++ frontend/src/efi-ui/save/SavePortfolioList/BuyPrincipalTokensForm.tsx
@@ -13,6 +13,14 @@ import { Web3Provider } from "@ethersproject/providers";
 import { formatBalance } from "efi/base/formatBalance";
 import { findAssetIcon2 } from "efi-ui/crypto/CryptoIcon";
 import { getCryptoSymbol } from "efi/crypto/getCryptoSymbol";
+import { useTokenYield } from "efi-ui/pools/useTokenYield";
+import { underlyingContracts } from "efi/underlying/underlying";
+import {
+  getPrincipalPoolForTranche,
+  principalPoolContracts,
+  principalPoolContractsByAddress,
+} from "efi/pools/ccpool";
+import { formatPercent } from "efi/base/formatPercent";
 
 interface BuyPrincipalTokensFormProps {
   library: Web3Provider | undefined;
@@ -27,12 +35,24 @@ export function BuyPrincipalTokensForm(
     library,
     account,
     principalToken: {
+      address: ptAddress,
       extensions: { underlying },
     },
   } = props;
   // inputs
   const { stringValue: baseAssetInputValue, setValue: onBaseAssetChange } =
     useNumericInput();
+
+  const baseAssetContract = underlyingContracts[underlying];
+  const principalPool = getPrincipalPoolForTranche(ptAddress);
+  const principalPoolContract =
+    principalPoolContractsByAddress[principalPool.address];
+  const apy = useTokenYield(
+    baseAssetContract,
+    principalPoolContract,
+    "principal"
+  );
+  const formattedAPY = apy ? formatPercent(apy) : "-";
 
   // base asset
   const baseAsset = getCryptoAssetForToken(underlying);
@@ -60,6 +80,7 @@ export function BuyPrincipalTokensForm(
         <span
           className={tw("pb-4")}
         >{t`Buy principal tokens with your ${baseAssetSymbol}`}</span>
+        <span className={tw("pb-4")}>{t`Current APY: ${formattedAPY}`}</span>
         <div className={tw("grid", "grid-cols-4", "gap-3")}>
           <SaveInput
             swapKind={SwapKind.GIVEN_IN}
