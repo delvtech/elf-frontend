b6baf900e3d9816f7f06e9ff6982d94b6b861b11
Danny Delott <dannydelott@gmail.com>
2021-06-18 09:40:50 -0700

    Add save app to local dev setup (#1309)

--- frontend/scripts/start.sh
+++ frontend/scripts/start.sh
@@ -2,4 +2,5 @@ set -e
 
 # Purposefully not exporting this env variable because we want vanilla
 # `npm start` to still work without having to specify "testnet".
+export REACT_APP_ELEMENT_APP_ID=$2
 REACT_APP_CHAIN_NAME=$1 npm start
