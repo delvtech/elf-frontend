1aba64c980efd99ca6ec359515f6ce53cbf0720d
Matthew Brown <mattfdbrown@gmail.com>
2021-06-24 11:11:44 -0700

    remove deprecated method

--- frontend/src/efi/base/time.ts
+++ frontend/src/efi/base/time.ts
@@ -1,5 +1,3 @@
-import { formatDuration, intervalToDuration } from "date-fns";
-
 export const ONE_MINUTE_IN_SECONDS = 60;
 export const ONE_HOUR_IN_SECONDS = 60 * ONE_MINUTE_IN_SECONDS;
 export const ONE_DAY_IN_SECONDS = 24 * ONE_HOUR_IN_SECONDS;
@@ -13,24 +11,3 @@ export const ONE_DAY_IN_MILLISECONDS = 24 * ONE_HOUR_IN_MILLISECONDS;
 export const ONE_WEEK_IN_MILLISECONDS = 7 * ONE_DAY_IN_MILLISECONDS;
 export const ONE_YEAR_IN_MILLISECONDS = 365 * ONE_DAY_IN_MILLISECONDS;
 export const THIRTY_DAYS_IN_MILLISECONDS = 30 * ONE_DAY_IN_MILLISECONDS;
-
-/**
- * @deprecated use `formatTimeLeft` instead
- */
-export function getTimeLeft(
-  maturationDate: Date | undefined
-): string | undefined {
-  if (!maturationDate) {
-    return;
-  }
-
-  const duration = intervalToDuration({
-    start: Date.now(),
-    end: maturationDate.getTime(),
-  });
-
-  return formatDuration(duration, {
-    delimiter: ", ",
-    format: ["years", "months", "days"],
-  });
-}
