2f90dfed78501eefd8785476d16fe031ed30b626
Matthew Brown <mattfdbrown@gmail.com>
2021-06-27 22:28:19 -0500

    update TVL calc (#1409)

--- frontend/src/efi-ui/pools/useTotalValueLockedForTranche.tsx
+++ frontend/src/efi-ui/pools/useTotalValueLockedForTranche.tsx
@@ -1,11 +1,19 @@
 import { ERC20 } from "elf-contracts/types/ERC20";
 import { formatUnits } from "ethers/lib/utils";
-import { PrincipalTokenInfo } from "tokenlists/types";
+import { PrincipalTokenInfo, YieldPoolTokenInfo } from "tokenlists/types";
 import { Money } from "ts-money";
 
 import { useSmartContractReadCall } from "efi-ui/contracts/useSmartContractReadCall/useSmartContractReadCall";
+import { useAccumulatedInterestForTranche } from "efi-ui/pools/useAccumulatedInterestForTranche";
+import { usePoolTokens } from "efi-ui/pools/usePoolTokens/usePoolTokens";
 import { useCurrencyPref } from "efi-ui/prefs/useCurrency/useCurencyPref";
 import { useTokenPrice } from "efi-ui/token/hooks/useTokenPrice";
+import { getPoolInfoForPrincipalToken } from "efi/pools/ccpool";
+import { getPoolContract } from "efi/pools/getPoolContract";
+import { getPoolTokens } from "efi/pools/getPoolTokens";
+import { PoolInfo } from "efi/pools/PoolInfo";
+import { getPoolForYieldToken } from "efi/pools/weightedPool";
+import { getTokenInfo } from "efi/tokenlists";
 import { trancheContractsByAddress } from "efi/tranche/tranches";
 
 export function useTotalValueLockedForTranche(
@@ -14,23 +22,67 @@ export function useTotalValueLockedForTranche(
 ): Money | undefined {
   const { address, decimals } = trancheInfo;
   const tranche = trancheContractsByAddress[address];
+  const poolInfo = getPoolInfoForPrincipalToken(address);
+  const { address: yieldPoolAddress } = getPoolForYieldToken(
+    trancheInfo.extensions.interestToken
+  );
+  const yieldPoolInfo = getTokenInfo<YieldPoolTokenInfo>(yieldPoolAddress);
 
+  // get all components of TVL: base asset in tranche, base asset in pool, accumulated interest for tranche
   const { data: baseAssetLockedBN } = useSmartContractReadCall(
     tranche,
     "valueSupplied"
   );
+  const accumulatedInterestBN = useAccumulatedInterestForTranche(poolInfo);
+  const baseReservesInPrincipalPoolBN = useBaseAssetReservesInPool(
+    poolInfo,
+    decimals
+  );
+  const baseReservesInYieldPoolBN = useBaseAssetReservesInPool(
+    yieldPoolInfo,
+    decimals
+  );
+
   const { currency } = useCurrencyPref();
   const [baseAssetPrice] = useTokenPrice(baseAssetContract, currency);
 
-  if (!baseAssetLockedBN) {
+  if (
+    !baseAssetLockedBN ||
+    !accumulatedInterestBN ||
+    !baseReservesInPrincipalPoolBN ||
+    !baseReservesInYieldPoolBN
+  ) {
     return undefined;
   }
 
+  // convert to numbers
   const baseAssetLocked = +formatUnits(baseAssetLockedBN, decimals);
-  const totalValueLocked = baseAssetPrice?.multiply(
-    baseAssetLocked,
+  const accumulatedInterest = +formatUnits(accumulatedInterestBN, decimals);
+  const baseReservesInPrincipalPool = +formatUnits(
+    baseReservesInPrincipalPoolBN,
+    decimals
+  );
+  const baseReservesInYieldPool = +formatUnits(
+    baseReservesInYieldPoolBN,
+    decimals
+  );
+
+  // get total, convert to fiat
+  const totalValueLocked =
+    baseAssetLocked +
+    accumulatedInterest +
+    baseReservesInPrincipalPool +
+    baseReservesInYieldPool;
+  const totalFiatValueLocked = baseAssetPrice?.multiply(
+    totalValueLocked,
     Math.round
   );
 
-  return totalValueLocked;
+  return totalFiatValueLocked;
+}
+function useBaseAssetReservesInPool(poolInfo: PoolInfo, decimals: number) {
+  const pool = getPoolContract(poolInfo.address);
+  const { data: [, balances] = [undefined, undefined] } = usePoolTokens(pool);
+  const { baseAssetIndex } = getPoolTokens(poolInfo);
+  return balances?.[baseAssetIndex];
 }
