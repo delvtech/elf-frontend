cda228a1a7339487b621d5d32746ce702ea339d7
Ryan Goree <goree.ryan@gmail.com>
2021-11-28 05:56:00 -0600

    Add svg loader

--- src/efi-ui/wallets/ConnectWalletButtons/ConnectWalletButtons.tsx
+++ src/efi-ui/wallets/ConnectWalletButtons/ConnectWalletButtons.tsx
@@ -7,12 +7,12 @@ import { useWeb3React } from "@web3-react/core";
 import { WalletConnectConnector } from "@web3-react/walletconnect-connector";
 import { t } from "ttag";
 
-import { ReactComponent as CoinbaseIcon } from "efi-static-assets/logos/coinbasewallet.svg";
-import { ReactComponent as LedgerIcon } from "efi-static-assets/logos/ledgerIcon.svg";
-import { ReactComponent as MetamaskIcon } from "efi-static-assets/logos/metamask.svg";
-import { ReactComponent as LatticeIcon } from "efi-static-assets/logos/svg/grid-plus-logo-white-blue.svg";
-import { ReactComponent as TrezorIcon } from "efi-static-assets/logos/svg/trezor-dark.svg";
-import { ReactComponent as WalletConnectIcon } from "efi-static-assets/logos/walletConnectIcon.svg";
+import CoinbaseIcon from "efi-static-assets/logos/coinbasewallet.svg";
+import LedgerIcon from "efi-static-assets/logos/ledgerIcon.svg";
+import MetamaskIcon from "efi-static-assets/logos/metamask.svg";
+import LatticeIcon from "efi-static-assets/logos/svg/grid-plus-logo-white-blue.svg";
+import TrezorIcon from "efi-static-assets/logos/svg/trezor-dark.svg";
+import WalletConnectIcon from "efi-static-assets/logos/walletConnectIcon.svg";
 import tw from "efi-tailwindcss-classnames";
 import {
   coinbaseConnector,
