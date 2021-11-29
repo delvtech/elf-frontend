046bbea465b36a950aef6e6c5592972e380555a9
Ryan Goree <goree.ryan@gmail.com>
2021-11-29 16:04:25 -0600

    Convert long number to string

--- src/efi/tranche/calculateTrancheAPY.test.ts
+++ src/efi/tranche/calculateTrancheAPY.test.ts
@@ -9,12 +9,12 @@ test("it should calculate the tranche apy on a one year tranche", () => {
   const end = ONE_YEAR_IN_MILLISECONDS;
   // simple math: selling dollars for 98 cents, redeemable in one year = 2%.
   const apy = calculateTrancheAPY(0.98, start, end);
-  expect(apy).toEqual(0.020000000000000018);
+  expect(apy.toString()).toEqual("0.020000000000000018");
 });
 
 test("it should calculate the tranche apy on a 4 week tranche", () => {
   const start = 0;
   const end = ONE_WEEK_IN_MILLISECONDS * 4;
   const apy = calculateTrancheAPY(0.98, start, end);
-  expect(apy).toEqual(0.26071428571428594);
+  expect(apy.toString()).toEqual("0.26071428571428594");
 });
