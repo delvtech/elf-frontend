d3a88e6bd7f7f7d16a5ae67281833ff82fb5eddb
Duke <hnguyen1179@berkeley.edu>
2021-11-29 15:24:35 -0800

    Fixed copy script to only copy over testnet jsons

--- scripts/copy-addresses.sh
+++
@@ -1,6 +0,0 @@
-# Copy over all the addresses.json files from the top-level directory into the frontend
-rm -f src/addresses/goerli.addresses.json src/addresses/testnet.addresses.json src/addresses/mainnet.addresses.json
-find ../elf-frontend-testnet/src/addresses/ -type f -name "*.addresses.json" -exec cp {} src/addresses/ \;
-
-# Copy the typescript definition file to the frontend
-cp -f ../elf-frontend-testnet/src/addresses/AddressesJsonFile.d.ts src/addresses/
--- scripts/copy-tokenlists.sh
+++
@@ -1,7 +0,0 @@
-# Remove files from frontend to make this a clean copy
-rm -f src/tokenlists/goerli.tokenlist.json src/tokenlists/testnet.tokenlist.json src/tokenlists/mainnet.tokenlist.json
-rm -f src/tokenlists/types.d.ts
-
-# Copy the tokenlist.json files and TS definitions to the frontend
-find ../elf-frontend-testnet/src/tokenlist/ -type f -name "*.tokenlist.json" -exec cp {} src/tokenlists/ \;
-cp ../elf-frontend-testnet/src/tokenlist/types.ts src/tokenlists/types.ts
