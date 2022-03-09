e47169d79207914a1fb7c100e3c531e3d2bc45a3
Pádraic Ó Mhuiris <patrick.morris.310@gmail.com>
2022-03-09 12:38:30 +0000

    list applicable zappable tokens in fixed rate list

---
+++ src/elf/zaps/zapPurchase/zapPurchase.ts
@@ -0,0 +1,48 @@
+import { TokenTag, TokenInfo, CurveLpTokenInfo } from "@elementfi/tokenlist";
+import { tokenListJson } from "tokenlists/tokenlists";
+
+export function isCurveLpTokenInfo(
+  tokenInfo: TokenInfo
+): tokenInfo is CurveLpTokenInfo {
+  return !!tokenInfo?.tags?.includes(TokenTag.CURVE);
+}
+
+const curveLpTokenInfos = tokenListJson.tokens.filter(isCurveLpTokenInfo);
+
+function getPoolAssetTokenInfosForCurveLpTokenInfo(
+  tokenInfo: CurveLpTokenInfo
+) {
+  return tokenInfo.extensions.poolAssets
+    .map((assetAddress) => {
+      return tokenListJson.tokens.find(
+        (tokenInfo) => tokenInfo.address === assetAddress
+      );
+    })
+    .filter((info): info is TokenInfo => !!info);
+}
+
+// Returns the various pool tokens if the underlying token
+// is a curve lp token
+export function getZappableTokenInfosForUnderlying(
+  underlyingAddress: string
+): TokenInfo[] {
+  const underlyingCurveTokenInfo = curveLpTokenInfos.find(
+    ({ address }) => address === underlyingAddress
+  );
+
+  if (!underlyingCurveTokenInfo) return [];
+
+  const poolAssetTokenInfos = getPoolAssetTokenInfosForCurveLpTokenInfo(
+    underlyingCurveTokenInfo
+  );
+
+  const poolAssetCurveLpTokenInfos =
+    poolAssetTokenInfos.filter(isCurveLpTokenInfo);
+
+  // Curve calls the likes of 3CRV "metapools"
+  const metaPoolAssetTokenInfo =
+    poolAssetCurveLpTokenInfos.length === 1
+      ? getPoolAssetTokenInfosForCurveLpTokenInfo(poolAssetCurveLpTokenInfos[0])
+      : [];
+  return [...poolAssetTokenInfos, ...metaPoolAssetTokenInfo];
+}
