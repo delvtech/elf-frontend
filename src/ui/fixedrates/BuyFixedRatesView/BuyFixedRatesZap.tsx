05f77b3795de1d97e8332fb60237d286d25831f1
Pádraic Ó Mhuiris <patrick.morris.310@gmail.com>
2022-03-23 14:31:11 +0000

    added principal market price to BuyFixedRatesZap

--- src/ui/fixedrates/BuyFixedRatesView/BuyFixedRatesZap.tsx
+++ src/ui/fixedrates/BuyFixedRatesView/BuyFixedRatesZap.tsx
@@ -1,4 +1,4 @@
-import { Classes, Intent } from "@blueprintjs/core";
+import { Button, Classes, Intent } from "@blueprintjs/core";
 import { PrincipalTokenInfo, TokenInfo } from "@elementfi/tokenlist";
 import { Web3Provider } from "@ethersproject/providers";
 import { useWeb3React } from "@web3-react/core";
@@ -6,21 +6,26 @@ import { formatBalance } from "base/formatBalance/formatBalance";
 import classNames from "classnames";
 import tw from "efi-tailwindcss-classnames";
 import { getCryptoAssetForToken } from "elf/crypto/getCryptoAssetForToken";
+import { commify } from "ethers/lib/utils";
 import { ReactElement } from "react";
 import { t } from "ttag";
 import { useNumericInput } from "ui/base/hooks/useNumericInput/useNumericInput";
+import { useMarketPrice } from "ui/ccpools/useMarketPrice";
 import { useCryptoBalanceOf } from "ui/crypto/hooks/useCryptoBalance/useCryptoBalance";
 import { useDarkMode } from "ui/prefs/useDarkMode/useDarkMode";
 import { TokenAmountInput2 } from "ui/token/TokenAmountInput/TokenAmountInput2";
+import { getMarketRateLabel } from "ui/tranche/getMarketRateLabel";
 
 interface BuyFixedRatesSwapProps {
   principalToken: PrincipalTokenInfo;
   inputToken: TokenInfo;
+  inputTokens: TokenInfo[];
 }
 
 export function BuyFixedRatesZap({
   principalToken,
   inputToken,
+  inputTokens,
 }: BuyFixedRatesSwapProps): ReactElement {
   const { account, library } = useWeb3React<Web3Provider>();
 
@@ -37,6 +42,16 @@ export function BuyFixedRatesZap({
 
   const hasInputError = false; // replace
   const inputErrorMessage = "";
+
+  const principalPrice = useMarketPrice(principalToken);
+  const roundedPrincipalPrice = commify((+principalPrice)?.toFixed(4));
+  const marketRateLabel = getMarketRateLabel(
+    inputTokens[0].symbol,
+    roundedPrincipalPrice,
+    inputTokens[0].symbol
+  );
+
+  const buyButtonIntent = hasInputError ? Intent.DANGER : Intent.PRIMARY;
   return (
     <>
       <div className={tw("flex", "flex-col")}>
@@ -46,27 +61,27 @@ export function BuyFixedRatesZap({
             className={classNames(Classes.TEXT_MUTED, tw("text-right", "mb-2"))}
           >{t`Balance: ${inputAssetDisplayBalance} ${inputToken.symbol}`}</span>
         )}
-      </div>
-      <div
-        style={{
-          background: isDarkMode ? "var(--bp3-dark-bg-color)" : "",
-        }}
-        className={classNames(tw("flex", "rounded", "mb-2"))}
-      >
-        <TokenAmountInput2
-          showMaxButton
-          placeholder="0.00"
-          inputGroupStyle={{
-            height: "72px",
-            width: "100%",
-            fontSize: "1.125rem",
+        <div
+          style={{
+            background: isDarkMode ? "var(--bp3-dark-bg-color)" : "",
           }}
-          intent={hasInputError ? Intent.DANGER : Intent.NONE}
-          value={inputTokenValue}
-          maxAmount={inputAssetBalanceOf}
-          tokenDecimals={inputToken.decimals}
-          onValueChange={onInputChange}
-        />
+          className={classNames(tw("flex", "rounded", "mb-2"))}
+        >
+          <TokenAmountInput2
+            showMaxButton
+            placeholder="0.00"
+            inputGroupStyle={{
+              height: "72px",
+              width: "100%",
+              fontSize: "1.125rem",
+            }}
+            intent={hasInputError ? Intent.DANGER : Intent.NONE}
+            value={inputTokenValue}
+            maxAmount={inputAssetBalanceOf}
+            tokenDecimals={inputToken.decimals}
+            onValueChange={onInputChange}
+          />
+        </div>
         {hasInputError && (
           <span
             className={classNames(
@@ -81,7 +96,36 @@ export function BuyFixedRatesZap({
             {inputErrorMessage}
           </span>
         )}
+        {marketRateLabel && (
+          <span
+            className={classNames(
+              Classes.TEXT_MUTED,
+              tw("text-xs", "text-right")
+            )}
+          >
+            {marketRateLabel}
+          </span>
+        )}
       </div>
+      <div className={tw("flex", "flex-col", "space-y-3")}>
+        <span className={tw("text-base", "text-left")}>{t`Review Order`}</span>
+
+        {/* <FixedRatePreviewCallout
+            principalTokensOut={principalTokensOut}
+            baseAssetIn={baseAssetInputValue}
+            baseAssetDecimals={baseAssetDecimals}
+            /> */}
+        <div>Fixed Preview Callout</div>
+      </div>
+      <Button
+        disabled={true}
+        onClick={() => null}
+        outlined
+        large
+        intent={buyButtonIntent}
+      >
+        {hasInputError ? "buttonErrorMessage" : t`Buy`}
+      </Button>
     </>
   );
 }
--- src/ui/tranche/getMarketRateLabel.ts
+++ src/ui/tranche/getMarketRateLabel.ts
@@ -1,12 +1,9 @@
 import { t } from "ttag";
 
 export function getMarketRateLabel(
-  baseAssetSymbol: string | undefined,
-  roundedTranchePrice: string | undefined,
-  inputAssetSymbol: string | undefined
-): string | undefined {
-  if (!baseAssetSymbol || !roundedTranchePrice || !inputAssetSymbol) {
-    return;
-  }
+  baseAssetSymbol: string,
+  roundedTranchePrice: string,
+  inputAssetSymbol: string
+): string {
   return t`1 ${baseAssetSymbol} Principal Token ≈ ${roundedTranchePrice} ${inputAssetSymbol}`;
 }
