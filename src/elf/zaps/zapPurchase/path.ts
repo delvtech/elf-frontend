708184d844e2f7c5d0436a8a2454f6986f0c732d
Pádraic Ó Mhuiris <patrick.morris.310@gmail.com>
2022-03-23 17:14:20 +0000

    comments

--- src/elf/zaps/zapPurchase/path.ts
+++ src/elf/zaps/zapPurchase/path.ts
@@ -28,38 +28,46 @@ interface ZapPurchasePathDoubleStep {
   metaToken: CurveLpTokenInfo;
   curvePoolToken: TokenInfo;
 }
-
-//
-// ZapPurchasePath is used to signify the relationship between the
-// curvePoolToken and the principalToken. This is required as we must know which
-// internal curvePoolLpTokens that are exchanged for in order to calculate
-// approximate pricing of the principalToken in terms of the curvePoolToken and
-// also to construct the zap contract inputs
-//
-// The properties under each indicate two paths
-//
-// 1) curvePoolToken <-> baseToken <-> principalToken
-// This is the single step approach as their is only 1 token between the
-// curvePoolToken and the principalToken. An example of this is the steCRV
-// principal token where ETH (being the curvePoolToken) must be exchanged into
-// steCRV (the baseToken) before being exchanged into it's namesake principal
-// token
-//
-// 2) curvePoolToken <-> metaToken <-> baseToken <-> principalToken
-// This is the double step approach as their is 2 tokens between the
-// curvePoolToken and principalToken. An example would be MIM-3LP principal
-// token where DAI (being the curvePoolToken) must be exchanged into 3CRV
-// (the metaToken) which must be exchanged into MIM-3LP (the baseToken) which
-// finally is exchanged for the MIM-3LP principalToken
-//
+/**
+ * ZapPurchasePath is used to signify the relationship between the
+ * curvePoolToken and the principalToken. This is required as we must know which
+ * internal curvePoolLpTokens that are exchanged for in order to calculate
+ * approximate pricing of the principalToken in terms of the curvePoolToken and
+ * also to construct the zap contract inputs
+ *
+ * The properties under each indicate two paths
+ *
+ * 1) curvePoolToken <-> baseToken <-> principalToken
+ * This is the single step approach as their is only 1 token between the
+ * curvePoolToken and the principalToken. An example of this is the steCRV
+ * principal token where ETH (being the curvePoolToken) must be exchanged into
+ * steCRV (the baseToken) before being exchanged into it's namesake principal
+ * token
+ *
+ * 2) curvePoolToken <-> metaToken <-> baseToken <-> principalToken
+ * This is the double step approach as their is 2 tokens between the
+ * curvePoolToken and principalToken. An example would be MIM-3LP principal
+ * token where DAI (being the curvePoolToken) must be exchanged into 3CRV
+ * (the metaToken) which must be exchanged into MIM-3LP (the baseToken) which
+ * finally is exchanged for the MIM-3LP principalToken
+ */
 type ZapPurchasePath = ZapPurchasePathSingleStep | ZapPurchasePathDoubleStep;
 
-// This function assumes that the curvePool token is a valid member of the
-// curve pools related to the principalToken's underlying token address.
-//
-// e.g curvePoolToken is a member of the returned values from
-// getCurvePoolTokensByPrincipalToken(principalToken). We assume this so as not
-// to have an undefined case for simplicity
+/**
+ * This function assumes that the curvePool token is a valid member of the
+ * curve pools related to the principalToken's underlying token address.
+ *
+ * e.g curvePoolToken is a member of the returned values from
+ * getCurvePoolTokensByPrincipalToken(principalToken). We assume this so as not
+ * to have an undefined case for simplicity
+ *
+ * @param principalToken - the principaToken
+ * @param curvePoolToken - a token which is a member of a curve pool related to
+ *                         the underlying pool of the principal token
+ * @returns zapPurchasePath - an object containing the inner lptokens which form
+ *                            the exchange path between the principal and
+ *                            curvePool tokens
+ * */
 export function getZapPurchasePath(
   principalToken: PrincipalTokenInfo,
   curvePoolToken: TokenInfo
