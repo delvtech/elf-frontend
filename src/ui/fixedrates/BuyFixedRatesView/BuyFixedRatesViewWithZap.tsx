18982f9ff8b340b5b75e0599ec9ad5837af2783e
Pádraic Ó Mhuiris <patrick.morris.310@gmail.com>
2022-03-23 00:37:08 +0000

    Branch zap and swap cases in BuyFixedRates (#124)

--- src/ui/fixedrates/BuyFixedRatesView/BuyFixedRatesViewWithZap.tsx
+++ src/ui/fixedrates/BuyFixedRatesView/BuyFixedRatesViewWithZap.tsx
@@ -1,6 +1,5 @@
-import { Button, Card, Classes, Colors, Icon, Intent } from "@blueprintjs/core";
+import { Button, Card, Colors, Icon } from "@blueprintjs/core";
 import { IconNames } from "@blueprintjs/icons";
-import { Select } from "@blueprintjs/select";
 import {
   PrincipalPoolTokenInfo,
   PrincipalTokenInfo,
@@ -8,36 +7,18 @@ import {
 } from "@elementfi/tokenlist";
 import { Web3Provider } from "@ethersproject/providers";
 import { useWeb3React } from "@web3-react/core";
-import { formatBalance } from "base/formatBalance/formatBalance";
-import classNames from "classnames";
 import tw from "efi-tailwindcss-classnames";
-import { getCryptoAssetForToken } from "elf/crypto/getCryptoAssetForToken";
-import { getCryptoSymbol } from "elf/crypto/getCryptoSymbol";
-import { commify } from "ethers/lib/utils";
-import { SwapKind } from "integrations/balancer/SwapKind";
-import { Fragment, ReactElement, useCallback, useMemo, useState } from "react";
+import { Fragment, ReactElement, useCallback, useState } from "react";
 import { t } from "ttag";
 import { useNavigation } from "ui/app/navigation/hooks/useNavigation";
 import { Navigation } from "ui/app/navigation/navigation";
-import { useNumericInput } from "ui/base/hooks/useNumericInput/useNumericInput";
-import { LabeledText } from "ui/base/LabeledText/LabeledText";
-import { useIsTailwindLargeScreen } from "ui/base/mediaBreakpoints";
 import { Title } from "ui/base/Title";
-import { useCalculatePrincipalTokenAmountOut } from "ui/ccpools/useCalculatePrincipalTokenAmountOut";
-import { useMarketPrice } from "ui/ccpools/useMarketPrice";
-import { findAssetIcon, findAssetIconByAddress } from "ui/crypto/CryptoIcon";
-import { useCryptoBalanceOf } from "ui/crypto/hooks/useCryptoBalance/useCryptoBalance";
 import { BuyFixedRatesKind } from "ui/fixedrates/buyFixedRateKind";
-import { useFixedRateInputAssetInfo } from "ui/fixedrates/useFixedRateInputAssetInfo";
-import { useFixedRateInputTokens } from "ui/fixedrates/useFixedRateInputTokens";
-import { useValidateBuyPrincipalTokenInput } from "ui/pools/hooks/useValidateBuyPrincipalTokenInput";
-import { useDarkMode } from "ui/prefs/useDarkMode/useDarkMode";
-import { SwapTokensTransactionConfirmationDrawer } from "ui/swaps/SwapTokensTransactionConfirmationDrawer/SwapTokensTransactionConfirmationDrawer";
-import { TokenAmountInput2 } from "ui/token/TokenAmountInput/TokenAmountInput2";
-import { getMarketRateLabel } from "ui/tranche/getMarketRateLabel";
-import { PrincipalTokenTermButtonLabel2 } from "ui/tranche/TermPicker/PrincipalTokenTermButtonLabel2";
-import { TermPicker2 } from "ui/tranche/TermPicker/TermPicker2";
-import { FixedRatePreviewCallout } from "./FixedRatePreviewCallout";
+import { BuyFixedRatesInputSelect } from "ui/fixedrates/BuyFixedRatesView/BuyFixedRatesInputSelect";
+import { BuyFixedRatesSwap } from "ui/fixedrates/BuyFixedRatesView/BuyFixedRatesSwap";
+import { BuyFixedRatesTermPicker } from "ui/fixedrates/BuyFixedRatesView/BuyFixedRatesTermPicker";
+import { BuyFixedRatesZap } from "ui/fixedrates/BuyFixedRatesView/BuyFixedRatesZap";
+import { getFixedRateInputTokens } from "ui/fixedrates/getFixedRateInputTokens";
 
 export interface BuyFixedRatesViewProps {
   availablePrincipalTokens: PrincipalTokenInfo[];
@@ -48,113 +29,33 @@ export interface BuyFixedRatesViewProps {
   principalTokenAddress?: string;
 }
 
-const TokenInfoSelect = Select.ofType<TokenInfo>();
-
 export function BuyFixedRatesViewWithZap({
   availablePrincipalTokens,
   principalTokenInfo,
   principalTokenPoolInfo,
-  principalTokenAddress,
 }: BuyFixedRatesViewProps): ReactElement {
-  const { account, library } = useWeb3React<Web3Provider>();
-  const { isDarkMode } = useDarkMode();
+  const { account } = useWeb3React<Web3Provider>();
   const { changeTab } = useNavigation();
   const goToFixedRatesListPage = useCallback(() => {
     changeTab(Navigation.FIXED_RATES);
   }, [changeTab]);
 
-  const [isDrawerOpen, setDrawerOpen] = useState(false);
-  const openDrawer = useCallback(() => setDrawerOpen(true), []);
-  const closeDrawer = useCallback(() => {
-    setDrawerOpen(false);
-  }, []);
-
-  // Tailwind mindset is mobile-first, but sometimes we need to know at runtime
-  // if a large screen is being used.
-  const isLargeScreen = useIsTailwindLargeScreen();
-
   const {
     extensions: { underlying },
   } = principalTokenInfo;
 
-  const baseAssetSymbol = useMemo(
-    () => getCryptoSymbol(getCryptoAssetForToken(underlying)),
-    [underlying]
-  );
-  const inputTokens = useFixedRateInputTokens(underlying);
-
-  const inputTokenIconsByAddress = useMemo(
-    () =>
-      Object.fromEntries(
-        inputTokens.map(({ address }) => [
-          address,
-          findAssetIconByAddress(address),
-        ])
-      ),
-    [inputTokens]
-  );
-
+  // inputTokenInfos will always give an array of either [underlyingTokenInfo]
+  // in the case when principalTokenis not curve based and otherwise will give
+  // the underlyingTokenInfo + other input tokens
+  const inputTokenInfos = getFixedRateInputTokens(principalTokenInfo);
   const [selectedInputToken, setSelectedInputToken] = useState<TokenInfo>(
-    inputTokens[0]
+    inputTokenInfos[0]
   );
 
   const buyFixedRatesKind =
     selectedInputToken.address === underlying
       ? BuyFixedRatesKind.Swap
       : BuyFixedRatesKind.Zap;
-
-  const {
-    inputAsset,
-    inputAssetAddress,
-    inputAssetName,
-    inputAssetDecimals,
-    inputAssetSymbol,
-  } = useFixedRateInputAssetInfo(selectedInputToken.address, buyFixedRatesKind);
-
-  const AssetIcon = findAssetIcon(inputAsset);
-  const height = isLargeScreen ? 40 : 30;
-  const width = height;
-
-  const inputAssetBalanceOf = useCryptoBalanceOf(library, account, inputAsset);
-  const inputAssetDisplayBalance = formatBalance(
-    inputAssetBalanceOf,
-    inputAssetDecimals
-  );
-
-  // Market price stuff
-  const principalPrice = useMarketPrice(principalTokenInfo);
-  const roundedPrincipalPrice = commify((+principalPrice)?.toFixed(4));
-
-  // TODO Get market rate for all tokens
-  const marketRateLabel = getMarketRateLabel(
-    baseAssetSymbol,
-    roundedPrincipalPrice,
-    inputAssetSymbol
-  );
-
-  // Deposit Amount stuff
-  const { stringValue: baseAssetInputValue, setValue: onBaseAssetInputChange } =
-    useNumericInput();
-  const { tokenOutError, tokenInError } = useValidateBuyPrincipalTokenInput(
-    library,
-    account,
-    principalTokenPoolInfo,
-    baseAssetInputValue
-  );
-  const { amountOut: principalTokensOut, error: previewError } =
-    useCalculatePrincipalTokenAmountOut(
-      principalTokenPoolInfo,
-      baseAssetInputValue
-    );
-
-  const inputErrorMessage = tokenInError || tokenOutError;
-  const hasInputError = !!inputErrorMessage || !!previewError;
-
-  const isBuyButtonDisabled = hasInputError || !+baseAssetInputValue;
-  const buttonErrorMessage = previewError
-    ? t`Insufficient liquidity in pool`
-    : inputErrorMessage;
-  const buyButtonIntent = hasInputError ? Intent.DANGER : Intent.PRIMARY;
 
   return (
     <Fragment>
@@ -195,7 +96,7 @@ export function BuyFixedRatesViewWithZap({
           <div style={{ width: 1 }} className={tw("h-full", "bg-white")} />
           <span
             className={tw("pl-2", "font-semibold")}
-          >{`${inputAssetSymbol}`}</span>
+          >{`${inputTokenInfos[0].symbol}`}</span>
         </div>
         <div
           className={tw(
@@ -213,206 +114,30 @@ export function BuyFixedRatesViewWithZap({
           )}
         >
           <Card className={tw("flex", "flex-col", "w-400", "p-6", "space-y-8")}>
-            {/* Base asset Picker */}
-            <div className={tw("flex", "flex-col", "space-y-3")}>
-              <span
-                className={tw("text-base", "text-left")}
-              >{t`Choose Token`}</span>
-
-              <div
-                style={{
-                  background: isDarkMode ? "var(--bp3-dark-bg-color)" : "",
-                }}
-                className={classNames(
-                  tw("flex", "p-1", "border", "rounded", "border-gray-500")
-                )}
-              >
-                <TokenInfoSelect
-                  items={inputTokens}
-                  itemPredicate={(_, s) =>
-                    s.address !== selectedInputToken.address
-                  }
-                  itemRenderer={(
-                    { name, symbol, address },
-                    { handleClick }
-                  ) => (
-                    <div
-                      className={classNames(tw("p-1"))}
-                      onClick={handleClick}
-                    >
-                      <LabeledText
-                        containerClassName={tw("p-4")}
-                        icon={inputTokenIconsByAddress[address]({
-                          height,
-                          width,
-                        })}
-                        iconClassName={tw("flex-shrink-0", "mr-4")}
-                        large={isLargeScreen}
-                        labelClassName={tw("text-xs", "text-left")}
-                        label={symbol}
-                        textClassName={tw("lg:text-base", "text-left")}
-                        text={name}
-                      />
-                    </div>
-                  )}
-                  disabled={inputTokens.length === 1}
-                  onItemSelect={setSelectedInputToken}
-                  filterable={false}
-                >
-                  <LabeledText
-                    containerClassName={tw("p-4")}
-                    icon={<AssetIcon height={height} width={width} />}
-                    iconClassName={tw("flex-shrink-0", "mr-4")}
-                    large={isLargeScreen}
-                    labelClassName={tw("text-xs", "text-left")}
-                    label={inputAssetSymbol}
-                    textClassName={tw("lg:text-base", "text-left")}
-                    text={inputAssetName}
-                  />
-                </TokenInfoSelect>
-              </div>
-            </div>
-
-            {/* Term Picker  */}
-            <div className={tw("flex", "flex-col", "space-y-3")}>
-              <span
-                className={tw("text-base", "text-left")}
-              >{t`Select Term Period`}</span>
-              <div
-                style={{
-                  background: isDarkMode ? "var(--bp3-dark-bg-color)" : "",
-                }}
-                className={classNames(
-                  tw("flex", "p-1", "border", "rounded", "border-gray-500")
-                )}
-              >
-                {availablePrincipalTokens.length > 1 ? (
-                  <TermPicker2
-                    account={account}
-                    principalTokenInfos={availablePrincipalTokens}
-                    activePrincipalToken={principalTokenInfo}
-                    buttonLabelRenderer={buttonLabelRenderer}
-                  />
-                ) : (
-                  // just show the label if there's no picker
-                  <PrincipalTokenTermButtonLabel2
-                    className={tw("p-4")}
-                    principalTokenInfo={principalTokenInfo}
-                  />
-                )}
-              </div>
-            </div>
-
-            {/* Deposit Amount */}
-            <div className={tw("flex", "flex-col")}>
-              <span
-                className={tw("text-base", "text-left")}
-              >{t`You Spend`}</span>
-              {!!account && (
-                <span
-                  className={classNames(
-                    Classes.TEXT_MUTED,
-                    tw("text-right", "mb-2")
-                  )}
-                >{t`Balance: ${inputAssetDisplayBalance} ${inputAssetSymbol}`}</span>
-              )}
-              <div
-                style={{
-                  background: isDarkMode ? "var(--bp3-dark-bg-color)" : "",
-                }}
-                className={classNames(tw("flex", "rounded", "mb-2"))}
-              >
-                <TokenAmountInput2
-                  showMaxButton
-                  placeholder="0.00"
-                  inputGroupStyle={{
-                    height: "72px",
-                    width: "100%",
-                    fontSize: "1.125rem",
-                  }}
-                  intent={hasInputError ? Intent.DANGER : Intent.NONE}
-                  value={baseAssetInputValue}
-                  maxAmount={inputAssetBalanceOf}
-                  tokenDecimals={inputAssetDecimals}
-                  onValueChange={onBaseAssetInputChange}
-                />
-              </div>
-              {hasInputError && (
-                <span
-                  className={classNames(
-                    tw(
-                      "text-xs",
-                      "text-right",
-                      "mb-2",
-                      isDarkMode ? "text-red-500" : "text-red-700"
-                    )
-                  )}
-                >
-                  {inputErrorMessage}
-                </span>
-              )}
-              {marketRateLabel && (
-                <span
-                  className={classNames(
-                    Classes.TEXT_MUTED,
-                    tw("text-xs", "text-right")
-                  )}
-                >
-                  {marketRateLabel}
-                </span>
-              )}
-            </div>
-
-            <div className={tw("flex", "flex-col", "space-y-3")}>
-              <span
-                className={tw("text-base", "text-left")}
-              >{t`Review Order`}</span>
-
-              <FixedRatePreviewCallout
-                principalTokensOut={principalTokensOut}
-                baseAssetIn={baseAssetInputValue}
-                baseAssetDecimals={inputAssetDecimals}
+            <BuyFixedRatesInputSelect
+              inputTokens={inputTokenInfos}
+              selectedToken={selectedInputToken}
+              onTokenSelect={setSelectedInputToken}
+            />
+            <BuyFixedRatesTermPicker
+              availablePrincipalTokens={availablePrincipalTokens}
+              account={account}
+              activePrincipalToken={principalTokenInfo}
+            />
+            {buyFixedRatesKind === BuyFixedRatesKind.Swap ? (
+              <BuyFixedRatesSwap
+                principalToken={principalTokenInfo}
+                principalTokenPoolInfo={principalTokenPoolInfo}
               />
-            </div>
-
-            <Button
-              disabled={isBuyButtonDisabled}
-              onClick={openDrawer}
-              outlined
-              large
-              intent={buyButtonIntent}
-            >
-              {hasInputError ? buttonErrorMessage : t`Buy`}
-            </Button>
+            ) : (
+              <BuyFixedRatesZap
+                principalToken={principalTokenInfo}
+                inputToken={selectedInputToken}
+              />
+            )}
           </Card>
         </div>
       </div>
-      {/* TODO should tokenOutDecimals correspond to inputAssetDecimals?*/}
-      <SwapTokensTransactionConfirmationDrawer
-        buttonLabel={t`Buy`}
-        tokenInAddress={inputAssetAddress}
-        tokenInSymbol={inputAssetSymbol}
-        tokenInDecimals={inputAssetDecimals}
-        tokenInAsset={inputAsset}
-        tokenInIcon={AssetIcon}
-        tokenOutAddress={principalTokenAddress as string}
-        tokenOutSymbol={principalTokenInfo.symbol}
-        tokenOutDecimals={inputAssetDecimals}
-        tokenOutIcon={undefined}
-        account={account}
-        library={library}
-        poolInfo={principalTokenPoolInfo}
-        amountIn={baseAssetInputValue}
-        amountOut={principalTokensOut}
-        swapKind={SwapKind.GIVEN_IN}
-        spotPrice={+principalPrice}
-        isOpen={isDrawerOpen}
-        onClose={closeDrawer}
-      />
     </Fragment>
   );
-}
-
-function buttonLabelRenderer(term: PrincipalTokenInfo): ReactElement {
-  return <PrincipalTokenTermButtonLabel2 principalTokenInfo={term} />;
 }
---
+++ src/ui/fixedrates/BuyFixedRatesView/BuyFixedRatesZap.tsx
@@ -0,0 +1,87 @@
+import { Classes, Intent } from "@blueprintjs/core";
+import { PrincipalTokenInfo, TokenInfo } from "@elementfi/tokenlist";
+import { Web3Provider } from "@ethersproject/providers";
+import { useWeb3React } from "@web3-react/core";
+import { formatBalance } from "base/formatBalance/formatBalance";
+import classNames from "classnames";
+import tw from "efi-tailwindcss-classnames";
+import { getCryptoAssetForToken } from "elf/crypto/getCryptoAssetForToken";
+import { ReactElement } from "react";
+import { t } from "ttag";
+import { useNumericInput } from "ui/base/hooks/useNumericInput/useNumericInput";
+import { useCryptoBalanceOf } from "ui/crypto/hooks/useCryptoBalance/useCryptoBalance";
+import { useDarkMode } from "ui/prefs/useDarkMode/useDarkMode";
+import { TokenAmountInput2 } from "ui/token/TokenAmountInput/TokenAmountInput2";
+
+interface BuyFixedRatesSwapProps {
+  principalToken: PrincipalTokenInfo;
+  inputToken: TokenInfo;
+}
+
+export function BuyFixedRatesZap({
+  principalToken,
+  inputToken,
+}: BuyFixedRatesSwapProps): ReactElement {
+  const { account, library } = useWeb3React<Web3Provider>();
+
+  const inputAsset = getCryptoAssetForToken(inputToken.address);
+  const inputAssetBalanceOf = useCryptoBalanceOf(library, account, inputAsset);
+  const inputAssetDisplayBalance = formatBalance(
+    inputAssetBalanceOf,
+    inputToken.decimals
+  );
+  const { stringValue: inputTokenValue, setValue: onInputChange } =
+    useNumericInput();
+
+  const { isDarkMode } = useDarkMode();
+
+  const hasInputError = false; // replace
+  const inputErrorMessage = "";
+  return (
+    <>
+      <div className={tw("flex", "flex-col")}>
+        <span className={tw("text-base", "text-left")}>{t`You Spend`}</span>
+        {!!account && (
+          <span
+            className={classNames(Classes.TEXT_MUTED, tw("text-right", "mb-2"))}
+          >{t`Balance: ${inputAssetDisplayBalance} ${inputToken.symbol}`}</span>
+        )}
+      </div>
+      <div
+        style={{
+          background: isDarkMode ? "var(--bp3-dark-bg-color)" : "",
+        }}
+        className={classNames(tw("flex", "rounded", "mb-2"))}
+      >
+        <TokenAmountInput2
+          showMaxButton
+          placeholder="0.00"
+          inputGroupStyle={{
+            height: "72px",
+            width: "100%",
+            fontSize: "1.125rem",
+          }}
+          intent={hasInputError ? Intent.DANGER : Intent.NONE}
+          value={inputTokenValue}
+          maxAmount={inputAssetBalanceOf}
+          tokenDecimals={inputToken.decimals}
+          onValueChange={onInputChange}
+        />
+        {hasInputError && (
+          <span
+            className={classNames(
+              tw(
+                "text-xs",
+                "text-right",
+                "mb-2",
+                isDarkMode ? "text-red-500" : "text-red-700"
+              )
+            )}
+          >
+            {inputErrorMessage}
+          </span>
+        )}
+      </div>
+    </>
+  );
+}
