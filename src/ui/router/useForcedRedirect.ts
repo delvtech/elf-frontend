acbb4c71f492804d15c2258abaddcb2c897385b9
Ryan Goree <goree.ryan@gmail.com>
2022-03-28 14:33:21 -0500

    Add default value

--- src/ui/router/useForcedRedirect.ts
+++ src/ui/router/useForcedRedirect.ts
@@ -1,7 +1,7 @@
 import { useEffect } from "react";
 import { useRouter } from "next/router";
 
-export const useForcedRedirect = (route: string, enabled: boolean): void => {
+export const useForcedRedirect = (route: string, enabled = true): void => {
   const router = useRouter();
   useEffect(() => {
     if (enabled && router.route !== route) router.replace(route);
