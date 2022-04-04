56f933e55370820b79870402641c1ad3d5552711
Pádraic Ó Mhuiris <patrick.morris.310@gmail.com>
2022-04-04 15:54:00 +0100

    added return type

--- src/ui/zaps/zapSwapCurve/useEstimateBaseTokensByZap.ts
+++ src/ui/zaps/zapSwapCurve/useEstimateBaseTokensByZap.ts
@@ -4,8 +4,8 @@ import { useBaseTokenZapPrice } from "./useBaseTokenZapPrice";
 export function useEstimateBaseTokensByZap(
   principalTokenInfo: PrincipalTokenInfo,
   inputToken: TokenInfo,
-  amountIn: string
-) {
+  amountIn: string,
+): string {
   const price = useBaseTokenZapPrice(principalTokenInfo, inputToken);
   if (price === "0" || amountIn === "0") return "0";
   return (+amountIn / +price).toString();
