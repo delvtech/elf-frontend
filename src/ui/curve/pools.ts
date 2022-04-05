d919cf7ea88de95a6a17714915a1891c4a673105
Pádraic Ó Mhuiris <patrick.morris.310@gmail.com>
2022-04-05 19:48:18 +0100

    Fixed Rate Price calc (#126)

--- src/ui/curve/pools.ts
+++ src/ui/curve/pools.ts
@@ -1,27 +1,32 @@
-import { useCallback } from "react";
-import { QueryObserverResult } from "react-query";
-
-import { BigNumber } from "ethers";
-import { formatUnits } from "ethers/lib/utils";
-import { Currencies, Money } from "ts-money";
-
-import { getCoinGeckoId } from "integrations/coingecko";
 import {
-  crv3CryptoPoolContract,
-  crvTriCryptoPoolContract,
-  steCrvPoolContract,
-} from "elf/curve/pools";
-import { useCoinGeckoPrice } from "ui/coingecko/useCoinGeckoPrice";
-import { useSmartContractReadCall } from "ui/contracts/useSmartContractReadCall/useSmartContractReadCall";
-import { useCurrencyPref } from "ui/prefs/useCurrency/useCurencyPref";
+  CurvePool1,
+  CurvePool2,
+  CurvePool3,
+} from "@elementfi/core-typechain/dist/libraries";
+import { CurveLpTokenInfo, TokenInfo } from "@elementfi/tokenlist";
 import { AddressesJson } from "addresses/addresses";
-import { ONE_MINUTE_IN_MILLISECONDS } from "base/time";
 import {
   isGoerli,
   isMainnet,
   NUM_ETH_DECIMALS,
   ONE_ETHER,
 } from "base/ethereum/ethereum";
+import { ONE_MINUTE_IN_MILLISECONDS } from "base/time";
+import {
+  crv3CryptoPoolContract,
+  crvTriCryptoPoolContract,
+  getCurvePoolContract,
+  steCrvPoolContract,
+} from "elf/curve/pools";
+import { BigNumber, BigNumberish, ethers } from "ethers";
+import { formatUnits, parseUnits } from "ethers/lib/utils";
+import { getCoinGeckoId } from "integrations/coingecko";
+import { useCallback } from "react";
+import { QueryObserverResult } from "react-query";
+import { Currencies, Money } from "ts-money";
+import { useCoinGeckoPrice } from "ui/coingecko/useCoinGeckoPrice";
+import { useSmartContractReadCall } from "ui/contracts/useSmartContractReadCall/useSmartContractReadCall";
+import { useCurrencyPref } from "ui/prefs/useCurrency/useCurencyPref";
 
 interface HookPriceOptions {
   enabled: boolean;
@@ -135,4 +140,87 @@ export function useSteCrvPrice({
   }
 
   return calcWithdrawOneCoinResult;
+}
+
+type CurveAmountArrayParam =
+  | [BigNumberish, BigNumberish, BigNumberish]
+  | [BigNumberish, BigNumberish];
+
+function buildCurveAmountArrayParam(
+  curveLpToken: CurveLpTokenInfo,
+  token: TokenInfo,
+  amount: string,
+): CurveAmountArrayParam {
+  const tokenIdxInPool = curveLpToken.extensions.poolAssets.findIndex(
+    (address) => address === token.address,
+  );
+  const amountInputLength = curveLpToken.extensions.poolAssets.length as 2 | 3;
+  const amountInput = new Array(amountInputLength).fill(
+    0,
+  ) as CurveAmountArrayParam;
+  amountInput[tokenIdxInPool] = parseUnits(amount, token.decimals);
+  return amountInput;
+}
+
+export function useCurveLpTokenPrice(
+  curveLpToken: CurveLpTokenInfo,
+  token: TokenInfo,
+  amount: string | undefined,
+): QueryObserverResult<string> {
+  const curvePoolContract = getCurvePoolContract(curveLpToken);
+  const amountInput = !!amount
+    ? buildCurveAmountArrayParam(curveLpToken, token, amount)
+    : undefined;
+
+  const isPool1 =
+    !["crv3crypto", "crvTricrypto"].includes(curveLpToken.symbol) &&
+    curveLpToken.extensions.poolAssets.length == 2;
+  const isPool2 =
+    !["crv3crypto", "crvTricrypto"].includes(curveLpToken.symbol) &&
+    curveLpToken.extensions.poolAssets.length == 3;
+  const isPool3 = ["crv3crypto", "crvTricrypto"].includes(curveLpToken.symbol);
+
+  const priceWhenCurvePool1 = useSmartContractReadCall(
+    curvePoolContract as CurvePool1,
+    "calc_token_amount",
+    {
+      callArgs: [amountInput as [BigNumberish, BigNumberish], true],
+      staleTime: ONE_MINUTE_IN_MILLISECONDS,
+      enabled: isPool1 && !!amountInput,
+      select: (lpAmount) =>
+        ethers.utils.formatUnits(lpAmount, curveLpToken.decimals),
+    },
+  );
+  const priceWhenCurvePool2 = useSmartContractReadCall(
+    curvePoolContract as CurvePool2,
+    "calc_token_amount",
+    {
+      callArgs: [
+        amountInput as [BigNumberish, BigNumberish, BigNumberish],
+        true,
+      ],
+      staleTime: ONE_MINUTE_IN_MILLISECONDS,
+      enabled: isPool2 && !!amountInput,
+      select: (lpAmount) =>
+        ethers.utils.formatUnits(lpAmount, curveLpToken.decimals),
+    },
+  );
+  const priceWhenCurvePool3 = useSmartContractReadCall(
+    curvePoolContract as CurvePool3,
+    "calc_token_amount",
+    {
+      callArgs: [
+        amountInput as [BigNumberish, BigNumberish, BigNumberish],
+        true,
+      ],
+      staleTime: ONE_MINUTE_IN_MILLISECONDS,
+      enabled: isPool3 && !!amountInput,
+      select: (lpAmount) =>
+        ethers.utils.formatUnits(lpAmount, curveLpToken.decimals),
+    },
+  );
+
+  if (isPool1) return priceWhenCurvePool1;
+  else if (isPool2) return priceWhenCurvePool2;
+  else return priceWhenCurvePool3;
 }
